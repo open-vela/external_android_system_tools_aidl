cb0b8283d0dddc0157dd70610532b5de2efcd2ed
openvela-robot <openvela-robot@xiaomi.com>
2018-07-24 11:12:15 +0800

    Refactor constant declarations. am: 693640b046 am: bef154504e am: a621180037 am: d99efd35e3
    Change-Id: I1e9b3400b52258f65844d9569517c24fb39be395

--- aidl_typenames.cpp
+++ aidl_typenames.cpp
@@ -42,22 +42,6 @@ using std::vector;
 namespace android {
 namespace aidl {
 
-// The built-in AIDL types..
-static const set<string> kBuiltinTypes = {
-    "void",           "boolean",      "byte",           "char",         "int", "long",
-    "float",          "double",       "String",         "List",         "Map", "IBinder",
-    "FileDescriptor", "CharSequence"};
-
-// Note: these types may look wrong because they look like Java
-// types, but they have long been supported from the time when Java
-// was the only target language of this compiler. They are added here for
-// backwards compatibility, but we internally treat them as List and Map,
-// respectively.
-static const map<string, string> kJavaLikeTypeToAidlType = {
-  {"java.util.List", "List"},
-  {"java.util.Map", "Map"},
-};
-
 bool AidlTypenames::AddDefinedType(const AidlDefinedType* type) {
   const string name = type->GetCanonicalName();
   if (defined_types_.find(name) != defined_types_.end()) {
@@ -77,20 +61,17 @@ bool AidlTypenames::AddPreprocessedType(unique_ptr<AidlDefinedType> type) {
 }
 
 bool AidlTypenames::IsBuiltinTypename(const string& type_name) {
-  return kBuiltinTypes.find(type_name) != kBuiltinTypes.end() ||
-      kJavaLikeTypeToAidlType.find(type_name) != kJavaLikeTypeToAidlType.end();
+  return builtin_types_.find(type_name) != builtin_types_.end();
 }
 
-const AidlDefinedType* AidlTypenames::TryGetDefinedType(const string& type_name) const {
+const AidlDefinedType* AidlTypenames::TryGetDefinedType(const string& type_name) {
   // Do the exact match first.
-  auto found_def = defined_types_.find(type_name);
-  if (found_def != defined_types_.end()) {
-    return found_def->second;
+  if (defined_types_.find(type_name) != defined_types_.end()) {
+    return defined_types_[type_name];
   }
 
-  auto found_prep = preprocessed_types_.find(type_name);
-  if (found_prep != preprocessed_types_.end()) {
-    return found_prep->second.get();
+  if (preprocessed_types_.find(type_name) != preprocessed_types_.end()) {
+    return preprocessed_types_[type_name].get();
   }
 
   // Then match with the class name. Defined types has higher priority than
@@ -110,12 +91,8 @@ const AidlDefinedType* AidlTypenames::TryGetDefinedType(const string& type_name)
   return nullptr;
 }
 
-pair<string, bool> AidlTypenames::ResolveTypename(const string& type_name) const {
+pair<string, bool> AidlTypenames::ResolveTypename(const string& type_name) {
   if (IsBuiltinTypename(type_name)) {
-    auto found = kJavaLikeTypeToAidlType.find(type_name);
-    if (found != kJavaLikeTypeToAidlType.end()) {
-      return make_pair(found->second, true);
-    }
     return make_pair(type_name, true);
   }
   const AidlDefinedType* defined_type = TryGetDefinedType(type_name);
--- aidl_typenames.h
+++ aidl_typenames.h
@@ -47,16 +47,25 @@ namespace aidl {
 // to resolve type names in AidlTypeSpecifier.
 //
 // Note that nothing here is specific to either Java or C++.
-class AidlTypenames final {
+class AidlTypenames {
  public:
   AidlTypenames() = default;
   bool AddDefinedType(const AidlDefinedType* type);
   bool AddPreprocessedType(unique_ptr<AidlDefinedType> type);
-  static bool IsBuiltinTypename(const string& type_name);
-  const AidlDefinedType* TryGetDefinedType(const string& type_name) const;
-  pair<string, bool> ResolveTypename(const string& type_name) const;
+  bool IsBuiltinTypename(const string& type_name);
+  const AidlDefinedType* TryGetDefinedType(const string& type_name);
+  pair<string, bool> ResolveTypename(const string& type_name);
 
  private:
+  // The built-in AIDL types..
+  // Note: the last three types may look wrong because they look like Java
+  // types, but they have long been supported from the time when Java
+  // was the only target language of this compiler. They are added here for
+  // backwards compatibility.
+  set<string> builtin_types_{
+      "void",           "boolean",      "byte",           "char",         "int", "long",
+      "float",          "double",       "String",         "List",         "Map", "IBinder",
+      "FileDescriptor", "CharSequence", "java.util.List", "java.util.Map"};
   map<string, const AidlDefinedType*> defined_types_;
   map<string, unique_ptr<AidlDefinedType>> preprocessed_types_;
 };
