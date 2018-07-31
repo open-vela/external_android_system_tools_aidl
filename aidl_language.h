d1d53cf8c4a2e16c7f031d69b8be40a8392ecd15
openvela-robot <openvela-robot@xiaomi.com>
2018-07-31 09:05:16 +0800

    unittests are no longer friend of Options am: 6f77e0ce10 am: 936105b02d am: 20f780a766 am: e1f4d12a61
    Change-Id: I07342fb5b375cc1676eeb921ee8a81a3c66f7f51

--- aidl_language.h
+++ aidl_language.h
@@ -488,6 +488,14 @@ class AidlInterface final : public AidlDefinedType {
     return constants_;
   }
 
+  void SetGenerateTraces(bool generate_traces) {
+    generate_traces_ = generate_traces;
+  }
+
+  bool ShouldGenerateTraces() const {
+    return generate_traces_;
+  }
+
   const AidlInterface* AsInterface() const override { return this; }
   std::string GetPreprocessDeclarationName() const override { return "interface"; }
 
@@ -497,6 +505,8 @@ class AidlInterface final : public AidlDefinedType {
   bool oneway_;
   std::vector<std::unique_ptr<AidlMethod>> methods_;
   std::vector<std::unique_ptr<AidlConstantDeclaration>> constants_;
+
+  bool generate_traces_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(AidlInterface);
 };
