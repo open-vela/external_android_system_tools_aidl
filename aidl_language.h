91fb9a5a9a53de4a5fe6c5a770f0a66b67266f51
openvela-robot <openvela-robot@xiaomi.com>
2018-08-29 20:04:11 +0800

    API dump includes default values for parcelable fields am: a468e2a0c6 am: 0909a66c19 am: f7cd62efa7
    Change-Id: I32da37cd7da0aee863fe7dfe0096f6441905ab79

--- aidl_language.h
+++ aidl_language.h
@@ -361,8 +361,6 @@ class AidlConstantDeclaration : public AidlMember {
   const AidlConstantValue& GetValue() const { return *value_; }
   bool CheckValid() const;
 
-  std::string ToString() const;
-  std::string Signature() const;
   string ValueString(const ConstantValueDecorator& decorator) const {
     return GetValue().As(GetType(), decorator);
   }
