06c4e99c734ca9e8816f1c52d0f627fd13312384
liujinye <liujinye@xiaomi.com>
2019-01-18 14:08:46 +0800

    Split readFromParcel into fromBinder. am: 3c316eda44 am: 6c85d236fa am: 0f40ec7922
    Change-Id: Ie38e56e817d6e1c3340bc3ea34771a926ac53332

--- generate_ndk.cpp
+++ generate_ndk.cpp
@@ -553,18 +553,16 @@ void GenerateInterfaceSource(CodeWriter& out, const AidlTypenames& types,
 
   out << "std::shared_ptr<" << clazz << "> " << clazz
       << "::fromBinder(const ::ndk::SpAIBinder& binder) {\n";
-  out.Indent();
   out << data_clazz << "* data = static_cast<" << data_clazz
       << "*>(AIBinder_getUserData(binder.get()));\n";
   out << "if (data) {\n";
   out.Indent();
   out << "return data->instance;\n";
   out.Dedent();
-  out << "}\n";
+  out << "}";
   // If it is local, it is not an 'ndk' instance, and parceling will happen locally.
   out << "return " << NdkFullClassName(defined_type, ClassNames::CLIENT)
       << "::associate(binder);\n";
-  out.Dedent();
   out << "}\n\n";
 
   out << "binder_status_t " << clazz << "::writeToParcel(AParcel* parcel, const std::shared_ptr<"
@@ -748,7 +746,7 @@ void GenerateInterfaceHeader(CodeWriter& out, const AidlTypenames& types,
         << ";\n";
   }
   out << "\n";
-  out << "static std::shared_ptr<" << clazz << "> fromBinder(const ::ndk::SpAIBinder& binder);\n";
+  out << "static std::shared_ptr<" << clazz << "> fromBinder(const ::ndk::SpAIBinder& binder);";
   out << "static binder_status_t writeToParcel(AParcel* parcel, const std::shared_ptr<" << clazz
       << ">& instance);";
   out << "\n";
