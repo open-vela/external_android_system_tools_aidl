f824e7d54ae8d5a802dce68866d2c653866b17d2
Dan Willemsen <dwillemsen@google.com>
2019-06-10 02:09:03 +0000

    Expose proper header dependencies
    Instead of giving the .cpp file as a dependency when using the exported
    header directories, actually return the header files.
    
    This is necessary for my RBE builds to properly pick up the header
    files.
    
    Test: treehugger
    Test: build w/RBE
    Change-Id: Ie7befe7feca28540eeaf025a3c20b6c5b9291c62

--- build/aidl_interface.go
+++ build/aidl_interface.go
@@ -158,9 +158,10 @@ type aidlGenRule struct {
 	implicitInputs android.Paths
 	importFlags    string
 
-	genOutDir    android.ModuleGenPath
-	genHeaderDir android.ModuleGenPath
-	genOutputs   android.WritablePaths
+	genOutDir     android.ModuleGenPath
+	genHeaderDir  android.ModuleGenPath
+	genHeaderDeps android.Paths
+	genOutputs    android.WritablePaths
 }
 
 var _ android.SourceFileProducer = (*aidlGenRule)(nil)
@@ -189,7 +190,9 @@ func (g *aidlGenRule) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 	g.genOutDir = android.PathForModuleGen(ctx)
 	g.genHeaderDir = android.PathForModuleGen(ctx, "include")
 	for _, src := range srcs {
-		g.genOutputs = append(g.genOutputs, g.generateBuildActionsForSingleAidl(ctx, src))
+		outFile, headers := g.generateBuildActionsForSingleAidl(ctx, src)
+		g.genOutputs = append(g.genOutputs, outFile)
+		g.genHeaderDeps = append(g.genHeaderDeps, headers...)
 	}
 
 	// This is to clean genOutDir before generating any file
@@ -203,7 +206,7 @@ func (g *aidlGenRule) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 	})
 }
 
-func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContext, src android.Path) android.WritablePath {
+func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContext, src android.Path) (android.WritablePath, android.Paths) {
 	var outFile android.WritablePath
 	if g.properties.Lang == langJava {
 		outFile = android.PathForModuleGen(ctx, pathtools.ReplaceExtension(src.Rel(), "java"))
@@ -216,6 +219,7 @@ func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContex
 		optionalFlags = append(optionalFlags, "--version "+g.properties.Version)
 	}
 
+	var headers android.WritablePaths
 	if g.properties.Lang == langJava {
 		ctx.ModuleBuild(pctx, android.ModuleBuildParams{
 			Rule:      aidlJavaRule,
@@ -245,7 +249,6 @@ func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContex
 			prefix = "aidl"
 		}
 
-		var headers android.WritablePaths
 		headers = append(headers, g.genHeaderDir.Join(ctx, prefix, packagePath,
 			typeName+".h"))
 		headers = append(headers, g.genHeaderDir.Join(ctx, prefix, packagePath,
@@ -278,7 +281,7 @@ func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContex
 		})
 	}
 
-	return outFile
+	return outFile, headers.Paths()
 }
 
 func (g *aidlGenRule) GeneratedSourceFiles() android.Paths {
@@ -290,7 +293,7 @@ func (g *aidlGenRule) Srcs() android.Paths {
 }
 
 func (g *aidlGenRule) GeneratedDeps() android.Paths {
-	return g.genOutputs.Paths()
+	return g.genHeaderDeps
 }
 
 func (g *aidlGenRule) GeneratedHeaderDirs() android.Paths {
