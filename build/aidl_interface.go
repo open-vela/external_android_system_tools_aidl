af646506f75eae5850baeaf55af9beb3e5eebfea
liujinye <liujinye@xiaomi.com>
2019-08-14 14:10:00 +0800

    [automerger skipped] DO NOT MERGE - Skip qt-dev-plus-aosp-without-vendor (5713463) in stage-aosp-master am: 6af4a5a74d -s ours am: 1555d20d2f
    Change-Id: Ifc24915faed88de61b99a6ab0a894b3d6df5bd16

--- build/aidl_interface.go
+++ build/aidl_interface.go
@@ -675,11 +675,6 @@ func (i *aidlInterface) checkImports(mctx android.LoadHookContext) {
 			mctx.PropertyErrorf("imports", "Import does not exist: "+anImport)
 		}
 
-		if i.shouldGenerateJavaBackend() && !other.shouldGenerateJavaBackend() {
-			mctx.PropertyErrorf("backend.java.enabled",
-				"Java backend not enabled in the imported AIDL interface %q", anImport)
-		}
-
 		if i.shouldGenerateCppBackend() && !other.shouldGenerateCppBackend() {
 			mctx.PropertyErrorf("backend.cpp.enabled",
 				"C++ backend not enabled in the imported AIDL interface %q", anImport)
@@ -788,11 +783,9 @@ func aidlInterfaceHook(mctx android.LoadHookContext, i *aidlInterface) {
 		}
 	}
 
-	if i.shouldGenerateJavaBackend() {
-		libs = append(libs, addJavaLibrary(mctx, i, currentVersion))
-		for _, version := range i.properties.Versions {
-			addJavaLibrary(mctx, i, version)
-		}
+	libs = append(libs, addJavaLibrary(mctx, i, currentVersion))
+	for _, version := range i.properties.Versions {
+		addJavaLibrary(mctx, i, version)
 	}
 
 	addApiModule(mctx, i)
