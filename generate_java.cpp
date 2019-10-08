5cb163ac217ceeb74cab416e39edbbbc419e415c
liujinye <liujinye@xiaomi.com>
2019-10-08 11:04:18 +0800

    C++ enum w/o const& am: ddec09dd4f am: b400ff683f am: baa3ae3e4b
    Change-Id: I0211ddf29ec6be9e530d87b57f0521e5c4e364b7

--- generate_java.cpp
+++ generate_java.cpp
@@ -171,8 +171,8 @@ std::unique_ptr<android::aidl::java::Class> generate_parcel_class(
         .writer = *(writer.get()),
         .typenames = typenames,
         .type = field->GetType(),
-        .parcel = parcel_variable->name,
         .var = field->GetName(),
+        .parcel = parcel_variable->name,
         .is_return_value = false,
     };
     WriteToParcelFor(context);
@@ -219,8 +219,8 @@ std::unique_ptr<android::aidl::java::Class> generate_parcel_class(
         .writer = *(writer.get()),
         .typenames = typenames,
         .type = field->GetType(),
-        .parcel = parcel_variable->name,
         .var = field->GetName(),
+        .parcel = parcel_variable->name,
         .is_classloader_created = &is_classloader_created,
     };
     context.writer.Indent();
