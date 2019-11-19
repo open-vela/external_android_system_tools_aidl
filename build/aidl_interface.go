efdf325e86f7afec9f97ca11197afad4834227ca
openvela-robot <openvela-robot@xiaomi.com>
2019-11-19 15:09:28 +0800

    Support ParcelFileDescriptor[] in ndk am: cc8dd1d421
    Change-Id: Ie01386f5fce289cfe32b953d72cc874a77c1a016

--- build/aidl_interface.go
+++ build/aidl_interface.go
@@ -35,7 +35,6 @@ import (
 
 var (
 	aidlInterfaceSuffix = "_interface"
-	aidlApiDir          = "aidl_api"
 	aidlApiSuffix       = "-api"
 	langCpp             = "cpp"
 	langJava            = "java"
@@ -262,9 +261,6 @@ func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContex
 	if g.properties.Version != "" {
 		optionalFlags = append(optionalFlags, "--version "+g.properties.Version)
 	}
-	if g.properties.Stability != nil {
-		optionalFlags = append(optionalFlags, "--stability", *g.properties.Stability)
-	}
 
 	var headers android.WritablePaths
 	if g.properties.Lang == langJava {
@@ -305,6 +301,10 @@ func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContex
 
 		if g.properties.GenLog {
 			optionalFlags = append(optionalFlags, "--log")
+		}
+
+		if g.properties.Stability != nil {
+			optionalFlags = append(optionalFlags, "--stability", *g.properties.Stability)
 		}
 
 		aidlLang := g.properties.Lang
@@ -363,6 +363,7 @@ type aidlApiProperties struct {
 	BaseName string
 	Srcs     []string `android:"path"`
 	Imports  []string
+	Api_dir  *string
 	Versions []string
 }
 
@@ -379,7 +380,11 @@ type aidlApi struct {
 }
 
 func (m *aidlApi) apiDir() string {
-	return filepath.Join(aidlApiDir, m.properties.BaseName)
+	if m.properties.Api_dir != nil {
+		return *(m.properties.Api_dir)
+	} else {
+		return "api"
+	}
 }
 
 // Version of the interface at ToT if it is frozen
@@ -610,6 +615,9 @@ type aidlInterfaceProperties struct {
 	// Used by gen dependency to fill out aidl include path
 	Full_import_paths []string `blueprint:"mutated"`
 
+	// Directory where API dumps are. Default is "api".
+	Api_dir *string
+
 	// Stability promise. Currently only supports "vintf".
 	// If this is unset, this corresponds to an interface with stability within
 	// this compilation context (so an interface loaded here can only be used
@@ -701,6 +709,10 @@ func (i *aidlInterface) checkImports(mctx android.LoadHookContext) {
 func (i *aidlInterface) checkStability(mctx android.LoadHookContext) {
 	if i.properties.Stability == nil {
 		return
+	}
+
+	if i.shouldGenerateJavaBackend() {
+		mctx.PropertyErrorf("stability", "Java backend does not yet support stability.")
 	}
 
 	// TODO(b/136027762): should we allow more types of stability (e.g. for APEX) or
@@ -796,7 +808,13 @@ func (i *aidlInterface) srcsForVersion(mctx android.LoadHookContext, version str
 	if i.isCurrentVersion(mctx, version) {
 		return i.properties.Srcs, i.properties.Local_include_dir
 	} else {
-		aidlRoot = filepath.Join(aidlApiDir, i.ModuleBase.Name(), version)
+		var apiDir string
+		if i.properties.Api_dir != nil {
+			apiDir = *(i.properties.Api_dir)
+		} else {
+			apiDir = "api"
+		}
+		aidlRoot = filepath.Join(apiDir, version)
 		full_paths, err := mctx.GlobWithDeps(filepath.Join(mctx.ModuleDir(), aidlRoot, "**/*.aidl"), nil)
 		if err != nil {
 			panic(err)
@@ -1017,6 +1035,7 @@ func addApiModule(mctx android.LoadHookContext, i *aidlInterface) string {
 		BaseName: i.ModuleBase.Name(),
 		Srcs:     i.properties.Srcs,
 		Imports:  concat(i.properties.Imports, []string{i.ModuleBase.Name()}),
+		Api_dir:  i.properties.Api_dir,
 		Versions: i.properties.Versions,
 	})
 	return apiModule
