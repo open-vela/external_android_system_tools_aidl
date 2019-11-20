5e96ce67ace68db8c8cd1e6f20a86df50b1328fa
liujinye <liujinye@xiaomi.com>
2019-11-20 12:02:50 +0800

    Merge "Remove api_dir property"

--- build/aidl_interface.go
+++ build/aidl_interface.go
@@ -35,6 +35,7 @@ import (
 
 var (
 	aidlInterfaceSuffix = "_interface"
+	aidlApiDir          = "aidl_api"
 	aidlApiSuffix       = "-api"
 	langCpp             = "cpp"
 	langJava            = "java"
@@ -361,9 +362,7 @@ func aidlGenFactory() android.Module {
 type aidlApiProperties struct {
 	BaseName string
 	Srcs     []string `android:"path"`
-	AidlRoot string   // base directory for the input aidl file
 	Imports  []string
-	Api_dir  *string
 	Versions []string
 }
 
@@ -380,11 +379,7 @@ type aidlApi struct {
 }
 
 func (m *aidlApi) apiDir() string {
-	if m.properties.Api_dir != nil {
-		return *(m.properties.Api_dir)
-	} else {
-		return "api"
-	}
+	return filepath.Join(aidlApiDir, m.properties.BaseName)
 }
 
 // Version of the interface at ToT if it is frozen
@@ -421,13 +416,7 @@ func (m *aidlApi) createApiDumpFromSource(ctx android.ModuleContext) (apiDir and
 
 	apiDir = android.PathForModuleOut(ctx, "dump")
 	for _, src := range srcs {
-		baseDir := strings.TrimSuffix(src.String(), src.Rel())
-		if aidlRoot := android.PathForModuleSrc(ctx, m.properties.AidlRoot).String(); strings.HasPrefix(aidlRoot, baseDir) {
-			baseDir = aidlRoot
-		}
-		relPath, _ := filepath.Rel(baseDir, src.String())
-		outFile := android.PathForModuleOut(ctx, "dump", relPath)
-		apiFiles = append(apiFiles, outFile)
+		apiFiles = append(apiFiles, android.PathForModuleOut(ctx, "dump", src.Rel()))
 	}
 	hashFile = android.PathForModuleOut(ctx, "dump", ".hash")
 	latestVersion := "latest-version"
@@ -637,9 +626,6 @@ type aidlInterfaceProperties struct {
 	// Used by gen dependency to fill out aidl include path
 	Full_import_paths []string `blueprint:"mutated"`
 
-	// Directory where API dumps are. Default is "api".
-	Api_dir *string
-
 	// Stability promise. Currently only supports "vintf".
 	// If this is unset, this corresponds to an interface with stability within
 	// this compilation context (so an interface loaded here can only be used
@@ -819,13 +805,7 @@ func (i *aidlInterface) srcsForVersion(mctx android.LoadHookContext, version str
 	if i.isCurrentVersion(mctx, version) {
 		return i.properties.Srcs, i.properties.Local_include_dir
 	} else {
-		var apiDir string
-		if i.properties.Api_dir != nil {
-			apiDir = *(i.properties.Api_dir)
-		} else {
-			apiDir = "api"
-		}
-		aidlRoot = filepath.Join(apiDir, version)
+		aidlRoot = filepath.Join(aidlApiDir, i.ModuleBase.Name(), version)
 		full_paths, err := mctx.GlobWithDeps(filepath.Join(mctx.ModuleDir(), aidlRoot, "**/*.aidl"), nil)
 		if err != nil {
 			panic(err)
@@ -1042,15 +1022,12 @@ func addJavaLibrary(mctx android.LoadHookContext, i *aidlInterface, version stri
 
 func addApiModule(mctx android.LoadHookContext, i *aidlInterface) string {
 	apiModule := i.ModuleBase.Name() + aidlApiSuffix
-	srcs, aidlRoot := i.srcsForVersion(mctx, i.currentVersion(mctx))
 	mctx.CreateModule(aidlApiFactory, &nameProperties{
 		Name: proptools.StringPtr(apiModule),
 	}, &aidlApiProperties{
 		BaseName: i.ModuleBase.Name(),
-		Srcs:     srcs,
-		AidlRoot: aidlRoot,
+		Srcs:     i.properties.Srcs,
 		Imports:  concat(i.properties.Imports, []string{i.ModuleBase.Name()}),
-		Api_dir:  i.properties.Api_dir,
 		Versions: i.properties.Versions,
 	})
 	return apiModule
