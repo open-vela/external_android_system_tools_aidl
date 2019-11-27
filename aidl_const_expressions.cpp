c2da0a457588db8830277dd828a25935002a5897
liujinye <liujinye@xiaomi.com>
2019-11-27 10:12:59 +0800

    Also detect 'x%0' to avoid exception. am: 485a51a75f
    Change-Id: I4be55f93234b7d6391ac742b705351a157689c27

--- aidl_const_expressions.cpp
+++ aidl_const_expressions.cpp
@@ -314,15 +314,11 @@ AidlConstantValue* AidlConstantValue::ShallowIntegralCopy(const AidlConstantValu
     AIDL_FATAL(other) << "Invalid value for ShallowIntegralCopy";
   }
   if (!other.evaluate(type)) {
-    AIDL_ERROR(other) << "Failed to parse expression as integer: " << other.value_;
-    return nullptr;
-  }
-  const std::string& value = other.ValueString(type, AidlConstantValueDecorator);
-  if (value.empty()) {
-    return nullptr;  // error already logged
+    AIDL_FATAL(other) << "Unsupported type for ShallowIntegralCopy: " << ToString(other.GetType());
   }
 
-  AidlConstantValue* result = Integral(AIDL_LOCATION_HERE, value);
+  AidlConstantValue* result =
+      Integral(AIDL_LOCATION_HERE, other.ValueString(type, AidlConstantValueDecorator));
   if (result == nullptr) {
     AIDL_FATAL(other) << "Unable to perform ShallowIntegralCopy.";
   }
@@ -552,29 +548,27 @@ bool AidlConstantValue::evaluate(const AidlTypeSpecifier& type) const {
 
 string AidlConstantValue::ToString(Type type) {
   switch (type) {
+    case Type::ARRAY:
+      return "a literal array";
     case Type::BOOLEAN:
       return "a literal boolean";
+    case Type::CHARACTER:
+      return "a literal char";
     case Type::INT8:
       return "an int8 literal";
     case Type::INT32:
       return "an int32 literal";
     case Type::INT64:
       return "an int64 literal";
-    case Type::ARRAY:
-      return "a literal array";
-    case Type::CHARACTER:
-      return "a literal char";
     case Type::STRING:
       return "a literal string";
-    case Type::FLOATING:
-      return "a literal float";
+    case Type::ERROR:
+      LOG(FATAL) << "aidl internal error: error type failed to halt program";
+      return "";
     case Type::UNARY:
       return "a unary expression";
     case Type::BINARY:
       return "a binary expression";
-    case Type::ERROR:
-      LOG(FATAL) << "aidl internal error: error type failed to halt program";
-      return "";
     default:
       LOG(FATAL) << "aidl internal error: unknown constant type: " << static_cast<int>(type);
       return "";  // not reached
