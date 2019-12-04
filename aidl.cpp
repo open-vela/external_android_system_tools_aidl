2dcd42033651db94fce714be54c1539e7f5a7001
liujinye <liujinye@xiaomi.com>
2019-12-05 01:19:34 +0800

    Don't emit output file name in dep file for parcelable delcaration am: 795ec97fef am: bd505d8097
    Change-Id: I7cab15f7d428e12bb81640a575da1e8159057ce7

--- aidl.cpp
+++ aidl.cpp
@@ -776,13 +776,8 @@ int compile_aidl(const Options& options, const IoDelegate& io_delegate) {
                          io_delegate);
         success = true;
       } else if (lang == Options::Language::JAVA) {
-        if (defined_type->AsUnstructuredParcelable() != nullptr) {
-          // Legacy behavior. For parcelable declarations in Java, don't generate output file.
-          success = true;
-        } else {
-          success =
-              java::generate_java(output_file_name, defined_type, &java_types, io_delegate, options);
-        }
+        success =
+            java::generate_java(output_file_name, defined_type, &java_types, io_delegate, options);
       } else {
         LOG(FATAL) << "Should not reach here" << endl;
         return 1;
