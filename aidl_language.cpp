471025c0494a759cc34d392e8114e529be14a975
liujinye <liujinye@xiaomi.com>
2020-01-09 17:05:51 +0800

    Combine main.cpp/main_cpp.cpp. am: bb654e3195 am: dd431c0f80
    Change-Id: I75d3249545744298525c019c45b2624a9ea63e98

--- aidl_language.cpp
+++ aidl_language.cpp
@@ -109,12 +109,8 @@ std::string AidlNode::PrintLocation() const {
 }
 
 AidlError::AidlError(bool fatal) : os_(std::cerr), fatal_(fatal) {
-  sHadError = true;
-
   os_ << "ERROR: ";
 }
-
-bool AidlError::sHadError = false;
 
 static const string kNullable("nullable");
 static const string kUtf8InCpp("utf8InCpp");
