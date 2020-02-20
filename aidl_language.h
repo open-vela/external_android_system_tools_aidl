db80a9068dff89714a132f7f739aaab68513fe3e
openvela-robot <openvela-robot@xiaomi.com>
2020-02-20 18:10:42 +0800

    Annotations for defined types in API dumps.
    These already existed for enum types (because of @Backing), but we were
    missing them for interface/parcelables where @VintfStability is needed.
    
    Bug: 147496715
    Test: m android.hardware.vibrator-freeze-api (and API compiles)
    Change-Id: I15be062490fe451d1cdb368eaa2d2e92b6fcbd1b
    (cherry picked from commit 7e4b9509e969f0216d107a268c5182aa8b9e44bc)
    Merged-In: I15be062490fe451d1cdb368eaa2d2e92b6fcbd1b

--- aidl_language.h
+++ aidl_language.h
@@ -691,7 +691,6 @@ class AidlDefinedType : public AidlAnnotatable {
   }
 
   virtual void Dump(CodeWriter* writer) const = 0;
-  void DumpHeader(CodeWriter* writer) const;
 
  private:
   std::string name_;
