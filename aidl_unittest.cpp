9edda109ca6699bc007ef5425a4ca64fb72d334a
liujinye <liujinye@xiaomi.com>
2020-02-21 16:01:09 +0800

    Annotations/@hide consistent across defined types.
    @hide was missing for enum.
    
    Probably, we should have something like 'AidlCommentable' to unify
    duplicated logic surrounding having comments (and whether they are
    hidden).
    
    Bug: 152655547
    Test: m
    Merged-In: I85c1976e5781f86c99cac610ab4e7b0950d46279
    (cherry picked from commit a5d9c5c80348494fff4058c73b2de1665ea9955c)
    Change-Id: I85c1976e5781f86c99cac610ab4e7b0950d46279

--- aidl_unittest.cpp
+++ aidl_unittest.cpp
@@ -149,6 +149,21 @@ public class Rect implements android.os.Parcelable
 
 class AidlTest : public ::testing::Test {
  protected:
+  void SetUp() override {
+    CaptureStderr();
+  }
+
+  void TearDown() override {
+    auto actual_stderr = GetCapturedStderr();
+    std::cerr << actual_stderr << std::endl;
+
+    if (expected_stderr_.size() > 0) {
+      EXPECT_EQ(android::base::Join(expected_stderr_, ""), actual_stderr);
+    }
+  }
+
+  void AddExpectedStderr(string expected) { expected_stderr_.push_back(expected); }
+
   AidlDefinedType* Parse(const string& path, const string& contents, AidlTypenames& typenames_,
                          Options::Language lang, AidlError* error = nullptr,
                          const vector<string> additional_arguments = {}) {
@@ -192,6 +207,7 @@ class AidlTest : public ::testing::Test {
   FakeIoDelegate io_delegate_;
   vector<string> preprocessed_files_;
   set<string> import_paths_;
+  vector<string> expected_stderr_;
   AidlTypenames typenames_;
 };
 
@@ -223,9 +239,10 @@ TEST_F(AidlTest, RejectsArraysOfBinders) {
 }
 
 TEST_F(AidlTest, SupportOnlyOutParameters) {
-  string interface_list = "package a; interface IBar { void f(out List<String> bar); }";
+  string interface_list = "package a; interface IBar { void f(out List bar); }";
   string interface_ibinder = "package a; interface IBaz { void f(out IBinder bar); }";
-  EXPECT_NE(nullptr, Parse("a/IBar.aidl", interface_list, typenames_, Options::Language::CPP));
+  // List without type isn't supported in cpp.
+  EXPECT_EQ(nullptr, Parse("a/IBar.aidl", interface_list, typenames_, Options::Language::CPP));
   typenames_.Reset();
   EXPECT_NE(nullptr, Parse("a/IBar.aidl", interface_list, typenames_, Options::Language::JAVA));
   typenames_.Reset();
@@ -276,78 +293,6 @@ TEST_F(AidlTest, RejectsDuplicatedAnnotationParams) {
   EXPECT_EQ(nullptr, Parse("a/IFoo.aidl", method, typenames_, Options::Language::JAVA));
 }
 
-TEST_F(AidlTest, RejectUnsupportedInterfaceAnnotations) {
-  AidlError error = AidlError::OK;
-  string method = "package a; @nullable interface IFoo { int f(); }";
-  string expected_stderr =
-      "ERROR: a/IFoo.aidl:1.21-31: 'nullable' is not a supported annotation for this node. "
-      "It must be one of: Hide, UnsupportedAppUsage, VintfStability\n";
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/IFoo.aidl", method, typenames_, Options::Language::CPP, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-  typenames_.Reset();
-
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/IFoo.aidl", method, typenames_, Options::Language::JAVA, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-}
-
-TEST_F(AidlTest, RejectUnsupportedTypeAnnotations) {
-  AidlError error = AidlError::OK;
-  string method = "package a; interface IFoo { @JavaOnlyStableParcelable int f(); }";
-  string expected_stderr =
-      "ERROR: a/IFoo.aidl:1.54-58: 'JavaOnlyStableParcelable' is not a supported annotation "
-      "for this node. It must be one of: Hide, UnsupportedAppUsage, nullable, utf8InCpp\n";
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/IFoo.aidl", method, typenames_, Options::Language::CPP, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-  typenames_.Reset();
-
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/IFoo.aidl", method, typenames_, Options::Language::JAVA, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-}
-
-TEST_F(AidlTest, RejectUnsupportedParcelableAnnotations) {
-  AidlError error = AidlError::OK;
-  string method = "package a; @nullable parcelable IFoo cpp_header \"IFoo.h\";";
-  string expected_stderr =
-      "ERROR: a/Foo.aidl:1.32-37: 'nullable' is not a supported annotation for this node. "
-      "It must be one of: Hide, JavaOnlyStableParcelable, UnsupportedAppUsage, VintfStability\n";
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/Foo.aidl", method, typenames_, Options::Language::CPP, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-  typenames_.Reset();
-
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/Foo.aidl", method, typenames_, Options::Language::JAVA, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-}
-
-TEST_F(AidlTest, RejectUnsupportedParcelableDefineAnnotations) {
-  AidlError error = AidlError::OK;
-  string method = "package a; @nullable parcelable Foo { String a; String b; }";
-  string expected_stderr =
-      "ERROR: a/Foo.aidl:1.32-36: 'nullable' is not a supported annotation for this node. "
-      "It must be one of: Hide, UnsupportedAppUsage, VintfStability\n";
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/Foo.aidl", method, typenames_, Options::Language::CPP, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-  typenames_.Reset();
-
-  CaptureStderr();
-  EXPECT_EQ(nullptr, Parse("a/Foo.aidl", method, typenames_, Options::Language::JAVA, &error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
-  EXPECT_EQ(AidlError::BAD_TYPE, error);
-}
-
 TEST_F(AidlTest, ParsesNullableAnnotation) {
   for (auto is_nullable: {true, false}) {
     auto parse_result = Parse("a/IFoo.aidl",
@@ -380,28 +325,16 @@ TEST_F(AidlTest, ParsesUtf8Annotations) {
 
 TEST_F(AidlTest, VintfRequiresStructuredAndStability) {
   AidlError error;
-  const string expected_stderr =
-      "ERROR: IFoo.aidl:1.16-26: Must compile @VintfStability type w/ aidl_interface 'stability: "
-      "\"vintf\"'\n"
-      "ERROR: IFoo.aidl:1.16-26: Must compile @VintfStability type w/ aidl_interface "
-      "--structured\n";
-  CaptureStderr();
   auto parse_result = Parse("IFoo.aidl", "@VintfStability interface IFoo {}", typenames_,
                             Options::Language::CPP, &error);
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
   ASSERT_EQ(AidlError::NOT_STRUCTURED, error);
   ASSERT_EQ(nullptr, parse_result);
 }
 
 TEST_F(AidlTest, VintfRequiresStructured) {
   AidlError error;
-  const string expected_stderr =
-      "ERROR: IFoo.aidl:1.16-26: Must compile @VintfStability type w/ aidl_interface "
-      "--structured\n";
-  CaptureStderr();
   auto parse_result = Parse("IFoo.aidl", "@VintfStability interface IFoo {}", typenames_,
                             Options::Language::CPP, &error, {"--stability", "vintf"});
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
   ASSERT_EQ(AidlError::NOT_STRUCTURED, error);
   ASSERT_EQ(nullptr, parse_result);
 }
@@ -438,12 +371,10 @@ TEST_F(AidlTest, ParsesJavaOnlyStableParcelable) {
 
   EXPECT_EQ(0, ::android::aidl::compile_aidl(java_options, io_delegate_));
   EXPECT_EQ(0, ::android::aidl::compile_aidl(cpp_options, io_delegate_));
-  string expected_stderr =
+  AddExpectedStderr(
       "ERROR: a/Foo.aidl:1.48-52: Cannot declared parcelable in a --structured interface. "
-      "Parcelable must be defined in AIDL directly.\n";
-  CaptureStderr();
+      "Parcelable must be defined in AIDL directly.\n");
   EXPECT_NE(0, ::android::aidl::compile_aidl(cpp_structured_options, io_delegate_));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
 }
 
 TEST_F(AidlTest, AcceptsOneway) {
@@ -657,8 +588,7 @@ TEST_F(AidlTest, FailOnDuplicateConstantNames) {
 
 TEST_F(AidlTest, FailOnManyDefinedTypes) {
   AidlError reported_error;
-  string expected_stderr = "ERROR: p/IFoo.aidl: You must declare only one type per a file.\n";
-  CaptureStderr();
+  AddExpectedStderr("ERROR: p/IFoo.aidl: You must declare only one type per a file.\n");
   EXPECT_EQ(nullptr, Parse("p/IFoo.aidl",
                            R"(package p;
                       interface IFoo {}
@@ -667,7 +597,6 @@ TEST_F(AidlTest, FailOnManyDefinedTypes) {
                       interface IBaz {}
                   )",
                            typenames_, Options::Language::CPP, &reported_error));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
   // Parse success is important for clear error handling even if the cases aren't
   // actually supported in code generation.
   EXPECT_EQ(AidlError::BAD_TYPE, reported_error);
@@ -1386,7 +1315,6 @@ class AidlTestIncompatibleChanges : public AidlTest {
 };
 
 TEST_F(AidlTestIncompatibleChanges, RemovedType) {
-  const string expected_stderr = "ERROR: new: API files have been removed: old/p/IFoo.aidl\n";
   io_delegate_.SetFileContents("old/p/IFoo.aidl",
                                "package p;"
                                "interface IFoo {"
@@ -1394,9 +1322,7 @@ TEST_F(AidlTestIncompatibleChanges, RemovedType) {
                                "  void bar(@utf8InCpp String str);"
                                "}");
   io_delegate_.SetFileContents("new/p/IFoo.aidl", "");
-  CaptureStderr();
   EXPECT_FALSE(::android::aidl::check_api(options_, io_delegate_));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
 }
 
 TEST_F(AidlTestIncompatibleChanges, RemovedMethod) {
@@ -1541,7 +1467,6 @@ TEST_F(AidlTestIncompatibleChanges, ReorderedField) {
 }
 
 TEST_F(AidlTestIncompatibleChanges, ChangedDirectionSpecifier) {
-  const string expected_stderr = "ERROR: new/p/IFoo.aidl:1.33-37: Direction changed: in to out.\n";
   io_delegate_.SetFileContents("old/p/IFoo.aidl",
                                "package p;"
                                "interface IFoo {"
@@ -1554,9 +1479,7 @@ TEST_F(AidlTestIncompatibleChanges, ChangedDirectionSpecifier) {
                                "  void foo(out String[] str);"
                                "  void bar(@utf8InCpp String str);"
                                "}");
-  CaptureStderr();
   EXPECT_FALSE(::android::aidl::check_api(options_, io_delegate_));
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
 }
 
 TEST_F(AidlTestIncompatibleChanges, AddedAnnotation) {
@@ -1816,11 +1739,6 @@ TEST_F(AidlTest, FailOnOutOfBoundsInt64MinConstInt) {
 
 TEST_F(AidlTest, FailOnOutOfBoundsAutofilledEnum) {
   AidlError reported_error;
-  const string expected_stderr =
-      "ERROR: p/TestEnum.aidl:3.35-44: Invalid type specifier for an int32 "
-      "literal: byte\n"
-      "ERROR: p/TestEnum.aidl:5.1-36: Enumerator type differs from enum backing type.\n";
-  CaptureStderr();
   EXPECT_EQ(nullptr, Parse("p/TestEnum.aidl",
                            R"(package p;
                               @Backing(type="byte")
@@ -1831,7 +1749,6 @@ TEST_F(AidlTest, FailOnOutOfBoundsAutofilledEnum) {
                              )",
                            typenames_, Options::Language::CPP, &reported_error));
   EXPECT_EQ(AidlError::BAD_TYPE, reported_error);
-  EXPECT_EQ(expected_stderr, GetCapturedStderr());
 }
 
 }  // namespace aidl
