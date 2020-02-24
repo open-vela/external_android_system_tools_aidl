eb61d94139ec741d243bfc296b51bc0ba67ac597
liujinye <liujinye@xiaomi.com>
2020-02-24 12:02:10 +0800

    Limit VNDK entries to latest frozen version.
    Given 'aidl_interface { name: "foo" }'
    we generate (a) 'foo-V1-ndk_platform.so'
    
    Given 'aidl_interface { name: "foo", versions: ["1"] }'
    we generate
    (b) 'foo-V1-ndk_platform.so'
    (c) 'foo-V2-ndk_platform.so'
    
    We want the latest version to be in the VNDK. So (a) and (c) get put in
    the VNDK. In the (a) case, the VNDK list will be updated during the
    release. In the (c) case, when version 2 is added by freezing this
    release, the entry in the VNDK list will be updated.
    
    This is to reduce the total number of libraries in the VNDK (since
    having (b) and (c) would be too much). Ultimately, we might remove all
    of these stubs from the VNDK. However, for now, this is a hedge against
    the AIDL hashing infrastructure.
    
    Bug: 149960746
    Test: freezing android.hardware.vibrator (which has no versions) allows
        the build to succeed without having to modify the VNDK current.txt
        file.
    
    Change-Id: Ifbeafbbf86a8908c9be7a29e39889eed7c5a6213
    (cherry picked from commit f3a5cf4c76f49e25350aefef3507ea081241d371)
    Merged-In: Ifbeafbbf86a8908c9be7a29e39889eed7c5a6213

--- aidl_language.cpp
+++ aidl_language.cpp
@@ -128,6 +128,7 @@ static const string kNullable("nullable");
 static const string kUtf8InCpp("utf8InCpp");
 static const string kVintfStability("VintfStability");
 static const string kUnsupportedAppUsage("UnsupportedAppUsage");
+static const string kSystemApi("SystemApi");
 static const string kJavaStableParcelable("JavaOnlyStableParcelable");
 static const string kBacking("Backing");
 
@@ -141,6 +142,7 @@ static const std::map<string, std::map<std::string, std::string>> kAnnotationPar
       {"maxTargetSdk", "int"},
       {"publicAlternatives", "String"},
       {"trackingBug", "long"}}},
+    {kSystemApi, {}},
     {kJavaStableParcelable, {}},
     {kBacking, {{"type", "String"}}}};
 
@@ -296,6 +298,10 @@ const AidlTypeSpecifier* AidlAnnotatable::BackingType(const AidlTypenames& typen
     }
   }
   return nullptr;
+}
+
+bool AidlAnnotatable::IsSystemApi() const {
+  return HasAnnotation(annotations_, kSystemApi);
 }
 
 bool AidlAnnotatable::IsStableApiParcelable(Options::Language lang) const {
@@ -677,13 +683,6 @@ std::string AidlDefinedType::GetCanonicalName() const {
   return GetPackage() + "." + GetName();
 }
 
-void AidlDefinedType::DumpHeader(CodeWriter* writer) const {
-  if (this->IsHidden()) {
-    AddHideComment(writer);
-  }
-  DumpAnnotations(writer);
-}
-
 AidlParcelable::AidlParcelable(const AidlLocation& location, AidlQualifiedName* name,
                                const std::vector<std::string>& package, const std::string& comments,
                                const std::string& cpp_header, std::vector<std::string>* type_params)
@@ -747,7 +746,7 @@ bool AidlParcelable::CheckValid(const AidlTypenames&) const {
 }
 
 void AidlParcelable::Dump(CodeWriter* writer) const {
-  DumpHeader(writer);
+  DumpAnnotations(writer);
   writer->Write("parcelable %s ;\n", GetName().c_str());
 }
 
@@ -758,7 +757,10 @@ AidlStructuredParcelable::AidlStructuredParcelable(
       variables_(std::move(*variables)) {}
 
 void AidlStructuredParcelable::Dump(CodeWriter* writer) const {
-  DumpHeader(writer);
+  if (this->IsHidden()) {
+    AddHideComment(writer);
+  }
+  DumpAnnotations(writer);
   writer->Write("parcelable %s {\n", GetName().c_str());
   writer->Indent();
   for (const auto& field : GetFields()) {
@@ -933,7 +935,7 @@ bool AidlEnumDeclaration::CheckValid(const AidlTypenames&) const {
 }
 
 void AidlEnumDeclaration::Dump(CodeWriter* writer) const {
-  DumpHeader(writer);
+  DumpAnnotations(writer);
   writer->Write("enum %s {\n", GetName().c_str());
   writer->Indent();
   for (const auto& enumerator : GetEnumerators()) {
@@ -985,7 +987,10 @@ AidlInterface::AidlInterface(const AidlLocation& location, const std::string& na
 }
 
 void AidlInterface::Dump(CodeWriter* writer) const {
-  DumpHeader(writer);
+  if (this->IsHidden()) {
+    AddHideComment(writer);
+  }
+  DumpAnnotations(writer);
   writer->Write("interface %s {\n", GetName().c_str());
   writer->Indent();
   for (const auto& method : GetMethods()) {
