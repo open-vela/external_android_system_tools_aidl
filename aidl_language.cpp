3b7894fad9953440c0f012fb904d2bb00163c464
openvela-robot <openvela-robot@xiaomi.com>
2020-02-26 16:58:09 +0800

    [automerger skipped] Merge "Use std::optional for nullable for C++ backend" into rvc-dev-plus-aosp am: f11a1e0dba am: a2da4f27ad am: 1f58844e33 -s ours am skip reason: Change-Id I3ba30c4cdd1f9ef14ed8ae15bae2bae53325c620 with SHA-1 733815146f is in history
    Change-Id: Ie25ea376dab1a381567c887d1d340967c889dd37

--- aidl_language.cpp
+++ aidl_language.cpp
@@ -677,6 +677,13 @@ std::string AidlDefinedType::GetCanonicalName() const {
   return GetPackage() + "." + GetName();
 }
 
+void AidlDefinedType::DumpHeader(CodeWriter* writer) const {
+  if (this->IsHidden()) {
+    AddHideComment(writer);
+  }
+  DumpAnnotations(writer);
+}
+
 AidlParcelable::AidlParcelable(const AidlLocation& location, AidlQualifiedName* name,
                                const std::vector<std::string>& package, const std::string& comments,
                                const std::string& cpp_header, std::vector<std::string>* type_params)
@@ -740,7 +747,7 @@ bool AidlParcelable::CheckValid(const AidlTypenames&) const {
 }
 
 void AidlParcelable::Dump(CodeWriter* writer) const {
-  DumpAnnotations(writer);
+  DumpHeader(writer);
   writer->Write("parcelable %s ;\n", GetName().c_str());
 }
 
@@ -751,10 +758,7 @@ AidlStructuredParcelable::AidlStructuredParcelable(
       variables_(std::move(*variables)) {}
 
 void AidlStructuredParcelable::Dump(CodeWriter* writer) const {
-  if (this->IsHidden()) {
-    AddHideComment(writer);
-  }
-  DumpAnnotations(writer);
+  DumpHeader(writer);
   writer->Write("parcelable %s {\n", GetName().c_str());
   writer->Indent();
   for (const auto& field : GetFields()) {
@@ -929,7 +933,7 @@ bool AidlEnumDeclaration::CheckValid(const AidlTypenames&) const {
 }
 
 void AidlEnumDeclaration::Dump(CodeWriter* writer) const {
-  DumpAnnotations(writer);
+  DumpHeader(writer);
   writer->Write("enum %s {\n", GetName().c_str());
   writer->Indent();
   for (const auto& enumerator : GetEnumerators()) {
@@ -981,10 +985,7 @@ AidlInterface::AidlInterface(const AidlLocation& location, const std::string& na
 }
 
 void AidlInterface::Dump(CodeWriter* writer) const {
-  if (this->IsHidden()) {
-    AddHideComment(writer);
-  }
-  DumpAnnotations(writer);
+  DumpHeader(writer);
   writer->Write("interface %s {\n", GetName().c_str());
   writer->Indent();
   for (const auto& method : GetMethods()) {
