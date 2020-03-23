c9ad176c077cfba74792904df97f4c4309a7771a
openvela-robot <openvela-robot@xiaomi.com>
2020-03-23 11:21:52 +0800

    Add API dump for the current version am: 4fc9d866e3 am: 7bf68ab5bf
    Change-Id: I0dbaabc58ab496b841ab58b21a0ffca402324994

--- tests/lazy_test/server.cpp
+++ tests/lazy_test/server.cpp
@@ -3,7 +3,6 @@
 #include <binder/IServiceManager.h>
 #include <binder/LazyServiceRegistrar.h>
 #include <utils/Log.h>
-#include "LazyTestService.h"
 
 using android::BBinder;
 using android::IBinder;
@@ -11,15 +10,14 @@ using android::IPCThreadState;
 using android::OK;
 using android::sp;
 using android::binder::LazyServiceRegistrar;
-using android::binder::LazyTestService;
 
 int main() {
-  sp<LazyTestService> service1 = new LazyTestService();
-  sp<LazyTestService> service2 = new LazyTestService();
+  sp<IBinder> binder1 = new BBinder;
+  sp<IBinder> binder2 = new BBinder;
 
   auto lazyRegistrar = LazyServiceRegistrar::getInstance();
-  LOG_ALWAYS_FATAL_IF(OK != lazyRegistrar.registerService(service1, "aidl_lazy_test_1"), "");
-  LOG_ALWAYS_FATAL_IF(OK != lazyRegistrar.registerService(service2, "aidl_lazy_test_2"), "");
+  LOG_ALWAYS_FATAL_IF(OK != lazyRegistrar.registerService(binder1, "aidl_lazy_test_1"), "");
+  LOG_ALWAYS_FATAL_IF(OK != lazyRegistrar.registerService(binder2, "aidl_lazy_test_2"), "");
 
   IPCThreadState::self()->joinThreadPool();
 
