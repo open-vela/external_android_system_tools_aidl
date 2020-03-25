15a284e2b78cebf50c0fce496c3c1fc5df3a32e6
openvela-robot <openvela-robot@xiaomi.com>
2020-03-25 17:38:24 +0800

    Change SupportOnlyOutParameters unit test am: 8164d993a1 am: a765f386d8
    Change-Id: I6b8537edcfd4f2f4a9a3e22c8e06011ff37176ff

--- aidl_language.cpp
+++ aidl_language.cpp
@@ -824,10 +824,9 @@ bool AidlTypeSpecifier::LanguageSpecificCheckValid(Options::Language lang) const
         return false;
       }
       if (lang == Options::Language::CPP) {
-        const string& contained_type = this->GetTypeParameters()[0]->GetName();
-        if (!(contained_type == "String" || contained_type == "IBinder")) {
-          AIDL_ERROR(this) << "List<" << contained_type
-                           << "> is not supported. List in cpp supports only String and IBinder.";
+        auto& name = this->GetTypeParameters()[0]->GetName();
+        if (!(name == "String" || name == "IBinder")) {
+          AIDL_ERROR(this) << "List in cpp supports only string and IBinder for now.";
           return false;
         }
       } else if (lang == Options::Language::JAVA) {
@@ -835,9 +834,7 @@ bool AidlTypeSpecifier::LanguageSpecificCheckValid(Options::Language lang) const
         if (AidlTypenames::IsBuiltinTypename(contained_type)) {
           if (contained_type != "String" && contained_type != "IBinder" &&
               contained_type != "ParcelFileDescriptor") {
-            AIDL_ERROR(this) << "List<" << contained_type
-                             << "> is not supported. List in Java supports only String, IBinder, "
-                                "and ParcelFileDescriptor.";
+            AIDL_ERROR(this) << "List<" << contained_type << "> isn't supported in Java";
             return false;
           }
         }
