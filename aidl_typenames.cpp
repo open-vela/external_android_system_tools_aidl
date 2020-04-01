3d046ebe25ab5ef45e642d71c8d9a371bf4adb2a
openvela-robot <openvela-robot@xiaomi.com>
2020-04-01 13:26:00 +0800

    restrict unstable aidl interface
    1. PLATFORM_VERSION_CODENAME is REL
    2. AIDL interface with versions
    3. Owner field is empty
    
    In these cases, check every linkage with unstable aidl interface except test modules.
    
    Test: m nothing when PLATFORM_VERSION_CODENAME is REL
    Bug: 133526962
    Change-Id: I558aa024ace1e43c2373bc81232f808b180349bd
    Merged-In: I558aa024ace1e43c2373bc81232f808b180349bd

--- aidl_typenames.cpp
+++ aidl_typenames.cpp
@@ -64,30 +64,21 @@ static const map<string, string> kJavaLikeTypeToAidlType = {
 // in Java and C++. Using these names will eventually cause compilation error,
 // so checking this here is not a must have, but early detection of errors
 // is always better.
-static const set<string> kCppOrJavaReservedWord = {
+static const set<string> kInvalidNames = {
     "break",  "case",   "catch", "char",     "class",  "continue", "default",
     "do",     "double", "else",  "enum",     "false",  "float",    "for",
     "goto",   "if",     "int",   "long",     "new",    "private",  "protected",
     "public", "return", "short", "static",   "switch", "this",     "throw",
     "true",   "try",    "void",  "volatile", "while"};
 
-static bool HasValidNameComponents(const AidlDefinedType& defined) {
-  bool success = true;
-  vector<string> pieces = Split(defined.GetCanonicalName(), ".");
-  for (const string& piece : pieces) {
-    if (kCppOrJavaReservedWord.find(piece) != kCppOrJavaReservedWord.end()) {
-      AIDL_ERROR(defined) << defined.GetCanonicalName() << " is an invalid name because '" << piece
-                          << "' is a Java or C++ identifier.";
-      success = false;
-    }
-    // not checking kJavaLikeTypeToAidl, since that wouldn't make sense here
-    if (kBuiltinTypes.find(piece) != kBuiltinTypes.end()) {
-      AIDL_ERROR(defined) << defined.GetCanonicalName() << " is an invalid name because '" << piece
-                          << "' is a built-in AIDL type.";
-      success = false;
+static bool IsValidName(const string& name) {
+  vector<string> pieces = Split(name, ".");
+  for (const auto& piece : pieces) {
+    if (kInvalidNames.find(piece) != kInvalidNames.end()) {
+      return false;
     }
   }
-  return success;
+  return true;
 }
 
 bool AidlTypenames::IsIgnorableImport(const string& import) const {
@@ -108,7 +99,7 @@ bool AidlTypenames::AddDefinedType(unique_ptr<AidlDefinedType> type) {
   if (defined_types_.find(name) != defined_types_.end()) {
     return false;
   }
-  if (!HasValidNameComponents(*type)) {
+  if (!IsValidName(type->GetPackage()) || !IsValidName(type->GetName())) {
     return false;
   }
   defined_types_.emplace(name, std::move(type));
@@ -120,7 +111,7 @@ bool AidlTypenames::AddPreprocessedType(unique_ptr<AidlDefinedType> type) {
   if (preprocessed_types_.find(name) != preprocessed_types_.end()) {
     return false;
   }
-  if (!HasValidNameComponents(*type)) {
+  if (!IsValidName(type->GetPackage()) || !IsValidName(type->GetName())) {
     return false;
   }
   preprocessed_types_.insert(make_pair(name, std::move(type)));
@@ -170,19 +161,19 @@ AidlTypenames::DefinedImplResult AidlTypenames::TryGetDefinedTypeImpl(
   return DefinedImplResult(nullptr, false);
 }
 
-AidlTypenames::ResolvedTypename AidlTypenames::ResolveTypename(const string& type_name) const {
+pair<string, bool> AidlTypenames::ResolveTypename(const string& type_name) const {
   if (IsBuiltinTypename(type_name)) {
     auto found = kJavaLikeTypeToAidlType.find(type_name);
     if (found != kJavaLikeTypeToAidlType.end()) {
-      return {found->second, true};
+      return make_pair(found->second, true);
     }
-    return {type_name, true};
+    return make_pair(type_name, true);
   }
   const AidlDefinedType* defined_type = TryGetDefinedType(type_name);
   if (defined_type != nullptr) {
-    return {defined_type->GetCanonicalName(), true};
+    return make_pair(defined_type->GetCanonicalName(), true);
   } else {
-    return {type_name, false};
+    return make_pair(type_name, false);
   }
 }
 
