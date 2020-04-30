70ef3787d4314b8854b3725e12a81d1d6f1e899b
liujinye <liujinye@xiaomi.com>
2020-04-30 19:47:31 +0800

    [automerger skipped] Merge "Allow nullable after API dump." into rvc-dev am: 9c9a93cbec -s ours am: 53335fb958 -s ours am: 7b4fee532d -s ours am: 240342954b -s ours
    am skip reason: Change-Id I151ae0758dc1edcba99680a996706ce9b0a7ac58 with SHA-1 7b6a7d9c31 is in history
    
    Change-Id: If9fe9b4206e8875e6b96d6bff8b7c7587431489c

--- aidl_checkapi.cpp
+++ aidl_checkapi.cpp
@@ -46,12 +46,12 @@ static set<AidlAnnotation> get_strict_annotations(const AidlAnnotatable& node) {
   // - a new implementation might change so that it no longer returns null
   // values (remove @nullable)
   // - a new implementation might start accepting null values (add @nullable)
-  static const set<std::string> kIgnoreAnnotations{
-      "nullable",
+  static const set<AidlAnnotation::Type> kIgnoreAnnotations{
+      AidlAnnotation::Type::NULLABLE,
   };
   set<AidlAnnotation> annotations;
   for (const AidlAnnotation& annotation : node.GetAnnotations()) {
-    if (kIgnoreAnnotations.find(annotation.GetName()) == kIgnoreAnnotations.end()) {
+    if (kIgnoreAnnotations.find(annotation.GetType()) == kIgnoreAnnotations.end()) {
       annotations.insert(annotation);
     }
   }
@@ -266,7 +266,7 @@ bool check_api(const Options& options, const IoDelegate& io_delegate) {
   vector<AidlDefinedType*> new_types;
   vector<string> new_files = io_delegate.ListFiles(new_dir);
   if (new_files.size() == 0) {
-    AIDL_ERROR(new_dir) << "No API file exist";
+    AIDL_ERROR(new_dir) << "API files have been removed: " << android::base::Join(old_files, ", ");
     return false;
   }
   for (const auto& file : new_files) {
