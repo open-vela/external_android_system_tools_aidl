e0e37c466efbe1d1657dcb3ba1a42e7713e7fd1b
openvela-robot <openvela-robot@xiaomi.com>
2020-04-30 19:47:31 +0800

    [automerger skipped] Merge "Allow nullable after API dump." into rvc-dev am: 9c9a93cbec -s ours am: 53335fb958 -s ours am: 7b4fee532d -s ours am: 240342954b -s ours
    am skip reason: Change-Id I151ae0758dc1edcba99680a996706ce9b0a7ac58 with SHA-1 7b6a7d9c31 is in history
    
    Change-Id: If9fe9b4206e8875e6b96d6bff8b7c7587431489c

--- aidl_typenames.cpp
+++ aidl_typenames.cpp
@@ -64,21 +64,30 @@ static const map<string, string> kJavaLikeTypeToAidlType = {
 // in Java and C++. Using these names will eventually cause compilation error,
 // so checking this here is not a must have, but early detection of errors
 // is always better.
-static const set<string> kInvalidNames = {
+static const set<string> kCppOrJavaReservedWord = {
     "break",  "case",   "catch", "char",     "class",  "continue", "default",
     "do",     "double", "else",  "enum",     "false",  "float",    "for",
     "goto",   "if",     "int",   "long",     "new",    "private",  "protected",
     "public", "return", "short", "static",   "switch", "this",     "throw",
     "true",   "try",    "void",  "volatile", "while"};
 
-static bool IsValidName(const string& name) {
-  vector<string> pieces = Split(name, ".");
-  for (const auto& piece : pieces) {
-    if (kInvalidNames.find(piece) != kInvalidNames.end()) {
-      return false;
+static bool HasValidNameComponents(const AidlDefinedType& defined) {
+  bool success = true;
+  vector<string> pieces = Split(defined.GetCanonicalName(), ".");
+  for (const string& piece : pieces) {
+    if (kCppOrJavaReservedWord.find(piece) != kCppOrJavaReservedWord.end()) {
+      AIDL_ERROR(defined) << defined.GetCanonicalName() << " is an invalid name because '" << piece
+                          << "' is a Java or C++ identifier.";
+      success = false;
+    }
+    // not checking kJavaLikeTypeToAidl, since that wouldn't make sense here
+    if (kBuiltinTypes.find(piece) != kBuiltinTypes.end()) {
+      AIDL_ERROR(defined) << defined.GetCanonicalName() << " is an invalid name because '" << piece
+                          << "' is a built-in AIDL type.";
+      success = false;
     }
   }
-  return true;
+  return success;
 }
 
 bool AidlTypenames::IsIgnorableImport(const string& import) const {
@@ -99,7 +108,7 @@ bool AidlTypenames::AddDefinedType(unique_ptr<AidlDefinedType> type) {
   if (defined_types_.find(name) != defined_types_.end()) {
     return false;
   }
-  if (!IsValidName(type->GetPackage()) || !IsValidName(type->GetName())) {
+  if (!HasValidNameComponents(*type)) {
     return false;
   }
   defined_types_.emplace(name, std::move(type));
@@ -111,7 +120,7 @@ bool AidlTypenames::AddPreprocessedType(unique_ptr<AidlDefinedType> type) {
   if (preprocessed_types_.find(name) != preprocessed_types_.end()) {
     return false;
   }
-  if (!IsValidName(type->GetPackage()) || !IsValidName(type->GetName())) {
+  if (!HasValidNameComponents(*type)) {
     return false;
   }
   preprocessed_types_.insert(make_pair(name, std::move(type)));
