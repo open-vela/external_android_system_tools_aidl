daaf09d6c2208bbade4d830d242f5a56840acb76
liujinye <liujinye@xiaomi.com>
2020-05-26 18:01:13 +0800

    aidl_integration_test: add back run script am: 6d26c075b4 am: 316159a6cc
    Change-Id: Idca3f8a761d2aa86d755d12930b8453fd09a5164

---
+++ run_integration_tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+# TODO(b/156942077): when this test was converted to atest, we lost the ability
+# to see test results on stdout, and we can't print out the gtest results on
+# stderr because it breaks python test parsing. So, this file is added to help
+# run integration tests.
+
+$ANDROID_BUILD_TOP/build/soong/soong_ui.bash --make-mode \
+    MODULES-IN-system-tools-aidl
+
+adb root
+adb sync data
+adb install -r \
+    ${ANDROID_PRODUCT_OUT}/testcases/aidl_test_services/x86/aidl_test_services.apk
+
+${ANDROID_BUILD_TOP}/system/tools/aidl/tests/aidl_integration_test.py
