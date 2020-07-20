4052ad5bc57d915752796ffb317b0646328e01ab
Jiyong Park <jiyong@google.com>
2020-07-20 16:26:19 +0900

    @JavaDebug annotation for the generation of toString()
    When a structured parcelable is annotated with @JavaDebug, the generated
    Java class for the parcelable has toString() implementation that can be
    used for debugging purpose.
    
    Bug: 150161123
    Test: aidl_unittests
    Test: atest aidl_integration_test
    Merged-In: I5e7e911910b46c9c0e6bf09222225a6d2f57881f
    Change-Id: I5e7e911910b46c9c0e6bf09222225a6d2f57881f
    (cherry picked from commit 43113fb051f6d7e5ea7408549ad367dfc3a76c5b)

--- aidl_language.h
+++ aidl_language.h
@@ -244,6 +244,7 @@ class AidlAnnotatable : public AidlNode {
   bool IsVintfStability() const;
   bool IsStableApiParcelable(Options::Language lang) const;
   bool IsHide() const;
+  bool IsJavaDebug() const;
 
   void DumpAnnotations(CodeWriter* writer) const;
 
