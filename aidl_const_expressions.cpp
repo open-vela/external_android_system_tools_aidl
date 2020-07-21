2df72baba4f3937ca7a9d7dc642606d2da25c7e4
liujinye <liujinye@xiaomi.com>
2020-07-21 17:24:32 +0800

    Allow failure in const expression calculations. am: e1ff67e53d am: 70f0a53cc5 am: 9d1ed2955f am: d85b875377
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1364914
    
    Change-Id: I3d1aeeac2adc3709bbd34b937034dfe13b42e48e

--- aidl_const_expressions.cpp
+++ aidl_const_expressions.cpp
@@ -74,20 +74,13 @@ bool handleUnary(const AidlConstantValue& context, const string& op, T val, int6
   COMPUTE_UNARY(+)
   COMPUTE_UNARY(-)
   COMPUTE_UNARY(!)
-  COMPUTE_UNARY(~)
-  AIDL_FATAL(context) << "Could not handleUnary for " << op << " " << val;
-  return false;
-}
-template <>
-bool handleUnary<bool>(const AidlConstantValue& context, const string& op, bool val, int64_t* out) {
-  COMPUTE_UNARY(+)
-  COMPUTE_UNARY(-)
-  COMPUTE_UNARY(!)
 
-  if (op == "~") {
-    AIDL_ERROR(context) << "Bitwise negation of a boolean expression is always true.";
-    return false;
-  }
+// bitwise negation of a boolean expression always evaluates to 'true'
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wbool-operation"
+  COMPUTE_UNARY(~)
+#pragma clang diagnostic pop
+
   AIDL_FATAL(context) << "Could not handleUnary for " << op << " " << val;
   return false;
 }
