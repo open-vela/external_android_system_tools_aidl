15da10c750c9559624086c2aa215f5cdc192e08b
liujinye <liujinye@xiaomi.com>
2020-07-29 01:54:58 +0800

    Some native-side tests are re-writting using gtest am: 7c8a2e1ca0 am: 15dd4a3679 am: a3474777a8 am: c5e2328bda am: 4a6ff20732
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1374116
    
    Change-Id: Ieeaaf27e029d7d67122c811049e7a25aa7098009

--- tests/aidl_test_client.cpp
+++ tests/aidl_test_client.cpp
@@ -14,9 +14,32 @@
  * limitations under the License.
  */
 
-#include "gtest/gtest.h"
+#include "aidl_test_client.h"
+
+#include "aidl_test_client_defaultimpl.h"
+#include "aidl_test_client_file_descriptors.h"
+#include "aidl_test_client_nullables.h"
+#include "aidl_test_client_service_exceptions.h"
+
+#include <android-base/logging.h>
+
+// These tests were written without gtest, and not all have been converted to
+// gtest. Places which haven't been converted are still included as part of this
+// test here.
+TEST_F(AidlTest, UnconvertedTests) {
+  namespace client_tests = android::aidl::tests::client;
+
+  EXPECT_TRUE(client_tests::ConfirmFileDescriptors(service));
+  EXPECT_TRUE(client_tests::ConfirmFileDescriptorArrays(service));
+  EXPECT_TRUE(client_tests::ConfirmParcelFileDescriptors(service));
+  EXPECT_TRUE(client_tests::ConfirmParcelFileDescriptorArrays(service));
+  EXPECT_TRUE(client_tests::ConfirmServiceSpecificExceptions(service));
+  EXPECT_TRUE(client_tests::ConfirmNullables(service));
+  EXPECT_TRUE(client_tests::ConfirmDefaultImpl(service));
+}
 
 int main(int argc, char* argv[]) {
+  android::base::InitLogging(argv, android::base::StderrLogger);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
