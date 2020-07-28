1059902acd7c74d3e147d9ac2d52ddf4ba0dd305
liujinye <liujinye@xiaomi.com>
2020-07-28 09:57:44 +0800

    More test code cleanup am: 0f06528586 am: 1724863c97
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1373158
    
    Change-Id: I66f3ea2dabe2167b09c19b5a3fa716b573feb522

--- tests/aidl_integration_test.py
+++ tests/aidl_integration_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import pipes
-import re
 import subprocess
 import unittest
 
@@ -10,9 +9,6 @@ BITNESS_64 = ("64", "64")
 
 NATIVE_TEST_CLIENT_FOR_BITNESS = ' /data/nativetest%s/aidl_test_client/aidl_test_client%s'
 NATIVE_TEST_SERVICE_FOR_BITNESS = ' /data/nativetest%s/aidl_test_service/aidl_test_service%s'
-
-# From tools/base/ddmlib/src/main/java/com/android/ddmlib/testrunner/InstrumentationResultParser.java
-INSTRUMENTATION_FAILURES_PATTERN = r'There (was|were) \d+ failure'
 
 class TestFail(Exception):
     """Raised on test failures."""
@@ -131,8 +127,10 @@ class JavaClient:
                                'android.aidl.tests/'
                                'androidx.test.runner.AndroidJUnitRunner')
         print(result.printable_string())
-        if re.search(INSTRUMENTATION_FAILURES_PATTERN, result.stdout) is not None:
-            raise TestFail(result.stdout)
+        # The exit code of 'am instrument' is 0 even for test failure. We have
+        # to check for the existence of the well known error message.
+        if "FAILURES!!!" in result.printable_string():
+            raise TestFail('Java client did not complete successfully.')
 
 def supported_bitnesses(host):
     bitnesses = []
