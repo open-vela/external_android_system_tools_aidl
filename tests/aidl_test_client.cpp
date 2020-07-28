a0b5ddb9f8c123735d2a0b2f089aabacf8e9e0ae
liujinye <liujinye@xiaomi.com>
2020-07-28 22:32:08 +0800

    Remaining native tests are re-written using gtest
    Bug: 162211592
    Test: atest aidl_integration_test
    Change-Id: Ic3bcac91b6782cabb628dc0cf15e0e16b5da4561

--- tests/aidl_test_client.cpp
+++ tests/aidl_test_client.cpp
@@ -14,42 +14,9 @@
  * limitations under the License.
  */
 
-#include "aidl_test_client.h"
-
-#include "aidl_test_client_defaultimpl.h"
-#include "aidl_test_client_file_descriptors.h"
-#include "aidl_test_client_nullables.h"
-#include "aidl_test_client_primitives.h"
-#include "aidl_test_client_service_exceptions.h"
-#include "aidl_test_client_utf8_strings.h"
-
-#include <android-base/logging.h>
-
-// These tests were written without gtest, and not all have been converted to
-// gtest. Places which haven't been converted are still included as part of this
-// test here.
-TEST_F(AidlTest, UnconvertedTests) {
-  namespace client_tests = android::aidl::tests::client;
-
-  EXPECT_TRUE(client_tests::ConfirmPrimitiveRepeat(service));
-  EXPECT_TRUE(client_tests::ConfirmReverseArrays(service));
-  EXPECT_TRUE(client_tests::ConfirmReverseLists(service));
-  EXPECT_TRUE(client_tests::ConfirmReverseBinderLists(service));
-  EXPECT_TRUE(client_tests::ConfirmIntfConstantExpressions(service));
-  EXPECT_TRUE(client_tests::ConfirmFileDescriptors(service));
-  EXPECT_TRUE(client_tests::ConfirmFileDescriptorArrays(service));
-  EXPECT_TRUE(client_tests::ConfirmParcelFileDescriptors(service));
-  EXPECT_TRUE(client_tests::ConfirmParcelFileDescriptorArrays(service));
-  EXPECT_TRUE(client_tests::ConfirmServiceSpecificExceptions(service));
-  EXPECT_TRUE(client_tests::ConfirmNullables(service));
-  EXPECT_TRUE(client_tests::ConfirmUtf8InCppStringRepeat(service));
-  EXPECT_TRUE(client_tests::ConfirmUtf8InCppStringArrayReverse(service));
-  EXPECT_TRUE(client_tests::ConfirmUtf8InCppStringListReverse(service));
-  EXPECT_TRUE(client_tests::ConfirmDefaultImpl(service));
-}
+#include "gtest/gtest.h"
 
 int main(int argc, char* argv[]) {
-  android::base::InitLogging(argv, android::base::StderrLogger);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
