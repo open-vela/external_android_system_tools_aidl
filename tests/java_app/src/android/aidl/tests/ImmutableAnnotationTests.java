9b6a93351a67c3850eafb62b9b2f84e768a60dea
Jeongik Cha <jeongik@google.com>
2020-07-29 14:49:05 +0900

    Supplement tests for @Immutable parcelable
    Actual read/write a parcelable from/to a parcel test is missing.
    
    Test: atest aidl_integration_test aidl_unittests
    Bug: 161506914
    Change-Id: I5ed0ef69d6d51a4a338b620a09aeca52795f72fb

--- tests/java_app/src/android/aidl/tests/ImmutableAnnotationTests.java
+++ tests/java_app/src/android/aidl/tests/ImmutableAnnotationTests.java
@@ -16,22 +16,57 @@
 
 package android.aidl.tests;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import android.aidl.tests.immutable.Bar;
 import android.aidl.tests.immutable.Foo;
+import android.os.Parcel;
 import android.os.RemoteException;
 import java.lang.UnsupportedOperationException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class ImmutableAnnotationTests {
+  @Test
+  public void testReadWriteImmutableParcelable() throws RemoteException {
+    Parcel parcel = Parcel.obtain();
+    List<Bar> list = new ArrayList<Bar>();
+    list.add(new Bar("aa"));
+    Map<String, Bar> map = new HashMap<String, Bar>();
+    map.put("key", new Bar("value"));
+    Bar[] array = new Bar[3];
+    array[0] = new Bar("zero");
+    array[1] = new Bar("one");
+    array[2] = new Bar("two");
+
+    Foo foo = new Foo(7, new Bar("my"), list, map, array);
+    foo.writeToParcel(parcel, 0);
+    parcel.setDataPosition(0);
+    Foo foo2 = Foo.CREATOR.createFromParcel(parcel);
+
+    assertThat(foo.a, is(foo2.a));
+    assertThat(foo.b.s, is(foo2.b.s));
+    assertThat(foo.c.size(), is(foo.c.size()));
+    assertThat(foo.c.get(0).s, is(foo2.c.get(0).s));
+    assertThat(foo.c.size(), is(foo.c.size()));
+    assertThat(foo.d.get("key").s, is(foo2.d.get("key").s));
+    assertThat(foo.e.length, is(foo.e.length));
+
+    for (int i = 0; i < foo.e.length; i++) {
+      assertThat(foo.e[i].s, is(foo2.e[i].s));
+    }
+  }
+
   @Test
   public void testEveryFieldIsFinal() throws RemoteException {
     for (Field f : Foo.class.getDeclaredFields()) {
