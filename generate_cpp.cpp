5a532f2f1ba97b3bb2008b7326a79ee702680c49
liujinye <liujinye@xiaomi.com>
2020-09-17 04:04:45 +0800

    Allow INT64 type as constant value in generate functions am: e1eaa1afeb am: 0e8f7fe833 am: cce5a11fe3 am: c210652a04 am: a8c4670f25
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1427568
    
    Change-Id: I47428508d7879435db183d7560b6aac38b939e70

--- generate_cpp.cpp
+++ generate_cpp.cpp
@@ -1076,12 +1076,6 @@ std::unique_ptr<Document> BuildParcelHeader(const AidlTypenames& typenames,
     if (variable->GetDefaultValue()) {
       out << " = " << cppType.c_str() << "(" << variable->ValueString(ConstantValueDecorator)
           << ")";
-    } else if (variable->GetType().GetName() == "ParcelableHolder") {
-      if (parcel.IsVintfStability()) {
-        out << " { ::android::Parcelable::Stability::STABILITY_VINTF }";
-      } else {
-        out << " { ::android::Parcelable::Stability::STABILITY_LOCAL }";
-      }
     }
     out << ";\n";
 
@@ -1103,12 +1097,6 @@ std::unique_ptr<Document> BuildParcelHeader(const AidlTypenames& typenames,
       MethodDecl::IS_OVERRIDE | MethodDecl::IS_CONST | MethodDecl::IS_FINAL));
   parcel_class->AddPublic(std::move(write));
 
-  parcel_class->AddPublic(std::unique_ptr<LiteralDecl>(
-      new LiteralDecl(StringPrintf("static const std::string& getParcelableDescriptor() {\n"
-                                   "  static const std::string DESCIPTOR = \"%s\";\n"
-                                   "  return DESCIPTOR;\n"
-                                   "}\n",
-                                   parcel.GetCanonicalName().c_str()))));
   return unique_ptr<Document>{
       new CppHeader{vector<string>(includes.begin(), includes.end()),
                     NestInNamespaces(std::move(parcel_class), parcel.GetSplitPackage())}};
