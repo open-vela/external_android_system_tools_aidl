350c099f9f9e04601a64697e8067436ca43cb9ca
liujinye <liujinye@xiaomi.com>
2020-09-17 03:19:06 +0800

    Promote constant expression shift type based on both operands am: 04823021af am: 4869cdb090 am: 849c629d74 am: 09899156bc am: 758b771886
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1424893
    
    Change-Id: I5ada15ee34535e310276d311875278d961bd7ade

--- generate_rust.cpp
+++ generate_rust.cpp
@@ -417,9 +417,7 @@ bool GenerateRustInterface(const string& filename, const AidlInterface* iface,
       }
       case AidlConstantValue::Type::BOOLEAN:  // fall-through
       case AidlConstantValue::Type::INT8:     // fall-through
-      case AidlConstantValue::Type::INT32:    // fall-through
-      // Type promotion may cause this. Value should be small enough to fit in int32.
-      case AidlConstantValue::Type::INT64: {
+      case AidlConstantValue::Type::INT32: {
         const_type = "i32";
         break;
       }
