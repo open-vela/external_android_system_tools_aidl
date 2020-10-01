d7c75eda5a24cb7fc6df90b4a6a2b98d58fc5e92
openvela-robot <openvela-robot@xiaomi.com>
2020-10-01 17:09:06 +0800

    ndk: Emit aligment values for known types in @FixedSize parcelables am: 83e7698cd5 am: c9e7ed4047 am: e513be0070 am: 6743fe56b9
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1443121
    
    Change-Id: Ie3f19d413b0f4077fe9b4662a4ef2a3884a69abc

--- aidl_const_expressions.cpp
+++ aidl_const_expressions.cpp
@@ -846,9 +846,8 @@ bool AidlBinaryConstExpression::evaluate(const AidlTypeSpecifier& type) const {
 
   // Handle String case first
   if (left_val_->final_type_ == Type::STRING) {
-    AIDL_FATAL_IF(right_val_->final_type_ != Type::STRING, this);
     if (!OPEQ("+")) {
-      AIDL_ERROR(this) << "Only '+' is supported for strings, not '" << op_ << "'.";
+      // invalid operation on strings
       final_type_ = Type::ERROR;
       is_valid_ = false;
       return false;
