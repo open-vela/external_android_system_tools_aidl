7130fd82db70cd222b2c534ce7eaf9495d1478ad
liujinye <liujinye@xiaomi.com>
2020-10-13 06:23:55 +0800

    support multiple @JavaPassthrough am: e6bc5e1a66 am: 52346075a0 am: 3b591287f7 am: 04b0e9d56f
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1457258
    
    Change-Id: I314d6b5d31c974bf09f0422936d86546cf12421e

--- aidl.cpp
+++ aidl.cpp
@@ -424,11 +424,11 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
   if (main_parser == nullptr) {
     return AidlError::PARSE_ERROR;
   }
-  int num_top_level_decls = 0;
+  int num_interfaces_or_structured_parcelables = 0;
   for (const auto& type : main_parser->ParsedDocument().DefinedTypes()) {
-    if (type->AsUnstructuredParcelable() == nullptr) {
-      num_top_level_decls++;
-      if (num_top_level_decls > 1) {
+    if (type->AsInterface() != nullptr || type->AsStructuredParcelable() != nullptr) {
+      num_interfaces_or_structured_parcelables++;
+      if (num_interfaces_or_structured_parcelables > 1) {
         AIDL_ERROR(*type) << "You must declare only one type per file.";
         return AidlError::BAD_TYPE;
       }
@@ -578,10 +578,8 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
     AidlStructuredParcelable* parcelable = defined_type->AsStructuredParcelable();
     AidlParcelable* unstructured_parcelable = defined_type->AsUnstructuredParcelable();
     AidlEnumDeclaration* enum_decl = defined_type->AsEnumDeclaration();
-    AidlUnionDecl* union_decl = defined_type->AsUnionDeclaration();
-    AIDL_FATAL_IF(
-        !!interface + !!parcelable + !!unstructured_parcelable + !!enum_decl + !!union_decl != 1,
-        defined_type);
+    AIDL_FATAL_IF(!!interface + !!parcelable + !!unstructured_parcelable + !!enum_decl != 1,
+                  defined_type);
 
     // Ensure that foo.bar.IFoo is defined in <some_path>/foo/bar/IFoo.aidl
     if (num_defined_types == 1 && !check_filename(input_file_name, *defined_type)) {
