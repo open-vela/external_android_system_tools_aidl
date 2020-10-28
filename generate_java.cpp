add9d45f7a2625c08dc6911b9382df1d4b9d7836
openvela-robot <openvela-robot@xiaomi.com>
2020-10-28 10:01:21 +0800

    remove trailing space am: 53705f9318
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1477960
    
    Change-Id: I39d79d586ef37b0f6f3a16c58b4d711c388cb9e1

--- generate_java.cpp
+++ generate_java.cpp
@@ -44,11 +44,18 @@ namespace {
 using android::aidl::java::CodeGeneratorContext;
 using android::aidl::java::ConstantValueDecorator;
 
+// join two non-empty strings according to `camelCase` naming.
+inline string camelcase_join(const string& a, const string& b, const AidlNode& context) {
+  AIDL_FATAL_IF(b.size() <= 0 || a.size() <= 0, context) << "Name cannot be empty.";
+  std::string name = a + b;
+  name[a.size()] = static_cast<char>(toupper(name[a.size()]));
+  return name;
+}
 inline string getter_name(const AidlVariableDeclaration& variable) {
-  return "get" + variable.GetCapitalizedName();
+  return camelcase_join("get", variable.GetName(), variable);
 }
 inline string setter_name(const AidlVariableDeclaration& variable) {
-  return "set" + variable.GetCapitalizedName();
+  return camelcase_join("set", variable.GetName(), variable);
 }
 
 // clang-format off
