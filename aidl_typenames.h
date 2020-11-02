58bf0f422dd55110e0dd3181bf1ac79f350be9a6
liujinye <liujinye@xiaomi.com>
2020-11-03 04:06:02 +0800

    fix toString in the Java backend am: ee73cb33fb am: 201e9eaf1c am: 8f5560b0f9 am: 16527be5e1
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1483636
    
    Change-Id: I25341fd401514945498669d1337e84cac62dff2f

--- aidl_typenames.h
+++ aidl_typenames.h
@@ -69,7 +69,6 @@ class AidlTypenames final {
   struct ResolvedTypename {
     std::string canonical_name;
     bool is_resolved;
-    const AidlDefinedType* defined_type;
   };
   ResolvedTypename ResolveTypename(const string& type_name) const;
   pair<bool, string> CanBeOutParameter(const AidlTypeSpecifier& type) const;
@@ -98,7 +97,7 @@ class AidlTypenames final {
     const bool from_preprocessed;
   };
   DefinedImplResult TryGetDefinedTypeImpl(const string& type_name) const;
-  map<string, AidlDefinedType*> defined_types_;
+  map<string, const AidlDefinedType*> defined_types_;
   map<string, unique_ptr<AidlDefinedType>> preprocessed_types_;
   std::vector<std::unique_ptr<AidlDocument>> documents_;
 };
