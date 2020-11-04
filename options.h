e53d474ab635c815249c56ec4e48fd370d1263f0
liujinye <liujinye@xiaomi.com>
2020-11-04 10:30:28 +0800

    gen_log: prints parcelable/union/enum types am: b8e01b9a94 am: a25d53370b am: 47b9808646 am: bb0ada5725
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1483640
    
    Change-Id: I628c6f683581e7450641b175f8ea6033c26dd222

--- options.h
+++ options.h
@@ -125,6 +125,8 @@ class Options final {
 
   bool GenLog() const { return gen_log_; }
 
+  bool GenParcelableToString() const { return gen_parcelable_to_string_; }
+
   bool Ok() const { return error_message_.stream_.str().empty(); }
 
   string GetErrorMessage() const { return error_message_.stream_.str(); }
@@ -165,6 +167,7 @@ class Options final {
   int version_ = 0;
   string hash_ = "";
   bool gen_log_ = false;
+  bool gen_parcelable_to_string_ = false;
   ErrorMessage error_message_;
 };
 
