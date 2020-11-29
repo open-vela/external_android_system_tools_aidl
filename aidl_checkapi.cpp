991ac54071de2441b1435f3f31d75f1b2f9cb815
openvela-robot <openvela-robot@xiaomi.com>
2020-11-30 05:29:21 +0800

    switch ToString/Signature for TypeSpecifier am: 965e31def7 am: 5993f801fe am: 6a616c9e28
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1512750
    
    Change-Id: I374c95189abac8f3393a7abb02baa00bceccaf06

--- aidl_checkapi.cpp
+++ aidl_checkapi.cpp
@@ -32,7 +32,6 @@ namespace aidl {
 
 using android::base::Error;
 using android::base::Result;
-using android::base::StartsWith;
 using std::map;
 using std::set;
 using std::string;
@@ -62,15 +61,9 @@ static vector<string> get_strict_annotations(const AidlAnnotatable& node) {
   };
   vector<string> annotations;
   for (const AidlAnnotation& annotation : node.GetAnnotations()) {
-    if (kIgnoreAnnotations.find(annotation.GetType()) != kIgnoreAnnotations.end()) {
-      continue;
+    if (kIgnoreAnnotations.find(annotation.GetType()) == kIgnoreAnnotations.end()) {
+      annotations.push_back(annotation.ToString());
     }
-    auto annotation_string = annotation.ToString();
-    // adding @Deprecated (with optional args) is okay
-    if (StartsWith(annotation_string, "@JavaPassthrough(annotation=\"@Deprecated")) {
-      continue;
-    }
-    annotations.push_back(annotation_string);
   }
   return annotations;
 }
