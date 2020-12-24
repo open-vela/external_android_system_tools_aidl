daa1407d3bb405852bd88a22b3c239598315ea6a
openvela-robot <openvela-robot@xiaomi.com>
2020-12-25 00:58:25 +0800

    Add "required_parameters" to Annotation params am: 5721a23745
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1534577
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ibd1641b40065f2ad505ae887182d8ddc12795b2b

--- aidl_language.h
+++ aidl_language.h
@@ -256,7 +256,7 @@ class AidlAnnotatable : public AidlNode {
 
   const AidlAnnotation* UnsupportedAppUsage() const;
   const AidlAnnotation* RustDerive() const;
-  const AidlAnnotation* BackingType() const;
+  const AidlTypeSpecifier* BackingType(const AidlTypenames& typenames) const;
 
   // ToString is for dumping AIDL.
   // Returns string representation of annotations.
@@ -973,7 +973,7 @@ class AidlEnumDeclaration : public AidlDefinedType {
   AidlEnumDeclaration& operator=(const AidlEnumDeclaration&) = delete;
   AidlEnumDeclaration& operator=(AidlEnumDeclaration&&) = delete;
 
-  bool Autofill(const AidlTypenames&);
+  void SetBackingType(std::unique_ptr<const AidlTypeSpecifier> type);
   const AidlTypeSpecifier& GetBackingType() const { return *backing_type_; }
   const std::vector<std::unique_ptr<AidlEnumerator>>& GetEnumerators() const {
     return enumerators_;
@@ -990,10 +990,11 @@ class AidlEnumDeclaration : public AidlDefinedType {
   const AidlEnumDeclaration* AsEnumDeclaration() const override { return this; }
 
  private:
+  void Autofill();
 
   const std::string name_;
   const std::vector<std::unique_ptr<AidlEnumerator>> enumerators_;
-  std::unique_ptr<AidlTypeSpecifier> backing_type_;
+  std::unique_ptr<const AidlTypeSpecifier> backing_type_;
 };
 
 class AidlUnionDecl : public AidlParcelable {
