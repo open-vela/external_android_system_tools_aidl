fcaa929447ccd7016ccd924bd486416ed18133ca
openvela-robot <openvela-robot@xiaomi.com>
2020-12-31 01:56:05 +0800

    Support @SuppressWarnings am: d911332007
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1537882
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ib703d291cc5701aad49f960aed024badb77d7c1a

--- diagnostics.cpp
+++ diagnostics.cpp
@@ -15,7 +15,13 @@
  */
 #include "diagnostics.h"
 
+#include <functional>
+#include <stack>
+
+#include "aidl_language.h"
 #include "logging.h"
+
+using std::placeholders::_1;
 
 namespace android {
 namespace aidl {
@@ -40,13 +46,153 @@ DiagnosticSeverity DiagnosticMapping::Severity(DiagnosticID id) const {
   return mapping_.at(id);
 }
 
-AidlErrorLog DiagnosticsContext::Report(const AidlLocation& loc, DiagnosticID id) {
-  return DoReport(loc, id, mapping_.Severity(id));
-}
-
 std::string to_string(DiagnosticID id) {
   return kDiagnosticsNames.at(id);
 }
 
+class DiagnosticsContext {
+ public:
+  DiagnosticsContext(DiagnosticMapping mapping) : mapping_({std::move(mapping)}) {}
+  AidlErrorLog Report(const AidlLocation& loc, DiagnosticID id,
+                      DiagnosticSeverity force_severity = DiagnosticSeverity::DISABLED) {
+    const std::string suffix = " [-W" + to_string(id) + "]";
+    auto severity = std::max(force_severity, mapping_.top().Severity(id));
+    switch (severity) {
+      case DiagnosticSeverity::DISABLED:
+        return AidlErrorLog(AidlErrorLog::NO_OP, loc);
+      case DiagnosticSeverity::WARNING:
+        return AidlErrorLog(AidlErrorLog::WARNING, loc, suffix);
+      case DiagnosticSeverity::ERROR:
+        error_count_++;
+        return AidlErrorLog(AidlErrorLog::ERROR, loc, suffix);
+    }
+  }
+  size_t ErrorCount() const { return error_count_; }
+  void Suppress(const AidlAnnotatable& a) {
+    const auto& warnings = a.SuppressWarnings();
+    DiagnosticMapping new_mapping = mapping_.top();
+    for (const auto& w : warnings) {
+      auto it = kAllDiagnostics.find(w);
+      if (it == kAllDiagnostics.end()) {
+        Report(a.GetLocation(), DiagnosticID::unknown_warning, DiagnosticSeverity::ERROR)
+            << "unknown warning: " << w;
+        continue;
+      }
+      new_mapping.Severity(it->second.id, DiagnosticSeverity::DISABLED);
+    }
+    mapping_.push(std::move(new_mapping));
+  }
+  void Restore(const AidlAnnotatable&) {
+    mapping_.pop();
+  }
+ private:
+  std::stack<DiagnosticMapping> mapping_;
+  size_t error_count_ = {};
+};
+
+class DiagnosticsVisitor : public AidlVisitor {
+ public:
+  DiagnosticsVisitor(DiagnosticsContext& diag) : diag(diag) {}
+  void Check(const AidlDocument& doc) {
+    DiagnosticsVisitor* visitor = this;
+    using Fun = std::function<void(const AidlAnnotatable&)>;
+    struct Hook : public AidlVisitor {
+      Fun fun;
+      Hook(Fun fun) : fun(fun) {}
+      bool Visit(const AidlInterface& a) override { fun(a); return true; }
+      bool Visit(const AidlEnumDeclaration& a) override { fun(a); return true; }
+      bool Visit(const AidlStructuredParcelable& a) override { fun(a); return true; }
+      bool Visit(const AidlUnionDecl& a) override { fun(a); return true; }
+      bool Visit(const AidlParcelable& a) override { fun(a); return true; }
+      bool Visit(const AidlMethod& a) override { fun(a.GetType()); return true; }
+    };
+    Hook suppress{std::bind(&DiagnosticsContext::Suppress, &diag, _1)};
+    Hook restore{std::bind(&DiagnosticsContext::Restore, &diag, _1)};
+    std::function<void(const AidlTraversable&)> topDown =
+        [&topDown, &suppress, &restore, visitor](const AidlTraversable& a) {
+      a.DispatchVisit(suppress);
+      if (a.DispatchVisit(*visitor)) {
+        a.TraverseChildren(topDown);
+      }
+      a.DispatchVisit(restore);
+    };
+    topDown(doc);
+  }
+ protected:
+  DiagnosticsContext& diag;
+};
+
+struct DiagnoseInterfaceName : DiagnosticsVisitor {
+  DiagnoseInterfaceName(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
+  bool Visit(const AidlInterface& i) override {
+    if (auto name = i.GetName(); name.size() < 1 || name[0] != 'I') {
+      diag.Report(i.GetLocation(), DiagnosticID::interface_name)
+          << "Interface names should start with I.";
+    }
+    return true;
+  }
+};
+
+struct DiagnoseEnumZero : DiagnosticsVisitor {
+  DiagnoseEnumZero(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
+  bool Visit(const AidlEnumDeclaration& e) override {
+    AIDL_FATAL_IF(e.GetEnumerators().empty(), e)
+        << "The enum '" << e.GetName() << "' has no enumerators.";
+    const auto& first = e.GetEnumerators()[0];
+    if (auto first_value = first->ValueString(e.GetBackingType(), AidlConstantValueDecorator);
+        first_value != "0") {
+      diag.Report(first->GetLocation(), DiagnosticID::enum_zero)
+          << "The first enumerator '" << first->GetName() << "' should be 0, but it is "
+          << first_value << ".";
+    }
+    return true;
+  }
+};
+
+struct DiagnoseInoutParameter : DiagnosticsVisitor {
+  DiagnoseInoutParameter(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
+  bool Visit(const AidlArgument& a) override {
+    if (a.GetDirection() == AidlArgument::INOUT_DIR) {
+      diag.Report(a.GetLocation(), DiagnosticID::inout_parameter)
+          << a.GetName()
+          << " is 'inout'. Avoid inout parameters. This is somewhat confusing for clients "
+             "because although the parameters are 'in', they look out 'out' parameters.";
+    }
+    return true;
+  }
+};
+
+struct DiagnoseConstName : DiagnosticsVisitor {
+  DiagnoseConstName(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
+  bool Visit(const AidlEnumerator& e) override {
+    if (ToUpper(e.GetName()) != e.GetName()) {
+      diag.Report(e.GetLocation(), DiagnosticID::const_name)
+          << "Enum values should be named in upper cases: " << ToUpper(e.GetName());
+    }
+    return true;
+  }
+  bool Visit(const AidlConstantDeclaration& c) override {
+    if (ToUpper(c.GetName()) != c.GetName()) {
+      diag.Report(c.GetLocation(), DiagnosticID::const_name)
+          << "Constants should be named in upper cases: " << ToUpper(c.GetName());
+    }
+    return true;
+  }
+  static std::string ToUpper(std::string name) {
+    for (auto& c : name) c = std::toupper(c);
+    return name;
+  }
+};
+
+bool Diagnose(const AidlDocument& doc, const DiagnosticMapping& mapping) {
+  DiagnosticsContext diag(mapping);
+
+  DiagnoseInterfaceName{diag}.Check(doc);
+  DiagnoseEnumZero{diag}.Check(doc);
+  DiagnoseInoutParameter{diag}.Check(doc);
+  DiagnoseConstName{diag}.Check(doc);
+
+  return diag.ErrorCount() == 0;
+}
 }  // namespace aidl
 }  // namespace android
