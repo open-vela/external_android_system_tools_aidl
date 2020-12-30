23366b5495fda33e58886a26bd61ee8ebe626eb9
openvela-robot <openvela-robot@xiaomi.com>
2020-12-31 02:29:06 +0800

    Refactor AidlVisitor so that it no longer responsible for traversing am: 512ed85c1d am: 47aa266598 am: 736e9a6585
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1536388
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I7455c148ab473b0e8cee79959a8a955ed01cceab

--- diagnostics.cpp
+++ diagnostics.cpp
@@ -15,14 +15,7 @@
  */
 #include "diagnostics.h"
 
-#include <functional>
-#include <stack>
-#include <unordered_set>
-
-#include "aidl_language.h"
 #include "logging.h"
-
-using std::placeholders::_1;
 
 namespace android {
 namespace aidl {
@@ -47,194 +40,13 @@ DiagnosticSeverity DiagnosticMapping::Severity(DiagnosticID id) const {
   return mapping_.at(id);
 }
 
+AidlErrorLog DiagnosticsContext::Report(const AidlLocation& loc, DiagnosticID id) {
+  return DoReport(loc, id, mapping_.Severity(id));
+}
+
 std::string to_string(DiagnosticID id) {
   return kDiagnosticsNames.at(id);
 }
 
-class DiagnosticsContext {
- public:
-  DiagnosticsContext(DiagnosticMapping mapping) : mapping_({std::move(mapping)}) {}
-  AidlErrorLog Report(const AidlLocation& loc, DiagnosticID id,
-                      DiagnosticSeverity force_severity = DiagnosticSeverity::DISABLED) {
-    const std::string suffix = " [-W" + to_string(id) + "]";
-    auto severity = std::max(force_severity, mapping_.top().Severity(id));
-    switch (severity) {
-      case DiagnosticSeverity::DISABLED:
-        return AidlErrorLog(AidlErrorLog::NO_OP, loc);
-      case DiagnosticSeverity::WARNING:
-        return AidlErrorLog(AidlErrorLog::WARNING, loc, suffix);
-      case DiagnosticSeverity::ERROR:
-        error_count_++;
-        return AidlErrorLog(AidlErrorLog::ERROR, loc, suffix);
-    }
-  }
-  size_t ErrorCount() const { return error_count_; }
-  void Suppress(const AidlAnnotatable& a) {
-    const auto& warnings = a.SuppressWarnings();
-    DiagnosticMapping new_mapping = mapping_.top();
-    for (const auto& w : warnings) {
-      auto it = kAllDiagnostics.find(w);
-      if (it == kAllDiagnostics.end()) {
-        Report(a.GetLocation(), DiagnosticID::unknown_warning, DiagnosticSeverity::ERROR)
-            << "unknown warning: " << w;
-        continue;
-      }
-      new_mapping.Severity(it->second.id, DiagnosticSeverity::DISABLED);
-    }
-    mapping_.push(std::move(new_mapping));
-  }
-  void Restore(const AidlAnnotatable&) {
-    mapping_.pop();
-  }
- private:
-  std::stack<DiagnosticMapping> mapping_;
-  size_t error_count_ = {};
-};
-
-class DiagnosticsVisitor : public AidlVisitor {
- public:
-  DiagnosticsVisitor(DiagnosticsContext& diag) : diag(diag) {}
-  void Check(const AidlDocument& doc) {
-    DiagnosticsVisitor* visitor = this;
-    using Fun = std::function<void(const AidlAnnotatable&)>;
-    struct Hook : public AidlVisitor {
-      Fun fun;
-      Hook(Fun fun) : fun(fun) {}
-      void Visit(const AidlInterface& a) override { fun(a); }
-      void Visit(const AidlEnumDeclaration& a) override { fun(a); }
-      void Visit(const AidlStructuredParcelable& a) override { fun(a); }
-      void Visit(const AidlUnionDecl& a) override { fun(a); }
-      void Visit(const AidlParcelable& a) override { fun(a); }
-      void Visit(const AidlMethod& a) override { fun(a.GetType()); }
-    };
-    Hook suppress{std::bind(&DiagnosticsContext::Suppress, &diag, _1)};
-    Hook restore{std::bind(&DiagnosticsContext::Restore, &diag, _1)};
-    std::function<void(const AidlTraversable&)> topDown =
-        [&topDown, &suppress, &restore, visitor](const AidlTraversable& a) {
-      a.DispatchVisit(suppress);
-      a.DispatchVisit(*visitor);
-      a.TraverseChildren(topDown);
-      a.DispatchVisit(restore);
-    };
-    topDown(doc);
-  }
- protected:
-  DiagnosticsContext& diag;
-};
-
-struct DiagnoseInterfaceName : DiagnosticsVisitor {
-  DiagnoseInterfaceName(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlInterface& i) override {
-    if (auto name = i.GetName(); name.size() < 1 || name[0] != 'I') {
-      diag.Report(i.GetLocation(), DiagnosticID::interface_name)
-          << "Interface names should start with I.";
-    }
-  }
-};
-
-struct DiagnoseEnumZero : DiagnosticsVisitor {
-  DiagnoseEnumZero(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlEnumDeclaration& e) override {
-    AIDL_FATAL_IF(e.GetEnumerators().empty(), e)
-        << "The enum '" << e.GetName() << "' has no enumerators.";
-    const auto& first = e.GetEnumerators()[0];
-    if (auto first_value = first->ValueString(e.GetBackingType(), AidlConstantValueDecorator);
-        first_value != "0") {
-      diag.Report(first->GetLocation(), DiagnosticID::enum_zero)
-          << "The first enumerator '" << first->GetName() << "' should be 0, but it is "
-          << first_value << ".";
-    }
-  }
-};
-
-struct DiagnoseInoutParameter : DiagnosticsVisitor {
-  DiagnoseInoutParameter(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlArgument& a) override {
-    if (a.GetDirection() == AidlArgument::INOUT_DIR) {
-      diag.Report(a.GetLocation(), DiagnosticID::inout_parameter)
-          << a.GetName()
-          << " is 'inout'. Avoid inout parameters. This is somewhat confusing for clients "
-             "because although the parameters are 'in', they look out 'out' parameters.";
-    }
-  }
-};
-
-struct DiagnoseConstName : DiagnosticsVisitor {
-  DiagnoseConstName(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlEnumerator& e) override {
-    if (ToUpper(e.GetName()) != e.GetName()) {
-      diag.Report(e.GetLocation(), DiagnosticID::const_name)
-          << "Enum values should be named in upper cases: " << ToUpper(e.GetName());
-    }
-  }
-  void Visit(const AidlConstantDeclaration& c) override {
-    if (ToUpper(c.GetName()) != c.GetName()) {
-      diag.Report(c.GetLocation(), DiagnosticID::const_name)
-          << "Constants should be named in upper cases: " << ToUpper(c.GetName());
-    }
-  }
-  static std::string ToUpper(std::string name) {
-    for (auto& c : name) c = std::toupper(c);
-    return name;
-  }
-};
-
-struct DiagnoseExplicitDefault : DiagnosticsVisitor {
-  DiagnoseExplicitDefault(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlStructuredParcelable& p) override {
-    for (const auto& var : p.GetFields()) {
-      CheckExplicitDefault(*var);
-    }
-  }
-  void Visit(const AidlUnionDecl& u) override {
-    AIDL_FATAL_IF(u.GetFields().empty(), u) << "The union '" << u.GetName() << "' has no fields.";
-    const auto& first = u.GetFields()[0];
-    CheckExplicitDefault(*first);
-  }
-  void CheckExplicitDefault(const AidlVariableDeclaration& v) {
-    if (ShouldHaveExplicitDefault(v) && !v.IsDefaultUserSpecified()) {
-      diag.Report(v.GetLocation(), DiagnosticID::explicit_default)
-          << "The field '" << v.GetName() << "' has no explicit value.";
-    }
-  }
-  bool ShouldHaveExplicitDefault(const AidlVariableDeclaration& v) {
-    if (v.GetType().IsNullable()) return false;
-    if (v.GetType().IsArray()) return true;
-    if (auto type_name = v.GetType().GetName(); AidlTypenames::IsBuiltinTypename(type_name)) {
-      static const std::unordered_set<std::string> default_not_available = {
-          "IBinder", "ParcelableHolder", "ParcelFileDescriptor", "FileDescriptor", "List", "Map"};
-      return default_not_available.find(type_name) == default_not_available.end();
-    }
-    const auto defined_type = v.GetType().GetDefinedType();
-    AIDL_FATAL_IF(!defined_type, v);
-    return defined_type->AsEnumDeclaration() != nullptr;
-  }
-};
-
-struct DiagnoseMixedOneway : DiagnosticsVisitor {
-  DiagnoseMixedOneway(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlInterface& i) override {
-    const auto& methods = i.GetMethods();
-    if (std::adjacent_find(begin(methods), end(methods), [](const auto& a, const auto& b) {
-          return a->IsOneway() != b->IsOneway();
-        }) != end(methods)) {
-      diag.Report(i.GetLocation(), DiagnosticID::mixed_oneway)
-          << "The interface '" << i.GetName() << "' has both one-way and two-way methods.";
-    }
-  }
-};
-
-bool Diagnose(const AidlDocument& doc, const DiagnosticMapping& mapping) {
-  DiagnosticsContext diag(mapping);
-
-  DiagnoseInterfaceName{diag}.Check(doc);
-  DiagnoseEnumZero{diag}.Check(doc);
-  DiagnoseInoutParameter{diag}.Check(doc);
-  DiagnoseConstName{diag}.Check(doc);
-  DiagnoseExplicitDefault{diag}.Check(doc);
-  DiagnoseMixedOneway{diag}.Check(doc);
-
-  return diag.ErrorCount() == 0;
-}
 }  // namespace aidl
 }  // namespace android
