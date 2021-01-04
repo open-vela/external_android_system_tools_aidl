4777c7df044a8c14cb8b8cc557009a9875186074
liujinye <liujinye@xiaomi.com>
2021-01-05 06:49:35 +0800

    Test case for ParcealbleHolder comparison am: 73cd9ce118 am: 54ef3b7f48 am: 80a22173dd
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1536143
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I0bb3d8ca2d68f552a7d3ea35dac291fb85f598cf

--- tests/golden_output/aidl_test_loggable_interface-java-source/gen/android/aidl/loggable/ILoggableInterface.java
+++ tests/golden_output/aidl_test_loggable_interface-java-source/gen/android/aidl/loggable/ILoggableInterface.java
@@ -53,9 +53,6 @@ public interface ILoggableInterface extends android.os.IInterface
           reply.writeString(descriptor);
           return true;
         }
-      }
-      switch (code)
-      {
         case TRANSACTION_LogThis:
         {
           try {
