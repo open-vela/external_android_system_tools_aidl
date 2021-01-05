19b9038f962551df2581721639041ad082a4fe67
liujinye <liujinye@xiaomi.com>
2021-01-06 05:25:14 +0800

    add -Wfile-descriptor am: 377795fadb am: d4bd9a44cf am: 18c033b743
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1538605
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I7b4b2d07797a8b3bdc495a58f3f88d60ea009dee

--- diagnostics.cpp
+++ diagnostics.cpp
@@ -109,14 +109,14 @@ class DiagnosticsVisitor : public AidlVisitor {
     };
     Hook suppress{std::bind(&DiagnosticsContext::Suppress, &diag, _1)};
     Hook restore{std::bind(&DiagnosticsContext::Restore, &diag, _1)};
-    std::function<void(const AidlNode&)> top_down = [&top_down, &suppress, &restore,
-                                                     visitor](const AidlNode& a) {
+    std::function<void(const AidlTraversable&)> topDown =
+        [&topDown, &suppress, &restore, visitor](const AidlTraversable& a) {
       a.DispatchVisit(suppress);
       a.DispatchVisit(*visitor);
-      a.TraverseChildren(top_down);
+      a.TraverseChildren(topDown);
       a.DispatchVisit(restore);
     };
-    top_down(doc);
+    topDown(doc);
   }
  protected:
   DiagnosticsContext& diag;
