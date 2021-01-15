e9a86910d9119df04e39625325ccd6b0c5f8771c
liujinye <liujinye@xiaomi.com>
2021-01-16 01:49:46 +0800

    Mark aidl-golden-test-build-hook as not gtest am: b2f4fe66d4 am: 5976c88457
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1551266
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I4309981e69b81c2427b5e08b6e8a6f36b62dc80b

--- aidl_language.h
+++ aidl_language.h
@@ -435,7 +435,6 @@ class AidlCommentable {
   void SetComments(const std::string comments) { comments_ = comments; }
   bool IsHidden() const;
   bool IsDeprecated() const;
-  void Dump(CodeWriter& out) const;
 
  private:
   std::string comments_;
@@ -959,7 +958,6 @@ class AidlDefinedType : public AidlAnnotatable, public AidlCommentable {
 
   virtual void Dump(CodeWriter* writer) const = 0;
   void DumpHeader(CodeWriter* writer) const;
-  void DumpMembers(CodeWriter& out) const;
 
   const std::vector<std::unique_ptr<AidlVariableDeclaration>>& GetFields() const {
     return variables_;
