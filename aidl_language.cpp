04daff6224863b6f31920d11a1fc83c73952ccdb
liujinye <liujinye@xiaomi.com>
2021-01-19 03:02:04 +0800

    refactor: AidlAnnotation/AidlTypeSpecifier are commentable am: 6736ffbd59
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1552053
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Iabb2cc278bb7ce7ff4fc1bee609d5c43ac36c02c

--- aidl_language.cpp
+++ aidl_language.cpp
@@ -69,6 +69,10 @@ bool IsJavaKeyword(const char* str) {
   };
   return std::find(kJavaKeywords.begin(), kJavaKeywords.end(), str) != kJavaKeywords.end();
 }
+
+inline bool HasHideComment(const std::string& comment) {
+  return std::regex_search(comment, std::regex("@hide\\b"));
+}
 }  // namespace
 
 AidlNode::AidlNode(const AidlLocation& location) : location_(location) {}
@@ -758,7 +762,7 @@ string AidlArgument::ToString() const {
 }
 
 bool AidlCommentable::IsHidden() const {
-  return android::aidl::HasHideInComments(GetComments());
+  return HasHideComment(GetComments());
 }
 
 bool AidlCommentable::IsDeprecated() const {
