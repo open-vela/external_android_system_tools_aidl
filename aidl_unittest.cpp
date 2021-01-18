f840a7eeadf99715f2231e4200fffb973d2271b5
openvela-robot <openvela-robot@xiaomi.com>
2021-01-19 04:10:01 +0800

    refactor: AidlAnnotation/AidlTypeSpecifier are commentable am: 6736ffbd59 am: 38d6121a19 am: 32384dba17
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1552053
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ia1f2d459918488a03dec6daa1f385cafac01611d

--- aidl_unittest.cpp
+++ aidl_unittest.cpp
@@ -781,11 +781,11 @@ TEST_P(AidlTest, SupportDeprecated) {
             R"(#[deprecated = "a really long deprecation message which is really long"])"}},
       });
 
-  // In AIDL @deprecated can be in block comments as well as javadoc style
+  // In AIDL @deprecated can be in any style of comments
   CheckDeprecated(
       "IFoo.aidl",
       "interface IFoo {\n"
-      "  /* @deprecated use bar() */\n"
+      "  // @deprecated use bar()\n"
       "  List<String> foo();\n"
       "}",
       {
@@ -798,11 +798,6 @@ TEST_P(AidlTest, SupportDeprecated) {
           {Options::Language::RUST, {"out/IFoo.rs", "#[deprecated = \"use bar()\"]"}},
       });
 
-  // but not in line comments
-  auto parsed = Parse("IFoo.aidl", "// @deprecated\ninterface IFoo {}", typenames_, GetLanguage());
-  EXPECT_FALSE(parsed->IsDeprecated());
-
-  // parcelable
   CheckDeprecated("Foo.aidl",
                   "parcelable Foo {\n"
                   "  /** @deprecated use bar*/\n"
@@ -815,7 +810,6 @@ TEST_P(AidlTest, SupportDeprecated) {
                       {Options::Language::RUST, {"out/Foo.rs", "#[deprecated"}},
                   });
 
-  // interface constants
   CheckDeprecated("IFoo.aidl",
                   "interface IFoo {\n"
                   "  /** @deprecated use bar*/\n"
@@ -1424,7 +1418,7 @@ TEST_F(AidlTest, ApiDump) {
       "foo/bar/IFoo.aidl",
       "package foo.bar;\n"
       "import foo.bar.Data;\n"
-      "// commented /* @hide */\n"
+      "// comment @hide\n"
       "interface IFoo {\n"
       "    /* @hide */\n"
       "    int foo(out int[] a, String b, boolean c, inout List<String>  d);\n"
@@ -1436,7 +1430,7 @@ TEST_F(AidlTest, ApiDump) {
       "        @deprecated\n"
       "          reason why... */\n"
       "    const int A = 1;\n"
-      "    // @deprecated tags in line comments are ignored\n"
+      "    // @deprecated do not use\n"
       "    const String STR = \"Hello\";\n"
       "}\n");
   io_delegate_.SetFileContents("foo/bar/Data.aidl",
@@ -1450,7 +1444,7 @@ TEST_F(AidlTest, ApiDump) {
                                "   int y;\n"
                                "   /*@hide2*/\n"
                                "   IFoo foo;\n"
-                               "   // Ignore @hide property in line comment\n"
+                               "   // It should be @hide property\n"
                                "   @nullable String[] c;\n"
                                "}\n");
   io_delegate_.SetFileContents("api.aidl", "");
@@ -1462,14 +1456,17 @@ TEST_F(AidlTest, ApiDump) {
   string actual;
   EXPECT_TRUE(io_delegate_.GetWrittenContents("dump/foo/bar/IFoo.aidl", &actual));
   EXPECT_EQ(actual, string(kPreamble).append(R"(package foo.bar;
+/* @hide */
 interface IFoo {
   /* @hide */
   int foo(out int[] a, String b, boolean c, inout List<String> d);
   int foo2(@utf8InCpp String x, inout List<String> y);
   foo.bar.IFoo foo3(foo.bar.IFoo foo);
   foo.bar.Data getData();
+  /* @hide */
   /* @deprecated reason why... */
   const int A = 1;
+  /* @deprecated do not use */
   const String STR = "Hello";
 }
 )"));
@@ -1478,9 +1475,12 @@ interface IFoo {
   EXPECT_EQ(actual, string(kPreamble).append(R"(package foo.bar;
 /* @hide */
 parcelable Data {
+  /* @hide */
   int x = 10;
+  /* @hide */
   int y;
   foo.bar.IFoo foo;
+  /* @hide */
   @nullable String[] c;
 }
 )"));
