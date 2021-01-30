8becc0a34e50efa304590defbaea523e7ad142a2
Jooyung Han <jooyung@google.com>
2021-01-30 11:19:33 +0900

    Be more explicit about union's catch-all ctor
    This is only for native backends.
    
    When a union is used in another union, the catch-all ctor fails to
    compile. Making the first parameter explicit makes compiler happy.
    
    Bug: none
    Test: m aidl-test-interface-ndk_platform (with example added)
    Change-Id: Ib9d722cc4476f90a4a75cbd8d42eb1c67e08a9ce

--- tests/golden_output/aidl-test-interface-ndk_platform-source/gen/include/aidl/android/aidl/tests/unions/UnionInUnion.h
+++ tests/golden_output/aidl-test-interface-ndk_platform-source/gen/include/aidl/android/aidl/tests/unions/UnionInUnion.h
@@ -17,7 +17,7 @@
 #ifdef BINDER_STABILITY_SUPPORT
 #include <android/binder_stability.h>
 #endif  // BINDER_STABILITY_SUPPORT
-#include <aidl/android/aidl/tests/ByteEnum.h>
+#include <aidl/android/aidl/tests/unions/EnumUnion.h>
 
 #ifndef __BIONIC__
 #define __assert2(a,b,c,d) ((void)0)
@@ -27,47 +27,43 @@ namespace aidl {
 namespace android {
 namespace aidl {
 namespace tests {
-class Union {
+namespace unions {
+class UnionInUnion {
 public:
   typedef std::false_type fixed_size;
   static const char* descriptor;
 
   enum Tag : int32_t {
-    ns = 0,  // int[] ns;
-    n,  // int n;
-    m,  // int m;
-    s,  // String s;
-    ibinder,  // IBinder ibinder;
-    ss,  // List<String> ss;
-    be,  // android.aidl.tests.ByteEnum be;
+    first = 0,  // android.aidl.tests.unions.EnumUnion first;
+    second,  // int second;
   };
 
   template<typename _Tp>
-  static constexpr bool _not_self = !std::is_same_v<std::remove_cv_t<std::remove_reference_t<_Tp>>, Union>;
+  static constexpr bool _not_self = !std::is_same_v<std::remove_cv_t<std::remove_reference_t<_Tp>>, UnionInUnion>;
 
-  Union() : _value(std::in_place_index<ns>, std::vector<int32_t>({})) { }
-  Union(const Union&) = default;
-  Union(Union&&) = default;
-  Union& operator=(const Union&) = default;
-  Union& operator=(Union&&) = default;
+  UnionInUnion() : _value(std::in_place_index<first>, ::aidl::android::aidl::tests::unions::EnumUnion()) { }
+  UnionInUnion(const UnionInUnion&) = default;
+  UnionInUnion(UnionInUnion&&) = default;
+  UnionInUnion& operator=(const UnionInUnion&) = default;
+  UnionInUnion& operator=(UnionInUnion&&) = default;
 
   template <typename _Tp, typename = std::enable_if_t<_not_self<_Tp>>>
   // NOLINTNEXTLINE(google-explicit-constructor)
-  constexpr Union(_Tp&& _arg)
+  constexpr UnionInUnion(_Tp&& _arg)
       : _value(std::forward<_Tp>(_arg)) {}
 
   template <size_t _Np, typename... _Tp>
-  constexpr explicit Union(std::in_place_index_t<_Np>, _Tp&&... _args)
+  constexpr explicit UnionInUnion(std::in_place_index_t<_Np>, _Tp&&... _args)
       : _value(std::in_place_index<_Np>, std::forward<_Tp>(_args)...) {}
 
   template <Tag _tag, typename... _Tp>
-  static Union make(_Tp&&... _args) {
-    return Union(std::in_place_index<_tag>, std::forward<_Tp>(_args)...);
+  static UnionInUnion make(_Tp&&... _args) {
+    return UnionInUnion(std::in_place_index<_tag>, std::forward<_Tp>(_args)...);
   }
 
   template <Tag _tag, typename _Tp, typename... _Up>
-  static Union make(std::initializer_list<_Tp> _il, _Up&&... _args) {
-    return Union(std::in_place_index<_tag>, std::move(_il), std::forward<_Up>(_args)...);
+  static UnionInUnion make(std::initializer_list<_Tp> _il, _Up&&... _args) {
+    return UnionInUnion(std::in_place_index<_tag>, std::move(_il), std::forward<_Up>(_args)...);
   }
 
   Tag getTag() const {
@@ -94,27 +90,26 @@ public:
   binder_status_t readFromParcel(const AParcel* _parcel);
   binder_status_t writeToParcel(AParcel* _parcel) const;
 
-  inline bool operator!=(const Union& rhs) const {
+  inline bool operator!=(const UnionInUnion& rhs) const {
     return _value != rhs._value;
   }
-  inline bool operator<(const Union& rhs) const {
+  inline bool operator<(const UnionInUnion& rhs) const {
     return _value < rhs._value;
   }
-  inline bool operator<=(const Union& rhs) const {
+  inline bool operator<=(const UnionInUnion& rhs) const {
     return _value <= rhs._value;
   }
-  inline bool operator==(const Union& rhs) const {
+  inline bool operator==(const UnionInUnion& rhs) const {
     return _value == rhs._value;
   }
-  inline bool operator>(const Union& rhs) const {
+  inline bool operator>(const UnionInUnion& rhs) const {
     return _value > rhs._value;
   }
-  inline bool operator>=(const Union& rhs) const {
+  inline bool operator>=(const UnionInUnion& rhs) const {
     return _value >= rhs._value;
   }
 
   static const ::ndk::parcelable_stability_t _aidl_stability = ::ndk::STABILITY_LOCAL;
-  static const char* S1;
   template <typename _T> class _has_toString {
     template <typename _U> static std::true_type __has_toString(decltype(&_U::toString));
     template <typename _U> static std::false_type __has_toString(...);
@@ -126,22 +121,18 @@ public:
   }
   inline std::string toString() const {
     std::ostringstream os;
-    os << "Union{";
+    os << "UnionInUnion{";
     switch (getTag()) {
-    case ns: os << "ns: " << [&](){ std::ostringstream o; o << "["; bool first = true; for (const auto& v: get<ns>()) { (void)v; if (first) first = false; else o << ", "; o << std::to_string(v); }; o << "]"; return o.str(); }(); break;
-    case n: os << "n: " << std::to_string(get<n>()); break;
-    case m: os << "m: " << std::to_string(get<m>()); break;
-    case s: os << "s: " << (std::ostringstream() << get<s>()).str(); break;
-    case ibinder: os << "ibinder: " << ""; break;
-    case ss: os << "ss: " << [&](){ std::ostringstream o; o << "["; bool first = true; for (const auto& v: get<ss>()) { (void)v; if (first) first = false; else o << ", "; o << (std::ostringstream() << v).str(); }; o << "]"; return o.str(); }(); break;
-    case be: os << "be: " << android::aidl::tests::toString(get<be>()); break;
+    case first: os << "first: " << _call_toString(get<first>()); break;
+    case second: os << "second: " << std::to_string(get<second>()); break;
     }
     os << "}";
     return os.str();
   }
 private:
-  std::variant<std::vector<int32_t>, int32_t, int32_t, std::string, ::ndk::SpAIBinder, std::vector<std::string>, ::aidl::android::aidl::tests::ByteEnum> _value;
+  std::variant<::aidl::android::aidl::tests::unions::EnumUnion, int32_t> _value;
 };
+}  // namespace unions
 }  // namespace tests
 }  // namespace aidl
 }  // namespace android
