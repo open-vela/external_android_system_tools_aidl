cf96d4ff5feaf4e380499a7a276d48322698104c
openvela-robot <openvela-robot@xiaomi.com>
2021-02-02 04:08:28 +0800

    Snap for 7117181 from 16640a61fe75354ba29b2393df7bda7704c1cd5b to tm-release
    Change-Id: Ic34d0a2f63e4beb494bd2e88d92829727d423ffe

--- tests/aidl_test_client_file_descriptors.cpp
+++ tests/aidl_test_client_file_descriptors.cpp
@@ -89,17 +89,9 @@ TEST_F(FdTest, fileDescriptorArray) {
   DoPipe(&array[0], &array[1]);
 
   std::vector<unique_fd> repeated;
-
-  if (backend == BackendType::JAVA) {
-    // other backends might require these to be valid FDs (not -1), since this
-    // isn't @nullable, but they don't require this to already be the correct
-    // size
-    repeated = std::vector<unique_fd>(array.size());
-  }
-
   std::vector<unique_fd> reversed;
   auto status = cpp_java_tests->ReverseFileDescriptorArray(array, &repeated, &reversed);
-  ASSERT_TRUE(status.isOk()) << status;
+  ASSERT_TRUE(status.isOk());
 
   WriteStringToFd("First", array[1]);
   WriteStringToFd("Second", repeated[1]);
@@ -136,15 +128,9 @@ TEST_F(FdTest, parcelFileDescriptorArray) {
   }
 
   std::vector<ParcelFileDescriptor> repeated;
-
-  if (backend == BackendType::JAVA) {
-    // TODO(b/169704480) - this always returns UNKNOWN_TRANSACTION
-    GTEST_SKIP() << "Broken in Java? b/169704480";
-  }
-
   std::vector<ParcelFileDescriptor> reversed;
   auto status = service->ReverseParcelFileDescriptorArray(input, &repeated, &reversed);
-  ASSERT_TRUE(status.isOk()) << status;
+  ASSERT_TRUE(status.isOk());
 
   WriteStringToFd("First", input[1].release());
   WriteStringToFd("Second", repeated[1].release());
--- tests/aidl_test_client_utf8_strings.cpp
+++ tests/aidl_test_client_utf8_strings.cpp
@@ -30,7 +30,6 @@ using android::String16;
 using android::String8;
 using android::binder::Status;
 
-using android::aidl::tests::BackendType;
 using android::aidl::tests::ITestService;
 
 using testing::Eq;
@@ -69,13 +68,10 @@ TEST_F(AidlTest, repeatUtf8String) {
 TEST_F(AidlTest, reverseUtf8StringArray) {
   std::vector<std::string> input = {"a", "", "\xc3\xb8"};
   decltype(input) repeated;
-  if (backend == BackendType::JAVA) {
-    repeated = decltype(input)(input.size());
-  }
   decltype(input) reversed;
 
   auto status = service->ReverseUtf8CppString(input, &repeated, &reversed);
-  ASSERT_TRUE(status.isOk()) << status;
+  ASSERT_TRUE(status.isOk());
   ASSERT_THAT(repeated, Eq(input));
 
   decltype(input) reversed_input(input);
@@ -89,32 +85,21 @@ struct AidlStringArrayTest : public AidlTest {
                                    std::optional<std::vector<std::optional<std::string>>>*,
                                    std::optional<std::vector<std::optional<std::string>>>*)) {
     std::optional<std::vector<std::optional<std::string>>> input;
-    decltype(input) repeated;
     decltype(input) reversed;
+    decltype(input) repeated;
 
-    auto status = (*service.*func)(input, &repeated, &reversed);
-    ASSERT_TRUE(status.isOk()) << status;
-
-    if (func == &ITestService::ReverseUtf8CppStringList && backend == BackendType::JAVA) {
-      // Java cannot clear the input variable to return a null value. It can
-      // only ever fill out a list.
-      ASSERT_TRUE(repeated.has_value());
-    } else {
-      ASSERT_FALSE(repeated.has_value());
-    }
-
+    auto status = (*service.*func)(input, &reversed, &repeated);
+    ASSERT_TRUE(status.isOk());
     ASSERT_FALSE(reversed.has_value());
+    ASSERT_FALSE(repeated.has_value());
 
     input = std::vector<std::optional<std::string>>();
     input->push_back("Deliver us from evil.");
     input->push_back(std::nullopt);
     input->push_back("\xF0\x90\x90\xB7\xE2\x82\xAC");
 
-    // usable size needs to be initialized for Java
-    repeated = std::vector<std::optional<std::string>>(input->size());
-
-    status = (*service.*func)(input, &repeated, &reversed);
-    ASSERT_TRUE(status.isOk()) << status;
+    status = service->ReverseUtf8CppStringList(input, &repeated, &reversed);
+    ASSERT_TRUE(status.isOk());
     ASSERT_TRUE(reversed.has_value());
     ASSERT_TRUE(repeated.has_value());
     ASSERT_THAT(reversed->size(), Eq(input->size()));
@@ -139,10 +124,10 @@ struct AidlStringArrayTest : public AidlTest {
   }
 };
 
-TEST_F(AidlStringArrayTest, nullableList) {
+TEST_F(AidlStringArrayTest, nonNullable) {
   DoTest(&ITestService::ReverseUtf8CppStringList);
 }
 
-TEST_F(AidlStringArrayTest, nullableArray) {
+TEST_F(AidlStringArrayTest, nullable) {
   DoTest(&ITestService::ReverseNullableUtf8CppString);
 }
