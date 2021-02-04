239e4b21c285adef4774b41d3ebdfd8764172961
liujinye <liujinye@xiaomi.com>
2021-02-05 04:08:40 +0800

    Snap for 7125416 from 3ca6370249cd990a38c55c64a2f9f1fe39b09e1f to tm-release
    Change-Id: I49b416fe62bb543a8c6a2a98b7eae27048284f35

--- tests/aidl_test_client_loggable_interface.cpp
+++ tests/aidl_test_client_loggable_interface.cpp
@@ -118,9 +118,9 @@ TEST_F(AidlTest, LoggableInterface) {
           {"stringArray", "[ghi, jkl]"},
           {"listValue", "[mno]"},
           {"dataValue", "Data{num: 42, str: abc, nestedUnion: Union{str: def}, nestedEnum: FOO}"},
-          {"binderValue", "(null)"},
-          {"pfdValue", "(null)"},
-          {"pfdArray", "[]"},
+          {"binderValue", ""},
+          {"pfdValue", ""},
+          {"pfdArray", ""},
       }));
   EXPECT_THAT(log.output_args,
               Eq(vector<pair<string, string>>{{"boolArray", "[false, true]"},
@@ -132,6 +132,6 @@ TEST_F(AidlTest, LoggableInterface) {
                                               {"doubleArray", "[53.000000, 54.000000]"},
                                               {"stringArray", "[ghi, jkl]"},
                                               {"listValue", "[mno]"},
-                                              {"pfdValue", "(null)"},
-                                              {"pfdArray", "[]"}}));
+                                              {"pfdValue", ""},
+                                              {"pfdArray", ""}}));
 }
