094e170f362df68040d36134725aeac7582faed6
openvela-robot <openvela-robot@xiaomi.com>
2021-03-30 03:09:15 +0800

    Snap for 7243657 from 14ee73586bf5677028a484dfea4f11a90dde7a2a to tm-release
    Change-Id: Icdf3a4e78ab5c55ccf4d8f242e1d8fc2f1a1ed14

--- tests/java/src/android/aidl/tests/AidlJavaTests.java
+++ tests/java/src/android/aidl/tests/AidlJavaTests.java
@@ -11,8 +11,7 @@ public class AidlJavaTests {
         junit.addListener(new TextListener(System.out));
         Result result = junit.run(ExtensionTests.class, GenericTests.class,
             JavaOnlyImmutableAnnotationTests.class, MapTests.class, NullableTests.class,
-            TestServiceClient.class, TestVersionedInterface.class, UnionTests.class,
-            VintfTests.class);
+            TestServiceClient.class, TestVersionedInterface.class, UnionTests.class);
 
         System.out.println(result.wasSuccessful() ? "TEST SUCCESS" : "TEST FAILURE");
     }
