4ea79407f1418c405cb6d660025953c84604ddbd
openvela-robot <openvela-robot@xiaomi.com>
2021-04-29 04:54:43 +0800

    Fix readFromParcel in every backend am: 9e85bce7f9 am: e498eb9808 am: 53d0d1abef am: 1958ab9975
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1688277
    
    Change-Id: Ie23425781670d8c16bb63aec2b5d8924784a1f86

--- tests/golden_output/aidl-test-interface-ndk_platform-source/gen/android/aidl/tests/OtherParcelableForToString.cpp
+++ tests/golden_output/aidl-test-interface-ndk_platform-source/gen/android/aidl/tests/OtherParcelableForToString.cpp
@@ -16,13 +16,13 @@ binder_status_t OtherParcelableForToString::readFromParcel(const AParcel* parcel
   if (_aidl_parcelable_size < 0) return STATUS_BAD_VALUE;
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  _aidl_ret_status = ::ndk::AParcel_readString(parcel, &field);
-  if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
-
   if (AParcel_getDataPosition(parcel) - _aidl_start_pos >= _aidl_parcelable_size) {
     AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
     return _aidl_ret_status;
   }
+  _aidl_ret_status = ::ndk::AParcel_readString(parcel, &field);
+  if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
+
   AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
