3867b9d4d18f4255690fe9a84dc3c48d859c68fe
liujinye <liujinye@xiaomi.com>
2021-04-29 04:07:04 +0800

    Add a structured parcelable to a versioned test am: 05bd8a852b am: e94d2b736a
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1688276
    
    Change-Id: I80b3cd52ea29fd716a374bf34f5ee16525b2a5ff

--- tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/GenericStructuredParcelable.rs
+++ tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/GenericStructuredParcelable.rs
@@ -49,16 +49,13 @@ impl binder::parcel::DeserializeOption for GenericStructuredParcelable {
     let parcelable_size: i32 = parcel.read()?;
     if parcelable_size < 0 { return Err(binder::StatusCode::BAD_VALUE); }
     let mut result = Self::default();
-    if (parcel.get_data_position() - start_pos) == parcelable_size {
-      return Ok(Some(result));
-    }
     result.a = parcel.read()?;
     if (parcel.get_data_position() - start_pos) == parcelable_size {
       return Ok(Some(result));
     }
     result.b = parcel.read()?;
-    unsafe {
-      parcel.set_data_position(start_pos + parcelable_size)?;
+    if (parcel.get_data_position() - start_pos) == parcelable_size {
+      return Ok(Some(result));
     }
     Ok(Some(result))
   }
