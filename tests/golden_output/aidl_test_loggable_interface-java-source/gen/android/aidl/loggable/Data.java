36f196213b6612947794e79f0167dd91e9bf9335
openvela-robot <openvela-robot@xiaomi.com>
2021-04-29 05:10:51 +0800

    Fix readFromParcel in every backend am: 9e85bce7f9 am: e498eb9808 am: 53d0d1abef am: 1958ab9975 am: 1b38c0c8b5
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1688277
    
    Change-Id: Ice2c589c4b152bcf5b399aebd5b6945a971e8de6

--- tests/golden_output/aidl_test_loggable_interface-java-source/gen/android/aidl/loggable/Data.java
+++ tests/golden_output/aidl_test_loggable_interface-java-source/gen/android/aidl/loggable/Data.java
@@ -45,6 +45,7 @@ public class Data implements android.os.Parcelable
     int _aidl_parcelable_size = _aidl_parcel.readInt();
     try {
       if (_aidl_parcelable_size < 0) return;
+      if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       num = _aidl_parcel.readInt();
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       str = _aidl_parcel.readString();
@@ -57,7 +58,6 @@ public class Data implements android.os.Parcelable
       }
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       nestedEnum = _aidl_parcel.readByte();
-      if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
     } finally {
       if (_aidl_start_pos > (Integer.MAX_VALUE - _aidl_parcelable_size)) {
         throw new android.os.BadParcelableException("Overflow in the size of parcelable");
