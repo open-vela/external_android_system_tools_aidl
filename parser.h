af1731f33234915aac13c79824e5b2fb1c52b05c
liujinye <liujinye@xiaomi.com>
2021-05-25 23:00:53 +0800

    dumpapi dumps generic type parameters am: 15a79f217a
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/14702399
    
    Change-Id: Iafd5f26c223f71fda9e9ca3f0c2aa0e0b85b089c

--- parser.h
+++ parser.h
@@ -54,8 +54,7 @@ class AidlToken {
   android::aidl::Comments comments_;
 };
 
-using TypeResolver = std::function<bool(const AidlDefinedType*, AidlTypeSpecifier*)>;
-bool ResolveReferences(const AidlDocument& document, TypeResolver& resolver);
+using TypeResolver = std::function<bool(const AidlDocument*, AidlTypeSpecifier*)>;
 
 class Parser {
  public:
@@ -66,9 +65,9 @@ class Parser {
   ~Parser();
 
   // Parse contents of file |filename|. Should only be called once.
-  static const AidlDocument* Parse(const std::string& filename,
-                                   const android::aidl::IoDelegate& io_delegate,
-                                   AidlTypenames& typenames);
+  static std::unique_ptr<Parser> Parse(const std::string& filename,
+                                       const android::aidl::IoDelegate& io_delegate,
+                                       AidlTypenames& typenames);
 
   void AddError() { error_++; }
   bool HasError() const { return error_ != 0; }
@@ -88,6 +87,13 @@ class Parser {
   void SetPackage(const std::string& package) { package_ = package; }
   const std::string& Package() const { return package_; }
 
+  void DeferResolution(AidlTypeSpecifier* typespec) {
+    unresolved_typespecs_.emplace_back(typespec);
+  }
+
+  const vector<AidlTypeSpecifier*>& GetUnresolvedTypespecs() const { return unresolved_typespecs_; }
+
+  bool Resolve(TypeResolver& type_resolver);
   void SetDocument(std::unique_ptr<AidlDocument>&& document) {
     // The parsed document is owned by typenames_. This parser object only has
     // a reference to it.
@@ -96,6 +102,11 @@ class Parser {
       document_ = nullptr;
       AddError();
     }
+  }
+
+  const AidlDocument& ParsedDocument() const {
+    AIDL_FATAL_IF(HasError(), FileName());
+    return *document_;
   }
 
  private:
@@ -110,5 +121,6 @@ class Parser {
   YY_BUFFER_STATE buffer_;
   int error_ = 0;
 
+  vector<AidlTypeSpecifier*> unresolved_typespecs_;
   const AidlDocument* document_;
 };
