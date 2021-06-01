c063ac58e929833c34d46820ab557244668446c3
liujinye <liujinye@xiaomi.com>
2021-06-01 23:27:19 +0800

    [automerger skipped] fix import paths when compiling snapshots am: 5b9fd4b418 am: 1124186a16 -s ours
    am skip reason: Merged-In I92f8e290b89c737f4b16fc064e763bd47cd1a9da with SHA-1 60699b5f00 is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/14732002
    
    Change-Id: If14e6e255851b900ef72be1356b8b8495ed6ca52

--- build/aidl_interface.go
+++ build/aidl_interface.go
@@ -53,6 +53,7 @@ var (
 func init() {
 	pctx.Import("android/soong/android")
 	pctx.HostBinToolVariable("aidlCmd", "aidl")
+	pctx.HostBinToolVariable("aidlHashGen", "aidl_hash_gen")
 	pctx.SourcePathVariable("aidlToJniCmd", "system/tools/aidl/build/aidl_to_jni.py")
 	pctx.SourcePathVariable("aidlRustGlueCmd", "system/tools/aidl/build/aidl_rust_glue.py")
 	android.RegisterModuleType("aidl_interface", aidlInterfaceFactory)
@@ -465,10 +466,10 @@ func checkImports(mctx android.BottomUpMutatorContext) {
 			other := lookupInterface(anImport, mctx.Config())
 
 			if other == nil {
-				if mctx.Config().AllowMissingDependencies() {
-					continue
+				if !mctx.Config().AllowMissingDependencies() {
+					mctx.PropertyErrorf("imports", "Import does not exist: "+anImport)
 				}
-				mctx.PropertyErrorf("imports", "Import does not exist: "+anImport)
+				continue
 			}
 			if version != "" {
 				candidateVersions := concat(other.properties.Versions, []string{other.nextVersion()})
