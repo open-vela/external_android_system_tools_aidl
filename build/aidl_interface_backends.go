909b77a435d2abde7b32b42d5c721bce81d30477
liujinye <liujinye@xiaomi.com>
2021-06-08 05:48:41 +0800

    Preprocessed uses the same syntax of AIDL am: 93f48f0569 am: eed0a77f05 am: d343b8f24a
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1727712
    
    Change-Id: I4c659ec39e45da2b2d287a054f9ba7cc4295f82c

--- build/aidl_interface_backends.go
+++ build/aidl_interface_backends.go
@@ -75,6 +75,7 @@ func addCppLibrary(mctx android.LoadHookContext, i *aidlInterface, version strin
 	}, &aidlGenProperties{
 		Srcs:                  srcs,
 		AidlRoot:              aidlRoot,
+		IsToT:                 version == i.nextVersion(),
 		ImportsWithoutVersion: i.properties.ImportsWithoutVersion,
 		Stability:             i.properties.Stability,
 		Lang:                  lang,
@@ -232,6 +233,7 @@ func addJavaLibrary(mctx android.LoadHookContext, i *aidlInterface, version stri
 	}, &aidlGenProperties{
 		Srcs:                  srcs,
 		AidlRoot:              aidlRoot,
+		IsToT:                 version == i.nextVersion(),
 		ImportsWithoutVersion: i.properties.ImportsWithoutVersion,
 		Stability:             i.properties.Stability,
 		Lang:                  langJava,
@@ -281,6 +283,7 @@ func addRustLibrary(mctx android.LoadHookContext, i *aidlInterface, version stri
 		Srcs:                  srcs,
 		AidlRoot:              aidlRoot,
 		ImportsWithoutVersion: i.properties.ImportsWithoutVersion,
+		IsToT:                 version == i.nextVersion(),
 		Stability:             i.properties.Stability,
 		Lang:                  langRust,
 		BaseName:              i.ModuleBase.Name(),
@@ -328,7 +331,7 @@ func (i *aidlInterface) versionedName(version string) string {
 	return name + "-V" + version
 }
 
-func (i *aidlInterface) srcsForVersion(mctx android.EarlyModuleContext, version string) (srcs []string, aidlRoot string) {
+func (i *aidlInterface) srcsForVersion(mctx android.LoadHookContext, version string) (srcs []string, aidlRoot string) {
 	if version == i.nextVersion() {
 		return i.properties.Srcs, i.properties.Local_include_dir
 	} else {
