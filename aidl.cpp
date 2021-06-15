02412b05d8cd9c91d99d4bf171016b91d628da46
openvela-robot <openvela-robot@xiaomi.com>
2021-06-16 00:34:35 +0800

    cleanup: remove unused parameter to ImportResolver am: 6529b82782 am: c22abe49c6
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1733935
    
    Change-Id: I54735047044823def6284dc0eb21f89c994b2ed0

--- aidl.cpp
+++ aidl.cpp
@@ -395,6 +395,9 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
   vector<string> import_paths;
   ImportResolver import_resolver{io_delegate, input_file_name, options.ImportDirs()};
   for (const auto& import : document->Imports()) {
+    if (AidlTypenames::IsBuiltinTypename(import->GetNeededClass())) {
+      continue;
+    }
     if (typenames->IsIgnorableImport(import->GetNeededClass())) {
       // There are places in the Android tree where an import doesn't resolve,
       // but we'll pick the type up through the preprocessed types.
@@ -403,6 +406,11 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
     }
     string import_path = import_resolver.FindImportFile(import->GetNeededClass());
     if (import_path.empty()) {
+      if (typenames->ResolveTypename(import->GetNeededClass()).is_resolved) {
+        // This could happen when the type is from the preprocessed aidl file.
+        // In that case, use the type from preprocessed aidl file
+        continue;
+      }
       AIDL_ERROR(input_file_name) << "Couldn't find import for class " << import->GetNeededClass();
       err = AidlError::BAD_IMPORT;
       continue;
@@ -423,23 +431,26 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
   }
 
   TypeResolver resolver = [&](const AidlDefinedType* scope, AidlTypeSpecifier* type) {
-    // resolve with already loaded types
-    if (type->Resolve(*typenames, scope)) {
-      return true;
+    if (type->Resolve(*typenames)) return true;
+
+    const string unresolved_name = type->GetUnresolvedName();
+    const auto doc = typenames->GetDocumentFor(scope);
+    const std::optional<string> canonical_name = doc->ResolveName(unresolved_name);
+    if (!canonical_name) {
+      return false;
     }
-    const string import_path = import_resolver.FindImportFile(scope->ResolveName(type->GetName()));
+    const string import_path = import_resolver.FindImportFile(*canonical_name);
     if (import_path.empty()) {
       return false;
     }
     import_paths.push_back(import_path);
+
     auto imported_doc = Parser::Parse(import_path, io_delegate, *typenames);
     if (imported_doc == nullptr) {
       AIDL_ERROR(import_path) << "error while importing " << import_path << " for " << import_path;
       return false;
     }
-
-    // now, try to resolve it again
-    if (!type->Resolve(*typenames, scope)) {
+    if (!type->Resolve(*typenames)) {
       AIDL_ERROR(type) << "Can't resolve " << type->GetName();
       return false;
     }
@@ -539,7 +550,7 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
       if (options.Version() > 0) {
         AidlTypeSpecifier* ret =
             new AidlTypeSpecifier(AIDL_LOCATION_HERE, "int", false, nullptr, Comments{});
-        ret->Resolve(*typenames, nullptr);
+        ret->Resolve(*typenames);
         vector<unique_ptr<AidlArgument>>* args = new vector<unique_ptr<AidlArgument>>();
         auto method = std::make_unique<AidlMethod>(
             AIDL_LOCATION_HERE, false, ret, "getInterfaceVersion", args, Comments{},
@@ -550,7 +561,7 @@ AidlError load_and_validate_aidl(const std::string& input_file_name, const Optio
       if (!options.Hash().empty()) {
         AidlTypeSpecifier* ret =
             new AidlTypeSpecifier(AIDL_LOCATION_HERE, "String", false, nullptr, Comments{});
-        ret->Resolve(*typenames, nullptr);
+        ret->Resolve(*typenames);
         vector<unique_ptr<AidlArgument>>* args = new vector<unique_ptr<AidlArgument>>();
         auto method = std::make_unique<AidlMethod>(
             AIDL_LOCATION_HERE, false, ret, kGetInterfaceHash, args, Comments{},
