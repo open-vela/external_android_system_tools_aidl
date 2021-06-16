ae050a266771d939c38a7893228a7c4bf896ef9a
liujinye <liujinye@xiaomi.com>
2021-06-17 03:10:09 +0800

    Snap for 7464528 from 72bc40c4091a1b303838a71ee3cd50bb96b1d9d2 to tm-release
    Change-Id: I58e836db2a6f6ac946e92e323e6357ee6b4cae31

--- tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/OtherParcelableForToString.rs
+++ tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/OtherParcelableForToString.rs
@@ -1,3 +1,4 @@
+#![forbid(unsafe_code)]
 #[derive(Debug)]
 pub struct OtherParcelableForToString {
   pub field: String,
@@ -33,19 +34,11 @@ impl binder::parcel::SerializeOption for OtherParcelableForToString {
 binder::impl_deserialize_for_parcelable!(OtherParcelableForToString);
 impl OtherParcelableForToString {
   fn deserialize_parcelable(&mut self, parcel: &binder::parcel::Parcel) -> binder::Result<()> {
-    let start_pos = parcel.get_data_position();
-    let parcelable_size: i32 = parcel.read()?;
-    if parcelable_size < 0 { return Err(binder::StatusCode::BAD_VALUE); }
-    if start_pos.checked_add(parcelable_size).is_none() {
-      return Err(binder::StatusCode::BAD_VALUE);
-    }
-    if (parcel.get_data_position() - start_pos) == parcelable_size {
-      return Ok(());
-    }
-    self.field = parcel.read()?;
-    unsafe {
-      parcel.set_data_position(start_pos + parcelable_size)?;
-    }
-    Ok(())
+    parcel.sized_read(|subparcel| {
+      if subparcel.has_more_data() {
+        self.field = subparcel.read()?;
+      }
+      Ok(())
+    })
   }
 }
