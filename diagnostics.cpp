12cabdd984ccb57ec9d4c3ca1a2c755d78586057
liujinye <liujinye@xiaomi.com>
2021-06-28 18:06:38 +0800

    union/cpp: remove "= default" copy/move am: 871fd74120 am: e97a79b901
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1749108
    
    Change-Id: I9e8449b11f9ce12ade22f75f2637d078797e071e

--- diagnostics.cpp
+++ diagnostics.cpp
@@ -285,18 +285,6 @@ struct DiagnoseImports : DiagnosticsVisitor {
   }
 };
 
-struct DiagnoseUntypedCollection : DiagnosticsVisitor {
-  DiagnoseUntypedCollection(DiagnosticsContext& diag) : DiagnosticsVisitor(diag) {}
-  void Visit(const AidlTypeSpecifier& t) override {
-    if (t.GetName() == "List" || t.GetName() == "Map") {
-      if (!t.IsGeneric()) {
-        diag.Report(t.GetLocation(), DiagnosticID::untyped_collection)
-            << "Use List<V> or Map<K,V> instead.";
-      }
-    }
-  }
-};
-
 bool Diagnose(const AidlDocument& doc, const DiagnosticMapping& mapping) {
   DiagnosticsContext diag(mapping);
 
@@ -309,7 +297,6 @@ bool Diagnose(const AidlDocument& doc, const DiagnosticMapping& mapping) {
   DiagnoseFileDescriptor{diag}.Check(doc);
   DiagnoseOutNullable{diag}.Check(doc);
   DiagnoseImports{diag}.Check(doc);
-  DiagnoseUntypedCollection{diag}.Check(doc);
 
   return diag.ErrorCount() == 0;
 }
