6a887d46be5c6f7de8655f4f66d72da50d35d2c8
Alice Ryhl <aliceryhl@google.com>
2021-09-28 11:57:22 +0000

    add Rust async code generation
    Test: golden tests
    Change-Id: I3ea52393fb54d9d4d513f09f93454d7f1510c286

--- tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/INamedCallback.rs
+++ tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/INamedCallback.rs
@@ -8,6 +8,7 @@ declare_binder_interface! {
     native: BnNamedCallback(on_transact),
     proxy: BpNamedCallback {
     },
+    async: INamedCallbackAsync,
   }
 }
 pub trait INamedCallback: binder::Interface + Send {
@@ -19,6 +20,10 @@ pub trait INamedCallback: binder::Interface + Send {
   fn setDefaultImpl(d: INamedCallbackDefaultRef) -> INamedCallbackDefaultRef where Self: Sized {
     std::mem::replace(&mut *DEFAULT_IMPL.lock().unwrap(), d)
   }
+}
+pub trait INamedCallbackAsync<P>: binder::Interface + Send {
+  fn get_descriptor() -> &'static str where Self: Sized { "android.aidl.tests.INamedCallback" }
+  fn GetName<'a>(&'a self) -> binder::BoxFuture<'a, binder::public_api::Result<String>>;
 }
 pub trait INamedCallbackDefault: Send + Sync {
   fn GetName(&self) -> binder::public_api::Result<String> {
@@ -33,21 +38,45 @@ use lazy_static::lazy_static;
 lazy_static! {
   static ref DEFAULT_IMPL: std::sync::Mutex<INamedCallbackDefaultRef> = std::sync::Mutex::new(None);
 }
-impl INamedCallback for BpNamedCallback {
-  fn GetName(&self) -> binder::public_api::Result<String> {
-    let _aidl_reply = self.binder.transact(transactions::GetName, binder::FLAG_PRIVATE_LOCAL, |_aidl_data| {
-      Ok(())
-    });
+impl BpNamedCallback {
+  fn build_parcel_GetName(&self) -> binder::public_api::Result<binder::OwnedParcel> {
+    let mut aidl_data_owned = self.binder.prepare_transact()?;
+    let mut aidl_data = aidl_data_owned.borrowed();
+    Ok(aidl_data_owned)
+  }
+  fn read_response_GetName(&self, _aidl_reply: binder::Result<binder::OwnedParcel>) -> binder::public_api::Result<String> {
     if let Err(binder::StatusCode::UNKNOWN_TRANSACTION) = _aidl_reply {
       if let Some(_aidl_default_impl) = <Self as INamedCallback>::getDefaultImpl() {
         return _aidl_default_impl.GetName();
       }
     }
-    let _aidl_reply = _aidl_reply?;
+    let _aidl_reply = _aidl_reply?.into_parcel();
     let _aidl_status: binder::Status = _aidl_reply.read()?;
     if !_aidl_status.is_ok() { return Err(_aidl_status); }
     let _aidl_return: String = _aidl_reply.read()?;
     Ok(_aidl_return)
+  }
+}
+impl INamedCallback for BpNamedCallback {
+  fn GetName(&self) -> binder::public_api::Result<String> {
+    let _aidl_data = self.build_parcel_GetName()?;
+    let _aidl_reply = self.binder.submit_transact(transactions::GetName, _aidl_data, binder::FLAG_PRIVATE_LOCAL);
+    self.read_response_GetName(_aidl_reply)
+  }
+}
+impl<P: binder::BinderAsyncPool> INamedCallbackAsync<P> for BpNamedCallback {
+  fn GetName<'a>(&'a self) -> binder::BoxFuture<'a, binder::public_api::Result<String>> {
+    let _aidl_data = match self.build_parcel_GetName() {
+      Ok(_aidl_data) => _aidl_data,
+      Err(err) => return Box::pin(std::future::ready(Err(err))),
+    };
+    let binder = self.binder.clone();
+    P::spawn(
+      move || binder.submit_transact(transactions::GetName, _aidl_data, binder::FLAG_PRIVATE_LOCAL),
+      move |_aidl_reply| async move {
+        self.read_response_GetName(_aidl_reply)
+      }
+    )
   }
 }
 impl INamedCallback for binder::Binder<BnNamedCallback> {
