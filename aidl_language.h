9e77445a972b657680dd2d0da398020469ecff44
openvela-robot <openvela-robot@xiaomi.com>
2021-09-29 08:00:49 +0800

    Adding AidlCast to replace downcasting methods am: baa7106219 am: be93dc1623 am: 1b3ab3a146
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1839633
    
    Change-Id: I8a2484415e596c668e66d66d3659c3670d6c38b2

--- aidl_language.h
+++ aidl_language.h
@@ -458,7 +458,7 @@ class AidlTypeSpecifier final : public AidlAnnotatable,
 // Returns the universal value unaltered.
 std::string AidlConstantValueDecorator(const AidlTypeSpecifier& type, const std::string& raw_value);
 
-class AidlMember : public AidlAnnotatable {
+class AidlMember : public AidlCommentable {
  public:
   AidlMember(const AidlLocation& location, const Comments& comments);
   virtual ~AidlMember() = default;
@@ -894,7 +894,7 @@ class AidlMethod : public AidlMember {
 
 // AidlDefinedType represents either an interface, parcelable, or enum that is
 // defined in the source file.
-class AidlDefinedType : public AidlMember, public AidlScope {
+class AidlDefinedType : public AidlAnnotatable, public AidlScope {
  public:
   AidlDefinedType(const AidlLocation& location, const std::string& name, const Comments& comments,
                   const std::string& package, std::vector<std::unique_ptr<AidlMember>>* members);
@@ -963,8 +963,7 @@ class AidlDefinedType : public AidlMember, public AidlScope {
     return const_cast<AidlParcelable*>(
         const_cast<const AidlDefinedType*>(this)->AsUnstructuredParcelable());
   }
-  const AidlDefinedType* GetParentType() const;
-  const std::vector<std::unique_ptr<AidlDefinedType>>& GetNestedTypes() const { return types_; }
+
   const std::vector<std::unique_ptr<AidlVariableDeclaration>>& GetFields() const {
     return variables_;
   }
@@ -993,7 +992,6 @@ class AidlDefinedType : public AidlMember, public AidlScope {
   std::vector<std::unique_ptr<AidlVariableDeclaration>> variables_;
   std::vector<std::unique_ptr<AidlConstantDeclaration>> constants_;
   std::vector<std::unique_ptr<AidlMethod>> methods_;
-  std::vector<std::unique_ptr<AidlDefinedType>> types_;
   std::vector<const AidlMember*> members_;  // keep members in order of appearance.
 };
 
