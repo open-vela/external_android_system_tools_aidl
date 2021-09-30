17dbb142b46926151366b22acadf88925f3f9396
liujinye <liujinye@xiaomi.com>
2021-10-01 03:54:07 +0800

    Remove unused functions in generate_cpp am: c0f59a4cf5 am: 7c7b48bc27 am: 9c76bd4d6d am: ca610d6e99 am: 33ddcda099
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1842934
    
    Change-Id: I240e1d12613601bf567dbaab20fd410b0ae66b64

--- tests/golden_output/aidl-test-interface-cpp-source/gen/android/aidl/tests/UnionWithFd.cpp
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/android/aidl/tests/UnionWithFd.cpp
@@ -1,8 +1,11 @@
 #include <android/aidl/tests/UnionWithFd.h>
 
 namespace android {
+
 namespace aidl {
+
 namespace tests {
+
 ::android::status_t UnionWithFd::readFromParcel(const ::android::Parcel* _aidl_parcel) {
   ::android::status_t _aidl_ret_status;
   int32_t _aidl_tag;
@@ -31,6 +34,7 @@ namespace tests {
   }
   return ::android::BAD_VALUE;
 }
+
 ::android::status_t UnionWithFd::writeToParcel(::android::Parcel* _aidl_parcel) const {
   ::android::status_t _aidl_ret_status = _aidl_parcel->writeInt32(getTag());
   if (_aidl_ret_status != ::android::OK) return _aidl_ret_status;
@@ -40,6 +44,9 @@ namespace tests {
   }
   __assert2(__FILE__, __LINE__, __PRETTY_FUNCTION__, "can't reach here");
 }
+
 }  // namespace tests
+
 }  // namespace aidl
+
 }  // namespace android
