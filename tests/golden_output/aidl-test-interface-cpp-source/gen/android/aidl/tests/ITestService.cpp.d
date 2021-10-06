bd01d25e1869282da4966015e7319f7be6bd8426
liujinye <liujinye@xiaomi.com>
2021-10-07 03:07:36 +0800

    Split type resolution from constref resolution am: e1abd594b4 am: ac300ac9a0 am: 662b9dfb14 am: a657ac8213
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1844188
    
    Change-Id: I0e70464ca2db9e576c976d89f96b42082a524545

--- tests/golden_output/aidl-test-interface-cpp-source/gen/android/aidl/tests/ITestService.cpp.d
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/android/aidl/tests/ITestService.cpp.d
@@ -8,4 +8,6 @@ out/soong/.intermediates/system/tools/aidl/aidl-test-interface-cpp-source/gen/an
   system/tools/aidl/tests/android/aidl/tests/IntEnum.aidl \
   system/tools/aidl/tests/android/aidl/tests/LongEnum.aidl \
   system/tools/aidl/tests/android/aidl/tests/RecursiveList.aidl \
-  system/tools/aidl/tests/android/aidl/tests/StructuredParcelable.aidl
+  system/tools/aidl/tests/android/aidl/tests/StructuredParcelable.aidl \
+  system/tools/aidl/tests/android/aidl/tests/ConstantExpressionEnum.aidl \
+  system/tools/aidl/tests/android/aidl/tests/Union.aidl
