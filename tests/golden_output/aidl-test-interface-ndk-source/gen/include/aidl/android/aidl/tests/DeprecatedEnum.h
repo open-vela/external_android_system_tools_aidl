98bea508e290093605e7abf0a4c719fec5b7066b
openvela-robot <openvela-robot@xiaomi.com>
2021-10-07 02:59:22 +0800

    Support nested types in the NDK backend am: a265009d50 am: 3d51f11bab am: 2298807ffd
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1845980
    
    Change-Id: I74013dee3210923a27e38aacda9d81e7d80bbfe1

--- tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/DeprecatedEnum.h
+++ tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/DeprecatedEnum.h
@@ -1,15 +1,16 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <memory>
 #include <optional>
 #include <string>
 #include <vector>
+#include <android/binder_enums.h>
 #ifdef BINDER_STABILITY_SUPPORT
 #include <android/binder_stability.h>
 #endif  // BINDER_STABILITY_SUPPORT
-#include <array>
-#include <android/binder_enums.h>
+
 namespace aidl {
 namespace android {
 namespace aidl {
@@ -20,6 +21,14 @@ enum class __attribute__((deprecated("test"))) DeprecatedEnum : int32_t {
   C = 2,
 };
 
+}  // namespace tests
+}  // namespace aidl
+}  // namespace android
+}  // namespace aidl
+namespace aidl {
+namespace android {
+namespace aidl {
+namespace tests {
 [[nodiscard]] static inline std::string toString(DeprecatedEnum val) __attribute__((deprecated("test")));
 [[nodiscard]] static inline std::string toString(DeprecatedEnum val) {
   switch(val) {
@@ -49,4 +58,4 @@ constexpr inline std::array<aidl::android::aidl::tests::DeprecatedEnum, 3> __att
 };
 #pragma clang diagnostic pop
 }  // namespace internal
-}  // namespace android
+}  // namespace ndk
