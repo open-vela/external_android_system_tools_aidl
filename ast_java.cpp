c0375b64b6a48ee3e3a4567887fb007702628fc3
liujinye <liujinye@xiaomi.com>
2021-10-08 02:52:01 +0800

    Remove Document from ast_java am: 7445d2883a am: 79b7e2195a am: b9d7ef585f am: 4498963ff0
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1849356
    
    Change-Id: I39190aaaa53347110652a010973751a5d4f5f6d5

--- ast_java.cpp
+++ ast_java.cpp
@@ -436,31 +436,6 @@ void Class::Write(CodeWriter* to) const {
   to->Write("}\n");
 }
 
-Document::Document(const std::string& comment,
-                   const std::string& package,
-                   std::unique_ptr<Class> clazz)
-    : comment_(comment),
-      package_(package),
-      clazz_(std::move(clazz)) {
-}
-
-void Document::Write(CodeWriter* to) const {
-  if (!comment_.empty()) {
-    to->Write("%s\n", comment_.c_str());
-  }
-  to->Write(
-      "/*\n"
-      " * This file is auto-generated.  DO NOT MODIFY.\n"
-      " */\n");
-  if (!package_.empty()) {
-    to->Write("package %s;\n", package_.c_str());
-  }
-
-  if (clazz_) {
-    clazz_->Write(to);
-  }
-}
-
 std::shared_ptr<Expression> NULL_VALUE = std::make_shared<LiteralExpression>("null");
 std::shared_ptr<Expression> THIS_VALUE = std::make_shared<LiteralExpression>("this");
 std::shared_ptr<Expression> SUPER_VALUE = std::make_shared<LiteralExpression>("super");
