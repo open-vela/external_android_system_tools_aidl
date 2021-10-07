fc556cc3f9fae8d7aac803f8d4d8d84872ab2e32
Jooyung Han <jooyung@google.com>
2021-10-07 17:36:22 +0900

    Remove unnecessary blank link in Java's union
    This is a preparation for further refactoring to remove "Document" of
    ast_java.
    
    Bug: n/a
    Test: golden_test.sh check
    Change-Id: I7a324a56dce859793327a251fc62df0df80c1354

--- tests/golden_output/aidl-test-interface-java-source/gen/android/aidl/tests/Union.java
+++ tests/golden_output/aidl-test-interface-java-source/gen/android/aidl/tests/Union.java
@@ -2,7 +2,6 @@
  * This file is auto-generated.  DO NOT MODIFY.
  */
 package android.aidl.tests;
-
 public final class Union implements android.os.Parcelable {
   // tags for union fields
   public final static int ns = 0;  // int[] ns;
