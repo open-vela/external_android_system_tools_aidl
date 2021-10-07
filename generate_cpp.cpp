eb76a658e22dadc59ce98c22eb42b611073fea37
openvela-robot <openvela-robot@xiaomi.com>
2021-10-08 02:03:35 +0800

    Remove unnecessary blank link in Java's union am: fc556cc3f9 am: e27c753896
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1849374
    
    Change-Id: I8be0cea68ad7fed802565ffe6506ebff324d745b

--- generate_cpp.cpp
+++ generate_cpp.cpp
@@ -519,17 +519,45 @@ void GenerateConstantDefinitions(CodeWriter& out, const AidlDefinedType& type,
 
 void GenerateConstantDeclarations(CodeWriter& out, const AidlDefinedType& type,
                                   const AidlTypenames& typenames) {
+  std::vector<std::unique_ptr<Declaration>> string_constants;
+  unique_ptr<Enum> byte_constant_enum{new Enum{"", "int8_t", false}};
+  unique_ptr<Enum> int_constant_enum{new Enum{"", "int32_t", false}};
+  unique_ptr<Enum> long_constant_enum{new Enum{"", "int64_t", false}};
   for (const auto& constant : type.GetConstantDeclarations()) {
     const AidlTypeSpecifier& type = constant->GetType();
-    const auto cpp_type = CppNameOf(type, typenames);
+    const AidlConstantValue& value = constant->GetValue();
+
+    const string attribute = GetDeprecatedAttribute(*constant);
     if (type.Signature() == "String") {
-      out << "static const " << cpp_type << "& " << constant->GetName() << "()";
-      cpp::GenerateDeprecated(out, *constant);
-      out << ";\n";
+      std::string cppType = CppNameOf(constant->GetType(), typenames);
+      unique_ptr<Declaration> getter(new MethodDecl("const " + cppType + "&", constant->GetName(),
+                                                    {}, MethodDecl::IS_STATIC, attribute));
+      string_constants.push_back(std::move(getter));
+    } else if (type.Signature() == "byte") {
+      byte_constant_enum->AddValue(constant->GetName(),
+                                   constant->ValueString(ConstantValueDecorator), attribute);
+    } else if (type.Signature() == "int") {
+      int_constant_enum->AddValue(constant->GetName(),
+                                  constant->ValueString(ConstantValueDecorator), attribute);
+    } else if (type.Signature() == "long") {
+      long_constant_enum->AddValue(constant->GetName(),
+                                   constant->ValueString(ConstantValueDecorator), attribute);
     } else {
-      out << "enum : " << cpp_type << " { " << constant->GetName();
-      cpp::GenerateDeprecated(out, *constant);
-      out << " = " << constant->ValueString(ConstantValueDecorator) << " };\n";
+      AIDL_FATAL(value) << "Unrecognized constant type: " << type.Signature();
+    }
+  }
+  if (byte_constant_enum->HasValues()) {
+    byte_constant_enum->Write(&out);
+  }
+  if (int_constant_enum->HasValues()) {
+    int_constant_enum->Write(&out);
+  }
+  if (long_constant_enum->HasValues()) {
+    long_constant_enum->Write(&out);
+  }
+  if (!string_constants.empty()) {
+    for (auto& string_constant : string_constants) {
+      string_constant->Write(&out);
     }
   }
 }
@@ -1086,6 +1114,13 @@ void GenerateParcelFields(CodeWriter& out, const AidlStructuredParcelable& decl,
       } else {
         out << " { ::android::Parcelable::Stability::STABILITY_LOCAL }";
       }
+    } else if (auto type = variable->GetType().GetDefinedType(); type) {
+      if (auto enum_type = type->AsEnumDeclaration(); enum_type) {
+        if (!variable->GetType().IsArray()) {
+          // if an enum doesn't have explicit default value, do zero-initialization
+          out << " = " << cppType << "(0)";
+        }
+      }
     }
     out << ";\n";
   }
@@ -1179,10 +1214,53 @@ void GenerateParcelSource(CodeWriter& out, const T& parcel, const AidlTypenames&
   LeaveNamespace(out, parcel);
 }
 
+// toString(enum_type) is defined in the same namespace, but not in other enclosing types.
+// So enum_decl should be partially qualified (based on namespace).
+std::string GenerateEnumToString(const AidlTypenames& typenames,
+                                 const AidlEnumDeclaration& enum_decl) {
+  const auto q_name = GetQualifiedName(enum_decl);
+  std::ostringstream code;
+  code << "[[nodiscard]]";
+  GenerateDeprecated(code, enum_decl);
+  code << " static inline std::string toString(" << q_name << " val)";
+  code << " {\n";
+  code << "  switch(val) {\n";
+  std::set<std::string> unique_cases;
+  for (const auto& enumerator : enum_decl.GetEnumerators()) {
+    std::string c = enumerator->ValueString(enum_decl.GetBackingType(), ConstantValueDecorator);
+    // Only add a case if its value has not yet been used in the switch
+    // statement. C++ does not allow multiple cases with the same value, but
+    // enums does allow this. In this scenario, the first declared
+    // enumerator with the given value is printed.
+    if (unique_cases.count(c) == 0) {
+      unique_cases.insert(c);
+      code << "  case " << q_name << "::" << enumerator->GetName() << ":\n";
+      code << "    return \"" << enumerator->GetName() << "\";\n";
+    }
+  }
+  code << "  default:\n";
+  code << "    return std::to_string(static_cast<"
+       << CppNameOf(enum_decl.GetBackingType(), typenames) << ">(val));\n";
+  code << "  }\n";
+  code << "}\n";
+  return code.str();
+}
+
 void GenerateEnumClassDecl(CodeWriter& out, const AidlEnumDeclaration& enum_decl,
                            const AidlTypenames& typenames) {
-  cpp::GenerateEnumClassDecl(out, enum_decl, CppNameOf(enum_decl.GetBackingType(), typenames),
-                             ConstantValueDecorator);
+  const string clazz = enum_decl.GetName();
+  const string backing_type = CppNameOf(enum_decl.GetBackingType(), typenames);
+
+  out << "enum class";
+  GenerateDeprecated(out, enum_decl);
+  out << " " << clazz << " : " << backing_type << " {\n";
+  out.Indent();
+  for (const auto& enumerator : enum_decl.GetEnumerators()) {
+    out << enumerator->GetName() << " = "
+        << enumerator->ValueString(enum_decl.GetBackingType(), ConstantValueDecorator) << ",\n";
+  }
+  out.Dedent();
+  out << "};\n";
 }
 
 void GenerateClassDecl(CodeWriter& out, const AidlDefinedType& defined_type,
@@ -1302,9 +1380,8 @@ void GenerateHeaderDefinitions(CodeWriter& out, const AidlDefinedType& defined_t
         : out(out), typenames(typenames), options(options) {}
 
     void Visit(const AidlEnumDeclaration& enum_decl) override {
-      const auto backing_type = CppNameOf(enum_decl.GetBackingType(), typenames);
       EnterNamespace(out, enum_decl);
-      out << GenerateEnumToString(enum_decl, backing_type);
+      out << GenerateEnumToString(typenames, enum_decl);
       LeaveNamespace(out, enum_decl);
 
       out << "namespace android {\n";
@@ -1379,6 +1456,7 @@ void GenerateServerHeader(CodeWriter& out, const AidlDefinedType& defined_type,
   }
 }
 
+// TODO(b/182508839) should emit nested types recursively
 void GenerateSource(CodeWriter& out, const AidlDefinedType& defined_type,
                     const AidlTypenames& typenames, const Options& options) {
   struct Visitor : AidlVisitor {
