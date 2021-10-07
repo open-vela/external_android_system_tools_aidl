95b7dc1c48549dc05968342dd10ade6ff17175a6
openvela-robot <openvela-robot@xiaomi.com>
2021-10-08 02:36:28 +0800

    Remove unnecessary blank link in Java's union am: fc556cc3f9 am: e27c753896 am: b806529ad3
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1849374
    
    Change-Id: I0580d209d6a676d208b771b0f8201ffe1ad2b2cd

--- tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/DeprecatedEnum.h
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/DeprecatedEnum.h
@@ -19,8 +19,7 @@ enum class __attribute__((deprecated("test"))) DeprecatedEnum : int32_t {
 namespace android {
 namespace aidl {
 namespace tests {
-[[nodiscard]] static inline std::string toString(DeprecatedEnum val) __attribute__((deprecated("test")));
-[[nodiscard]] static inline std::string toString(DeprecatedEnum val) {
+[[nodiscard]] __attribute__((deprecated("test"))) static inline std::string toString(DeprecatedEnum val) {
   switch(val) {
   case DeprecatedEnum::A:
     return "A";
