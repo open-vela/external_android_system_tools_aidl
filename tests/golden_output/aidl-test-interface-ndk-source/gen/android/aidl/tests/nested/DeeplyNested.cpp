fc85f0725d537c7c36a2a329630d0bffc8539d2c
openvela-robot <openvela-robot@xiaomi.com>
2021-10-18 07:41:32 +0800

    Fix NDK backend's identifier conflict am: e358e32d11
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1860313
    
    Change-Id: I0bab33156ec2dacf08c29db3818d9e5b3365e8b0

--- tests/golden_output/aidl-test-interface-ndk-source/gen/android/aidl/tests/nested/DeeplyNested.cpp
+++ tests/golden_output/aidl-test-interface-ndk-source/gen/android/aidl/tests/nested/DeeplyNested.cpp
@@ -9,27 +9,27 @@ namespace tests {
 namespace nested {
 const char* DeeplyNested::descriptor = "android.aidl.tests.nested.DeeplyNested";
 
-binder_status_t DeeplyNested::readFromParcel(const AParcel* parcel) {
+binder_status_t DeeplyNested::readFromParcel(const AParcel* _aidl_parcel) {
   int32_t _aidl_parcelable_size;
-  int32_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  binder_status_t _aidl_ret_status = AParcel_readInt32(parcel, &_aidl_parcelable_size);
+  int32_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  binder_status_t _aidl_ret_status = AParcel_readInt32(_aidl_parcel, &_aidl_parcelable_size);
   if (_aidl_start_pos > INT32_MAX - _aidl_parcelable_size) return STATUS_BAD_VALUE;
   if (_aidl_parcelable_size < 0) return STATUS_BAD_VALUE;
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
-binder_status_t DeeplyNested::writeToParcel(AParcel* parcel) const {
+binder_status_t DeeplyNested::writeToParcel(AParcel* _aidl_parcel) const {
   binder_status_t _aidl_ret_status;
-  size_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  _aidl_ret_status = AParcel_writeInt32(parcel, 0);
+  size_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  _aidl_ret_status = AParcel_writeInt32(_aidl_parcel, 0);
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  size_t _aidl_end_pos = AParcel_getDataPosition(parcel);
-  AParcel_setDataPosition(parcel, _aidl_start_pos);
-  AParcel_writeInt32(parcel, _aidl_end_pos - _aidl_start_pos);
-  AParcel_setDataPosition(parcel, _aidl_end_pos);
+  size_t _aidl_end_pos = AParcel_getDataPosition(_aidl_parcel);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos);
+  AParcel_writeInt32(_aidl_parcel, _aidl_end_pos - _aidl_start_pos);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_end_pos);
   return _aidl_ret_status;
 }
 
@@ -49,37 +49,37 @@ namespace tests {
 namespace nested {
 const char* DeeplyNested::A::descriptor = "android.aidl.tests.nested.DeeplyNested.A";
 
-binder_status_t DeeplyNested::A::readFromParcel(const AParcel* parcel) {
+binder_status_t DeeplyNested::A::readFromParcel(const AParcel* _aidl_parcel) {
   int32_t _aidl_parcelable_size;
-  int32_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  binder_status_t _aidl_ret_status = AParcel_readInt32(parcel, &_aidl_parcelable_size);
+  int32_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  binder_status_t _aidl_ret_status = AParcel_readInt32(_aidl_parcel, &_aidl_parcelable_size);
   if (_aidl_start_pos > INT32_MAX - _aidl_parcelable_size) return STATUS_BAD_VALUE;
   if (_aidl_parcelable_size < 0) return STATUS_BAD_VALUE;
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  if (AParcel_getDataPosition(parcel) - _aidl_start_pos >= _aidl_parcelable_size) {
-    AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
+  if (AParcel_getDataPosition(_aidl_parcel) - _aidl_start_pos >= _aidl_parcelable_size) {
+    AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos + _aidl_parcelable_size);
     return _aidl_ret_status;
   }
-  _aidl_ret_status = AParcel_readByte(parcel, reinterpret_cast<int8_t*>(&e));
+  _aidl_ret_status = AParcel_readByte(_aidl_parcel, reinterpret_cast<int8_t*>(&e));
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
-binder_status_t DeeplyNested::A::writeToParcel(AParcel* parcel) const {
+binder_status_t DeeplyNested::A::writeToParcel(AParcel* _aidl_parcel) const {
   binder_status_t _aidl_ret_status;
-  size_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  _aidl_ret_status = AParcel_writeInt32(parcel, 0);
+  size_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  _aidl_ret_status = AParcel_writeInt32(_aidl_parcel, 0);
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  _aidl_ret_status = AParcel_writeByte(parcel, static_cast<int8_t>(e));
+  _aidl_ret_status = AParcel_writeByte(_aidl_parcel, static_cast<int8_t>(e));
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  size_t _aidl_end_pos = AParcel_getDataPosition(parcel);
-  AParcel_setDataPosition(parcel, _aidl_start_pos);
-  AParcel_writeInt32(parcel, _aidl_end_pos - _aidl_start_pos);
-  AParcel_setDataPosition(parcel, _aidl_end_pos);
+  size_t _aidl_end_pos = AParcel_getDataPosition(_aidl_parcel);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos);
+  AParcel_writeInt32(_aidl_parcel, _aidl_end_pos - _aidl_start_pos);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_end_pos);
   return _aidl_ret_status;
 }
 
@@ -99,27 +99,27 @@ namespace tests {
 namespace nested {
 const char* DeeplyNested::B::descriptor = "android.aidl.tests.nested.DeeplyNested.B";
 
-binder_status_t DeeplyNested::B::readFromParcel(const AParcel* parcel) {
+binder_status_t DeeplyNested::B::readFromParcel(const AParcel* _aidl_parcel) {
   int32_t _aidl_parcelable_size;
-  int32_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  binder_status_t _aidl_ret_status = AParcel_readInt32(parcel, &_aidl_parcelable_size);
+  int32_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  binder_status_t _aidl_ret_status = AParcel_readInt32(_aidl_parcel, &_aidl_parcelable_size);
   if (_aidl_start_pos > INT32_MAX - _aidl_parcelable_size) return STATUS_BAD_VALUE;
   if (_aidl_parcelable_size < 0) return STATUS_BAD_VALUE;
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
-binder_status_t DeeplyNested::B::writeToParcel(AParcel* parcel) const {
+binder_status_t DeeplyNested::B::writeToParcel(AParcel* _aidl_parcel) const {
   binder_status_t _aidl_ret_status;
-  size_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  _aidl_ret_status = AParcel_writeInt32(parcel, 0);
+  size_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  _aidl_ret_status = AParcel_writeInt32(_aidl_parcel, 0);
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  size_t _aidl_end_pos = AParcel_getDataPosition(parcel);
-  AParcel_setDataPosition(parcel, _aidl_start_pos);
-  AParcel_writeInt32(parcel, _aidl_end_pos - _aidl_start_pos);
-  AParcel_setDataPosition(parcel, _aidl_end_pos);
+  size_t _aidl_end_pos = AParcel_getDataPosition(_aidl_parcel);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos);
+  AParcel_writeInt32(_aidl_parcel, _aidl_end_pos - _aidl_start_pos);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_end_pos);
   return _aidl_ret_status;
 }
 
@@ -139,27 +139,27 @@ namespace tests {
 namespace nested {
 const char* DeeplyNested::B::C::descriptor = "android.aidl.tests.nested.DeeplyNested.B.C";
 
-binder_status_t DeeplyNested::B::C::readFromParcel(const AParcel* parcel) {
+binder_status_t DeeplyNested::B::C::readFromParcel(const AParcel* _aidl_parcel) {
   int32_t _aidl_parcelable_size;
-  int32_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  binder_status_t _aidl_ret_status = AParcel_readInt32(parcel, &_aidl_parcelable_size);
+  int32_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  binder_status_t _aidl_ret_status = AParcel_readInt32(_aidl_parcel, &_aidl_parcelable_size);
   if (_aidl_start_pos > INT32_MAX - _aidl_parcelable_size) return STATUS_BAD_VALUE;
   if (_aidl_parcelable_size < 0) return STATUS_BAD_VALUE;
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
-binder_status_t DeeplyNested::B::C::writeToParcel(AParcel* parcel) const {
+binder_status_t DeeplyNested::B::C::writeToParcel(AParcel* _aidl_parcel) const {
   binder_status_t _aidl_ret_status;
-  size_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  _aidl_ret_status = AParcel_writeInt32(parcel, 0);
+  size_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  _aidl_ret_status = AParcel_writeInt32(_aidl_parcel, 0);
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  size_t _aidl_end_pos = AParcel_getDataPosition(parcel);
-  AParcel_setDataPosition(parcel, _aidl_start_pos);
-  AParcel_writeInt32(parcel, _aidl_end_pos - _aidl_start_pos);
-  AParcel_setDataPosition(parcel, _aidl_end_pos);
+  size_t _aidl_end_pos = AParcel_getDataPosition(_aidl_parcel);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos);
+  AParcel_writeInt32(_aidl_parcel, _aidl_end_pos - _aidl_start_pos);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_end_pos);
   return _aidl_ret_status;
 }
 
@@ -179,27 +179,27 @@ namespace tests {
 namespace nested {
 const char* DeeplyNested::B::C::D::descriptor = "android.aidl.tests.nested.DeeplyNested.B.C.D";
 
-binder_status_t DeeplyNested::B::C::D::readFromParcel(const AParcel* parcel) {
+binder_status_t DeeplyNested::B::C::D::readFromParcel(const AParcel* _aidl_parcel) {
   int32_t _aidl_parcelable_size;
-  int32_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  binder_status_t _aidl_ret_status = AParcel_readInt32(parcel, &_aidl_parcelable_size);
+  int32_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  binder_status_t _aidl_ret_status = AParcel_readInt32(_aidl_parcel, &_aidl_parcelable_size);
   if (_aidl_start_pos > INT32_MAX - _aidl_parcelable_size) return STATUS_BAD_VALUE;
   if (_aidl_parcelable_size < 0) return STATUS_BAD_VALUE;
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  AParcel_setDataPosition(parcel, _aidl_start_pos + _aidl_parcelable_size);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
-binder_status_t DeeplyNested::B::C::D::writeToParcel(AParcel* parcel) const {
+binder_status_t DeeplyNested::B::C::D::writeToParcel(AParcel* _aidl_parcel) const {
   binder_status_t _aidl_ret_status;
-  size_t _aidl_start_pos = AParcel_getDataPosition(parcel);
-  _aidl_ret_status = AParcel_writeInt32(parcel, 0);
+  size_t _aidl_start_pos = AParcel_getDataPosition(_aidl_parcel);
+  _aidl_ret_status = AParcel_writeInt32(_aidl_parcel, 0);
   if (_aidl_ret_status != STATUS_OK) return _aidl_ret_status;
 
-  size_t _aidl_end_pos = AParcel_getDataPosition(parcel);
-  AParcel_setDataPosition(parcel, _aidl_start_pos);
-  AParcel_writeInt32(parcel, _aidl_end_pos - _aidl_start_pos);
-  AParcel_setDataPosition(parcel, _aidl_end_pos);
+  size_t _aidl_end_pos = AParcel_getDataPosition(_aidl_parcel);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_start_pos);
+  AParcel_writeInt32(_aidl_parcel, _aidl_end_pos - _aidl_start_pos);
+  AParcel_setDataPosition(_aidl_parcel, _aidl_end_pos);
   return _aidl_ret_status;
 }
 
