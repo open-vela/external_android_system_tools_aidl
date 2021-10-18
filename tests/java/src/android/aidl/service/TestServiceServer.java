51a6878cf1f1ea513ccbd49aeccbf5e487b934c5
liujinye <liujinye@xiaomi.com>
2021-10-18 08:36:27 +0800

    Fix NDK backend's identifier conflict am: e358e32d11 am: 00753f8c05 am: ce89bd6c4d am: 86a9724831 am: 780cf699dd
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1860313
    
    Change-Id: I2dc0a40b3ca7b3f0b699b25746bd4db7ec61cb1e

--- tests/java/src/android/aidl/service/TestServiceServer.java
+++ tests/java/src/android/aidl/service/TestServiceServer.java
@@ -365,15 +365,6 @@ public class TestServiceServer extends ITestService.Stub {
     // do nothing
   }
   @Override
-  public void TakesAnIBinderList(List<IBinder> input) throws RemoteException {
-    // do nothing
-  }
-  @Override
-  public void TakesANullableIBinderList(List<IBinder> input) throws RemoteException {
-    // do nothing
-  }
-
-  @Override
   public String RepeatUtf8CppString(String token) throws RemoteException {
     return token;
   }
@@ -586,6 +577,10 @@ public class TestServiceServer extends ITestService.Stub {
       }
       return reversed;
     }
+    @Override
+    public void TakesAnIBinderList(List<IBinder> input) throws RemoteException {}
+    @Override
+    public void TakesANullableIBinderList(List<IBinder> input) throws RemoteException {}
   }
 
   @Override
