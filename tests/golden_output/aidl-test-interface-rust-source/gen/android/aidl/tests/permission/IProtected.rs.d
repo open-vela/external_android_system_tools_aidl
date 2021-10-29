133da84076cdac4e61d9c6002c44c973bbcc468b
Thiébaud Weksteen <tweek@google.com>
2021-10-29 16:49:58 +1100

    Accept @Enforce on interface definition
    The @Enforce annotation may be used on interfaces in combination with
    annotations at the method level. If both are present, both are verified.
    For now, this is only implemented for the Java backend.
    
    Move permission-related golden tests to its own directory.
    
    Bug: 197828948
    Test: atest --host aidl_unittests
    Change-Id: I57fe072c616ddc2652bff911adbda1a79841742a

---
+++ tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/permission/IProtected.rs.d
@@ -0,0 +1,2 @@
+out/soong/.intermediates/system/tools/aidl/aidl-test-interface-rust-source/gen/android/aidl/tests/permission/IProtected.rs : \
+  system/tools/aidl/tests/android/aidl/tests/permission/IProtected.aidl
