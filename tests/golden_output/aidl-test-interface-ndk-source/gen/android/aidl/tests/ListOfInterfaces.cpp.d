c91806d64622e21746a15c088a8d480d3e4adbae
liujinye <liujinye@xiaomi.com>
2021-11-10 02:10:02 +0800

    Snap for 7899729 from 6b30d4e4cd7eb7542e303389f21cf31ecc4ead85 to tm-release
    Change-Id: I920dcc7926b52b01d3185d65b6f1ffc661cb2a68

--- tests/golden_output/aidl-test-interface-ndk-source/gen/android/aidl/tests/ListOfInterfaces.cpp.d
+++
@@ -1,2 +0,0 @@
-out/soong/.intermediates/system/tools/aidl/aidl-test-interface-ndk-source/gen/android/aidl/tests/ListOfInterfaces.cpp : \
-  system/tools/aidl/tests/android/aidl/tests/ListOfInterfaces.aidl
