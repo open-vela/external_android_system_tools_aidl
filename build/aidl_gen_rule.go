9ab5301cca9266b0f967b74bc43287407c84431a
liujinye <liujinye@xiaomi.com>
2021-11-10 02:09:08 +0800

    Use Parcel methods for interface[] am: 14fc8ee793 am: 5cf728122b am: a8d49c4181 am: 001a86ec80 am: 8003b03844
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1881017
    
    Change-Id: Ifc69106cd576207ef8e93a150c5c5ff3e8b13ad4

--- build/aidl_gen_rule.go
+++ build/aidl_gen_rule.go
@@ -192,8 +192,10 @@ func (g *aidlGenRule) generateBuildActionsForSingleAidl(ctx android.ModuleContex
 	if g.properties.Platform_apis {
 		optionalFlags = append(optionalFlags, "--min_sdk_version platform_apis")
 	} else {
-		minSdkVer := proptools.StringDefault(g.properties.Min_sdk_version, "current")
-		optionalFlags = append(optionalFlags, "--min_sdk_version "+minSdkVer)
+		minSdkVer := g.properties.Min_sdk_version
+		if minSdkVer != nil {
+			optionalFlags = append(optionalFlags, "--min_sdk_version "+*minSdkVer)
+		}
 	}
 	optionalFlags = append(optionalFlags, wrap("-p", g.deps.preprocessed.Strings(), "")...)
 
