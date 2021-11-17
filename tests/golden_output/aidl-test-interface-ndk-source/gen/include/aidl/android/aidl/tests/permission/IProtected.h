2b8dbc22bdf5307ad6f707ec849ca4aee2d53e42
liujinye <liujinye@xiaomi.com>
2021-11-17 16:41:15 +0800

    Add automatic default value for char-type field
    char type fields are auto-initialized with '\0' when not specified.
    
    Ignore-AOSP-First: security fix
    Bug: 206718630
    Test: aidl_unittests
    Merged-In: I15cfe2a90c0c9adca1692d9a9c997901b90bbbb6
    Change-Id: I15cfe2a90c0c9adca1692d9a9c997901b90bbbb6
    (cherry picked from commit f04d003b4f5fa9f8ee73b6b560c897de45e644cd)

---
+++ tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/permission/IProtected.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <android/binder_interface_utils.h>
+#ifdef BINDER_STABILITY_SUPPORT
+#include <android/binder_stability.h>
+#endif  // BINDER_STABILITY_SUPPORT
+
+namespace aidl {
+namespace android {
+namespace aidl {
+namespace tests {
+namespace permission {
+class IProtected : public ::ndk::ICInterface {
+public:
+  static const char* descriptor;
+  IProtected();
+  virtual ~IProtected();
+
+  static constexpr uint32_t TRANSACTION_PermissionProtected = FIRST_CALL_TRANSACTION + 0;
+  static constexpr uint32_t TRANSACTION_MultiplePermissions = FIRST_CALL_TRANSACTION + 1;
+  static constexpr uint32_t TRANSACTION_MultiplePermissions2 = FIRST_CALL_TRANSACTION + 2;
+
+  static std::shared_ptr<IProtected> fromBinder(const ::ndk::SpAIBinder& binder);
+  static binder_status_t writeToParcel(AParcel* parcel, const std::shared_ptr<IProtected>& instance);
+  static binder_status_t readFromParcel(const AParcel* parcel, std::shared_ptr<IProtected>* instance);
+  static bool setDefaultImpl(const std::shared_ptr<IProtected>& impl);
+  static const std::shared_ptr<IProtected>& getDefaultImpl();
+  virtual ::ndk::ScopedAStatus PermissionProtected() = 0;
+  virtual ::ndk::ScopedAStatus MultiplePermissions() = 0;
+  virtual ::ndk::ScopedAStatus MultiplePermissions2() = 0;
+private:
+  static std::shared_ptr<IProtected> default_impl;
+};
+class IProtectedDefault : public IProtected {
+public:
+  ::ndk::ScopedAStatus PermissionProtected() override;
+  ::ndk::ScopedAStatus MultiplePermissions() override;
+  ::ndk::ScopedAStatus MultiplePermissions2() override;
+  ::ndk::SpAIBinder asBinder() override;
+  bool isRemote() override;
+};
+}  // namespace permission
+}  // namespace tests
+}  // namespace aidl
+}  // namespace android
+}  // namespace aidl
