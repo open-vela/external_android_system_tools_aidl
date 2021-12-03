f2c49a34378d52c7f4b811e2f72cb6eeb416634f
openvela-robot <openvela-robot@xiaomi.com>
2021-12-03 10:56:49 +0800

    Revert "Add automatic default value for char-type field"
    This reverts commit 3cd81ac2ecc4998bfc9ace215c39d15b4d72d1b2.
    
    Reason for revert: Breaks some sc-dev builds.
    Bug: 208964910
    
    Change-Id: I8649ad843224c2cec1c866b39a9ae580acfa2174

--- tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/BpDeprecated.h
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/BpDeprecated.h
@@ -6,13 +6,19 @@
 #include <android/aidl/tests/IDeprecated.h>
 
 namespace android {
+
 namespace aidl {
+
 namespace tests {
+
 class __attribute__((deprecated("test"))) BpDeprecated : public ::android::BpInterface<IDeprecated> {
 public:
   explicit BpDeprecated(const ::android::sp<::android::IBinder>& _aidl_impl);
   virtual ~BpDeprecated() = default;
 };  // class BpDeprecated
+
 }  // namespace tests
+
 }  // namespace aidl
+
 }  // namespace android
--- tests/golden_output/aidl-test-interface-java-source/gen/android/aidl/tests/ParcelableForToString.java
+++ tests/golden_output/aidl-test-interface-java-source/gen/android/aidl/tests/ParcelableForToString.java
@@ -53,19 +53,37 @@ public class ParcelableForToString implements android.os.Parcelable
     _aidl_parcel.writeFloatArray(floatArray);
     _aidl_parcel.writeByte(byteValue);
     _aidl_parcel.writeByteArray(byteArray);
-    _aidl_parcel.writeBoolean(booleanValue);
+    _aidl_parcel.writeInt(((booleanValue)?(1):(0)));
     _aidl_parcel.writeBooleanArray(booleanArray);
     _aidl_parcel.writeString(stringValue);
     _aidl_parcel.writeStringArray(stringArray);
     _aidl_parcel.writeStringList(stringList);
-    _aidl_parcel.writeTypedObject(parcelableValue, 0);
+    if ((parcelableValue!=null)) {
+      _aidl_parcel.writeInt(1);
+      parcelableValue.writeToParcel(_aidl_parcel, 0);
+    }
+    else {
+      _aidl_parcel.writeInt(0);
+    }
     _aidl_parcel.writeTypedArray(parcelableArray, 0);
     _aidl_parcel.writeInt(enumValue);
     _aidl_parcel.writeIntArray(enumArray);
     _aidl_parcel.writeStringArray(nullArray);
     _aidl_parcel.writeStringList(nullList);
-    _aidl_parcel.writeTypedObject(parcelableGeneric, 0);
-    _aidl_parcel.writeTypedObject(unionValue, 0);
+    if ((parcelableGeneric!=null)) {
+      _aidl_parcel.writeInt(1);
+      parcelableGeneric.writeToParcel(_aidl_parcel, 0);
+    }
+    else {
+      _aidl_parcel.writeInt(0);
+    }
+    if ((unionValue!=null)) {
+      _aidl_parcel.writeInt(1);
+      unionValue.writeToParcel(_aidl_parcel, 0);
+    }
+    else {
+      _aidl_parcel.writeInt(0);
+    }
     int _aidl_end_pos = _aidl_parcel.dataPosition();
     _aidl_parcel.setDataPosition(_aidl_start_pos);
     _aidl_parcel.writeInt(_aidl_end_pos - _aidl_start_pos);
@@ -98,7 +116,7 @@ public class ParcelableForToString implements android.os.Parcelable
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       byteArray = _aidl_parcel.createByteArray();
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
-      booleanValue = _aidl_parcel.readBoolean();
+      booleanValue = (0!=_aidl_parcel.readInt());
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       booleanArray = _aidl_parcel.createBooleanArray();
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
@@ -108,7 +126,12 @@ public class ParcelableForToString implements android.os.Parcelable
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       stringList = _aidl_parcel.createStringArrayList();
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
-      parcelableValue = _aidl_parcel.readTypedObject(android.aidl.tests.OtherParcelableForToString.CREATOR);
+      if ((0!=_aidl_parcel.readInt())) {
+        parcelableValue = android.aidl.tests.OtherParcelableForToString.CREATOR.createFromParcel(_aidl_parcel);
+      }
+      else {
+        parcelableValue = null;
+      }
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       parcelableArray = _aidl_parcel.createTypedArray(android.aidl.tests.OtherParcelableForToString.CREATOR);
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
@@ -120,9 +143,19 @@ public class ParcelableForToString implements android.os.Parcelable
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       nullList = _aidl_parcel.createStringArrayList();
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
-      parcelableGeneric = _aidl_parcel.readTypedObject(android.aidl.tests.GenericStructuredParcelable.CREATOR);
+      if ((0!=_aidl_parcel.readInt())) {
+        parcelableGeneric = android.aidl.tests.GenericStructuredParcelable.CREATOR.createFromParcel(_aidl_parcel);
+      }
+      else {
+        parcelableGeneric = null;
+      }
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
-      unionValue = _aidl_parcel.readTypedObject(android.aidl.tests.Union.CREATOR);
+      if ((0!=_aidl_parcel.readInt())) {
+        unionValue = android.aidl.tests.Union.CREATOR.createFromParcel(_aidl_parcel);
+      }
+      else {
+        unionValue = null;
+      }
     } finally {
       if (_aidl_start_pos > (Integer.MAX_VALUE - _aidl_parcelable_size)) {
         throw new android.os.BadParcelableException("Overflow in the size of parcelable");
