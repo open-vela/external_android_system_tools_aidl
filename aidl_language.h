1ff1d9e45d53c759bbe43e7b3f0cf9bffabd931f
liujinye <liujinye@xiaomi.com>
2021-12-03 10:00:23 +0800

    Add automatic default value for char-type field am: 3cd81ac2ec
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/16339498
    
    Change-Id: Ief8f89ae521896d9cd2f5fac04bdc427e4fb8c3d

--- aidl_language.h
+++ aidl_language.h
@@ -209,7 +209,6 @@ class AidlAnnotation : public AidlNode {
     SENSITIVE_DATA,
     JAVA_PASSTHROUGH,
     JAVA_DERIVE,
-    JAVA_DEFAULT,
     JAVA_ONLY_IMMUTABLE,
     FIXED_SIZE,
     DESCRIPTOR,
@@ -326,7 +325,6 @@ class AidlAnnotatable : public AidlCommentable {
   bool IsStableApiParcelable(Options::Language lang) const;
   bool IsHide() const;
   bool JavaDerive(const std::string& method) const;
-  bool IsJavaDefault() const;
   std::string GetDescriptor() const;
 
   const AidlAnnotation* UnsupportedAppUsage() const;
