356489553fb790856fcc0845af3136fda4a566a6
liujinye <liujinye@xiaomi.com>
2021-12-03 09:58:54 +0800

    Add automatic default value for char-type field am: 3cd81ac2ec
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/16339498
    
    Change-Id: I528621a149381bdb92144aa2cbb904d4766d8012

--- tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/FixedSize.rs.d
+++
@@ -1,3 +0,0 @@
-out/soong/.intermediates/system/tools/aidl/aidl-test-interface-rust-source/gen/android/aidl/tests/FixedSize.rs : \
-  system/tools/aidl/tests/android/aidl/tests/FixedSize.aidl \
-  system/tools/aidl/tests/android/aidl/tests/LongEnum.aidl
