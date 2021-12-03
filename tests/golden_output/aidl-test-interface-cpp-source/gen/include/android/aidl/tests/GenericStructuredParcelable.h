ea4ab0d45a4f41b466a4ecd67680ef32cbaf2fab
liujinye <liujinye@xiaomi.com>
2021-12-03 10:23:27 +0800

    Add automatic default value for char-type field am: 3cd81ac2ec am: 7b023a58f7
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/16339498
    
    Change-Id: I082ec97eebefa7b76ee7066e0931edf9e5f07e83

--- tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/GenericStructuredParcelable.h
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/GenericStructuredParcelable.h
@@ -8,13 +8,14 @@
 #include <utils/String16.h>
 
 namespace android {
+
 namespace aidl {
+
 namespace tests {
+
 template <typename T, typename U, typename B>
 class GenericStructuredParcelable : public ::android::Parcelable {
 public:
-  int32_t a = 0;
-  int32_t b = 0;
   inline bool operator!=(const GenericStructuredParcelable& rhs) const {
     return std::tie(a, b) != std::tie(rhs.a, rhs.b);
   }
@@ -34,6 +35,8 @@ public:
     return std::tie(a, b) >= std::tie(rhs.a, rhs.b);
   }
 
+  int32_t a = int32_t(0);
+  int32_t b = int32_t(0);
   ::android::status_t readFromParcel(const ::android::Parcel* _aidl_parcel) final;
   ::android::status_t writeToParcel(::android::Parcel* _aidl_parcel) const final;
   static const ::android::String16& getParcelableDescriptor() {
@@ -49,14 +52,20 @@ public:
     return os.str();
   }
 };  // class GenericStructuredParcelable
+
 }  // namespace tests
+
 }  // namespace aidl
+
 }  // namespace android
 #include <android/aidl/tests/GenericStructuredParcelable.h>
 
 namespace android {
+
 namespace aidl {
+
 namespace tests {
+
 template <typename T, typename U, typename B>
 ::android::status_t GenericStructuredParcelable<T,U,B>::readFromParcel(const ::android::Parcel* _aidl_parcel) {
   ::android::status_t _aidl_ret_status = ::android::OK;
@@ -84,6 +93,7 @@ template <typename T, typename U, typename B>
   _aidl_parcel->setDataPosition(_aidl_start_pos + _aidl_parcelable_size);
   return _aidl_ret_status;
 }
+
 template <typename T, typename U, typename B>
 ::android::status_t GenericStructuredParcelable<T,U,B>::writeToParcel(::android::Parcel* _aidl_parcel) const {
   ::android::status_t _aidl_ret_status = ::android::OK;
@@ -103,6 +113,9 @@ template <typename T, typename U, typename B>
   _aidl_parcel->setDataPosition(_aidl_end_pos);
   return _aidl_ret_status;
 }
+
 }  // namespace tests
+
 }  // namespace aidl
+
 }  // namespace android
