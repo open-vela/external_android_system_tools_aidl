dac5ee1bd2609814b0f4829b64ee902c416afe10
liujinye <liujinye@xiaomi.com>
2021-12-03 13:31:55 +0800

    Fix sc-dev-plus-aosp builds
    Ignore-AOSP-First: Fix merge conflict
    Bug: 208964910
    Change-Id: I7924d7b19ec30e42fd1d17ec913c4a75d51ea6bc

--- tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/BpNewName.h
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/BpNewName.h
@@ -6,14 +6,20 @@
 #include <android/aidl/tests/INewName.h>
 
 namespace android {
+
 namespace aidl {
+
 namespace tests {
+
 class BpNewName : public ::android::BpInterface<INewName> {
 public:
   explicit BpNewName(const ::android::sp<::android::IBinder>& _aidl_impl);
   virtual ~BpNewName() = default;
   ::android::binder::Status RealName(::android::String16* _aidl_return) override;
 };  // class BpNewName
+
 }  // namespace tests
+
 }  // namespace aidl
+
 }  // namespace android
--- tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/ConstantExpressionEnum.h
+++ tests/golden_output/aidl-test-interface-cpp-source/gen/include/android/aidl/tests/ConstantExpressionEnum.h
@@ -6,8 +6,11 @@
 #include <string>
 
 namespace android {
+
 namespace aidl {
+
 namespace tests {
+
 enum class ConstantExpressionEnum : int32_t {
   decInt32_1 = 1,
   decInt32_2 = 1,
@@ -20,12 +23,7 @@ enum class ConstantExpressionEnum : int32_t {
   hexInt32_3 = 1,
   hexInt64_1 = 1,
 };
-}  // namespace tests
-}  // namespace aidl
-}  // namespace android
-namespace android {
-namespace aidl {
-namespace tests {
+
 [[nodiscard]] static inline std::string toString(ConstantExpressionEnum val) {
   switch(val) {
   case ConstantExpressionEnum::decInt32_1:
@@ -34,11 +32,16 @@ namespace tests {
     return std::to_string(static_cast<int32_t>(val));
   }
 }
+
 }  // namespace tests
+
 }  // namespace aidl
+
 }  // namespace android
 namespace android {
+
 namespace internal {
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wc++17-extensions"
 template <>
@@ -55,5 +58,7 @@ constexpr inline std::array<::android::aidl::tests::ConstantExpressionEnum, 10> 
   ::android::aidl::tests::ConstantExpressionEnum::hexInt64_1,
 };
 #pragma clang diagnostic pop
+
 }  // namespace internal
+
 }  // namespace android
