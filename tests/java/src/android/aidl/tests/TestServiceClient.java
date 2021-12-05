a67b259001681f8f7072d09e3cb626fe1cf8921a
liujinye <liujinye@xiaomi.com>
2021-12-06 03:33:59 +0800

    [automerger skipped] Add automatic default value for char-type field am: 4d08d5ccdd -s ours
    am skip reason: Merged-In I15cfe2a90c0c9adca1692d9a9c997901b90bbbb6 with SHA-1 1d8d50a56e is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/16388704
    
    Change-Id: Icab477ba1af6ed7292954ba35646928076bca167

--- tests/java/src/android/aidl/tests/TestServiceClient.java
+++ tests/java/src/android/aidl/tests/TestServiceClient.java
@@ -18,10 +18,11 @@ package android.aidl.tests;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assume.assumeTrue;
 
 import android.aidl.tests.ByteEnum;
@@ -283,6 +284,38 @@ public class TestServiceClient {
     }
 
     @Test
+    public void testBinderArrayExchange() throws RemoteException {
+      String[] names = {"Fizz", "Buzz"};
+      INamedCallback[] got = service.GetInterfaceArray(names);
+      assertThat(got[0].GetName(), is(names[0]));
+      assertThat(got[1].GetName(), is(names[1]));
+
+      assertThat(service.VerifyNamesWithInterfaceArray(got, names), is(true));
+    }
+
+    @Test
+    public void testNullableBinderArrayExchange() throws RemoteException {
+      String[] names = {"Fizz", null, "Buzz"};
+      INamedCallback[] got = service.GetNullableInterfaceArray(names);
+      assertThat(got[0].GetName(), is(names[0]));
+      assertNull(got[1]);
+      assertThat(got[2].GetName(), is(names[2]));
+
+      assertThat(service.VerifyNamesWithNullableInterfaceArray(got, names), is(true));
+    }
+
+    @Test
+    public void testInterfaceListExchange() throws RemoteException {
+      String[] names = {"Fizz", null, "Buzz"};
+      List<INamedCallback> got = service.GetInterfaceList(names);
+      assertThat(got.get(0).GetName(), is(names[0]));
+      assertNull(got.get(1));
+      assertThat(got.get(2).GetName(), is(names[2]));
+
+      assertThat(service.VerifyNamesWithInterfaceList(got, names), is(true));
+    }
+
+    @Test
     public void testListReversal() throws RemoteException {
         List<String> input = Arrays.asList("Walk", "into", "Córdoba");
         List<String> echoed = new ArrayList<String>();
@@ -437,6 +470,21 @@ public class TestServiceClient {
         assertThat(reversed[2].getDouble(testDoubleKey), is(input[0].getDouble(testDoubleKey)));
     }
 
+    private void writeToFd(FileDescriptor fd, byte[] testData) throws IOException {
+      FileOutputStream fdStream = new FileOutputStream(fd);
+      fdStream.write(testData);
+      fdStream.close();
+    }
+
+    private void verifyFileContents(String file, byte[] testData) throws IOException {
+      FileInputStream fis = new FileInputStream(file);
+      byte[] readData = new byte[testData.length];
+
+      assertThat(fis.read(readData), is(readData.length));
+      assertThat(readData, is(testData));
+      assertThat(fis.read(), is(-1));
+    }
+
     @Test
     public void testFileDescriptorPassing() throws RemoteException, IOException {
         assumeTrue(cpp_java_tests != null);
@@ -448,18 +496,43 @@ public class TestServiceClient {
         FileDescriptor journeyed = cpp_java_tests.RepeatFileDescriptor(descriptor);
         fos.close();
 
-        FileOutputStream journeyedStream = new FileOutputStream(journeyed);
-
         String testData = "FrazzleSnazzleFlimFlamFlibbityGumboChops";
-        byte[] output = testData.getBytes();
-        journeyedStream.write(output);
-        journeyedStream.close();
+        writeToFd(journeyed, testData.getBytes());
+        verifyFileContents(file, testData.getBytes());
+    }
 
-        FileInputStream fis = new FileInputStream(file);
-        byte[] input = new byte[output.length];
+    @Test
+    public void testFileDescriptorArrayPassing() throws RemoteException, IOException {
+      assumeTrue(cpp_java_tests != null);
 
-        assertThat(fis.read(input), is(input.length));
-        assertThat(input, is(output));
+      final int kTestSize = 2;
+
+      String fileBase = "/data/local/tmp/aidl-test-file_";
+      String[] files = new String[kTestSize]; // any size to test
+      FileOutputStream[] fos = new FileOutputStream[kTestSize];
+      FileDescriptor[] descriptors = new FileDescriptor[kTestSize];
+      for (int i = 0; i < kTestSize; i++) {
+        files[i] = fileBase + i;
+        fos[i] = new FileOutputStream(files[i], false /*append*/);
+        descriptors[i] = fos[i].getFD();
+      }
+
+      FileDescriptor[] repeated = new FileDescriptor[kTestSize];
+      FileDescriptor[] reversed = cpp_java_tests.ReverseFileDescriptorArray(descriptors, repeated);
+
+      for (int i = 0; i < kTestSize; i++) {
+        fos[i].close();
+      }
+
+      for (int i = 0; i < kTestSize; i++) {
+        String testData = "Something " + i;
+
+        writeToFd(reversed[kTestSize - 1 - i], testData.getBytes());
+        verifyFileContents(files[i], testData.getBytes());
+
+        writeToFd(repeated[i], testData.getBytes());
+        verifyFileContents(files[i], (testData + testData).getBytes());
+      }
     }
 
     @Test
@@ -512,7 +585,7 @@ public class TestServiceClient {
 
     @Test
     public void testReverseUtf8StringArray() throws RemoteException {
-        String[] input = (String[])utf8_queries.toArray();
+        String[] input = utf8_queries.toArray(new String[0]);
         String echoed[] = new String[input.length];
 
         String[] reversed = service.ReverseUtf8CppString(input, echoed);
@@ -535,7 +608,7 @@ public class TestServiceClient {
                 null,
                 // Java doesn't handle unicode code points above U+FFFF well.
                 new String(Character.toChars(0x1F701)) + "\u03A9");
-        String[] input = (String[])utf8_queries_and_nulls.toArray();
+        String[] input = utf8_queries_and_nulls.toArray(new String[0]);
         String echoed[] = new String[input.length];
 
         String[] reversed = service.ReverseNullableUtf8CppString(input, echoed);
@@ -628,6 +701,9 @@ public class TestServiceClient {
         assertThat(p.int32_max, is(Integer.MAX_VALUE));
         assertThat(p.int64_max, is(Long.MAX_VALUE));
         assertThat(p.hexInt32_neg_1, is(-1));
+        for (int ndx = 0; ndx < p.int8_1.length; ndx++) {
+          assertThat(p.int8_1[ndx], is((byte) 1));
+        }
         for (int ndx = 0; ndx < p.int32_1.length; ndx++) {
             assertThat(p.int32_1[ndx], is(1));
         }
@@ -687,6 +763,7 @@ public class TestServiceClient {
             + "int64_max: 9223372036854775807, "
             + "hexInt32_neg_1: -1, "
             + "ibinder: null, "
+            + "int8_1: [1, 1, 1, 1, 1], "
             + "int32_1: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "
             + "1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "
             + "1, 1, 1, 1], "
@@ -833,5 +910,24 @@ public class TestServiceClient {
       assertNotNull(reversed);
       assertThat(repeated.getNs(), is(new int[] {1, 2, 3}));
       assertThat(reversed.getNs(), is(new int[] {3, 2, 1}));
+    }
+
+    @Test
+    public void testReverseRecursiveList() throws RemoteException {
+      RecursiveList head = null;
+      for (int i = 0; i < 10; i++) {
+        RecursiveList node = new RecursiveList();
+        node.value = i;
+        node.next = head;
+        head = node;
+      }
+      // head: [9, 8, .. , 0]
+      RecursiveList reversed = service.ReverseList(head);
+      // reversed should be [0, 1, .. 9]
+      for (int i = 0; i < 10; i++) {
+        assertThat(reversed.value, is(i));
+        reversed = reversed.next;
+      }
+      assertNull(reversed);
     }
 }
