51719c33d4065aeca257c8f1b020e865fb3e5d5b
liujinye <liujinye@xiaomi.com>
2021-12-15 02:10:42 +0800

    Snap for 8001728 from d93a1ea6361348dc3e69c390bca3f0f637636b06 to tm-release
    Change-Id: I24b37398997d68bc3388142682afbe1499584892

--- tests/golden_output/aidl-test-fixedsizearray-cpp-source/gen/android/aidl/fixedsizearray/FixedSizeArrayExample.cpp.d
+++
@@ -1,2 +0,0 @@
-out/soong/.intermediates/system/tools/aidl/aidl-test-fixedsizearray-cpp-source/gen/android/aidl/fixedsizearray/FixedSizeArrayExample.cpp : \
-  system/tools/aidl/tests/android/aidl/fixedsizearray/FixedSizeArrayExample.aidl
