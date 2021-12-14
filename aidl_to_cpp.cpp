f81854f9bbe3f6d63737332c76ee50127985d38a
openvela-robot <openvela-robot@xiaomi.com>
2021-12-15 02:10:42 +0800

    Snap for 8001728 from d93a1ea6361348dc3e69c390bca3f0f637636b06 to tm-release
    Change-Id: I24b37398997d68bc3388142682afbe1499584892

--- aidl_to_cpp.cpp
+++ aidl_to_cpp.cpp
@@ -35,9 +35,6 @@ namespace aidl {
 namespace cpp {
 
 namespace {
-static const AidlTypeSpecifier kIntType{AIDL_LOCATION_HERE, "int", /*array=*/std::nullopt, nullptr,
-                                        Comments{}};
-
 std::string RawParcelMethod(const AidlTypeSpecifier& type, const AidlTypenames& typenames,
                             bool readMethod) {
   static map<string, string> kBuiltin = {
@@ -72,10 +69,6 @@ std::string RawParcelMethod(const AidlTypeSpecifier& type, const AidlTypenames& 
   const bool nullable = type.IsNullable();
   const bool isVector = type.IsArray() || typenames.IsList(type);
   const bool utf8 = type.IsUtf8InCpp();
-
-  if (type.IsFixedSizeArray()) {
-    return "FixedArray";
-  }
 
   if (auto enum_decl = typenames.GetEnumDeclaration(type); enum_decl != nullptr) {
     if (isVector) {
@@ -218,28 +211,16 @@ std::string GetTransactionIdFor(const std::string& clazz, const AidlMethod& meth
 }
 
 std::string CppNameOf(const AidlTypeSpecifier& type, const AidlTypenames& typenames) {
-  // get base type's cpp_name with nullable processed.
-  std::string cpp_name = GetCppName(type, typenames);
-
   if (type.IsArray() || typenames.IsList(type)) {
-    if (type.IsFixedSizeArray()) {
-      for (const auto& dim : std::get<FixedSizeArray>(type.GetArray()).dimensions) {
-        cpp_name = "std::array<" + cpp_name + ", " +
-                   dim->ValueString(kIntType, ConstantValueDecorator) + ">";
-      }
-    } else {
-      cpp_name = "::std::vector<" + cpp_name + ">";
-    }
-
-    // wrap nullable again because @nullable applies to BOTH array type(outermost type) AND base
-    // type(innermost type)
+    std::string cpp_name = GetCppName(type, typenames);
     if (type.IsHeapNullable()) {
-      cpp_name = "::std::unique_ptr<" + cpp_name + ">";
+      return "::std::unique_ptr<::std::vector<" + cpp_name + ">>";
     } else if (type.IsNullable()) {
-      cpp_name = "::std::optional<" + cpp_name + ">";
+      return "::std::optional<::std::vector<" + cpp_name + ">>";
     }
+    return "::std::vector<" + cpp_name + ">";
   }
-  return cpp_name;
+  return GetCppName(type, typenames);
 }
 
 bool IsNonCopyableType(const AidlTypeSpecifier& type, const AidlTypenames& typenames) {
@@ -288,28 +269,25 @@ std::string ParcelWriteCastOf(const AidlTypeSpecifier& type, const AidlTypenames
 // Add includes for a type ref. Note that this is non-recursive.
 void AddHeaders(const AidlTypeSpecifier& type, const AidlTypenames& typenames,
                 std::set<std::string>* headers) {
-  if (type.IsArray()) {
-    if (type.IsFixedSizeArray()) {
-      headers->insert("array");
-    } else {
-      headers->insert("vector");
-    }
+  AIDL_FATAL_IF(typenames.IsList(type) && type.GetTypeParameters().size() != 1, type);
+  bool isVector = type.IsArray() || typenames.IsList(type);
+  bool isNullable = type.IsNullable();
+  bool utf8 = type.IsUtf8InCpp();
+
+  if (isVector) {
+    headers->insert("vector");
   }
-  if (type.IsNullable()) {
+  if (isNullable) {
     if (type.GetName() != "IBinder") {
       headers->insert("optional");
     }
   }
   if (typenames.IsList(type)) {
-    headers->insert("vector");
+    // Nothing else to do for List.
     return;
   }
   if (type.GetName() == "String") {
-    if (type.IsUtf8InCpp()) {
-      headers->insert("string");
-    } else {
-      headers->insert("utils/String16.h");
-    }
+    headers->insert(utf8 ? "string" : "utils/String16.h");
     return;
   }
   if (type.GetName() == "IBinder") {
--- tests/golden_test.sh
+++ tests/golden_test.sh
@@ -46,7 +46,6 @@ function _golden_test() {
     "aidl_test_loggable_interface-java-source"
     "aidl_test_loggable_interface-ndk-source"
     "aidl-test-interface-platform-java-source"
-    "aidl-test-fixedsizearray-cpp-source"
     "aidl-test-fixedsizearray-ndk-source"
   )
 
