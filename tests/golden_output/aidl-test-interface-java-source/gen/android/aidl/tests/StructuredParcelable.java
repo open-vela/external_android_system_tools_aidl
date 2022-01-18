2fd57cfef586d184a6b4266f9e69ae99fb017695
liujinye <liujinye@xiaomi.com>
2022-01-18 20:06:33 +0800

    Throw error for invalid Java parcelable size. am: f5d7608d89 am: 8ffb02c0d5 am: 085fec1f1d am: c76471f861
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1950405
    
    Change-Id: Iccf5a71f349f6a536bc169d28a496db8f8d11bd9

--- tests/golden_output/aidl-test-interface-java-source/gen/android/aidl/tests/StructuredParcelable.java
+++ tests/golden_output/aidl-test-interface-java-source/gen/android/aidl/tests/StructuredParcelable.java
@@ -144,7 +144,7 @@ public class StructuredParcelable implements android.os.Parcelable
     int _aidl_start_pos = _aidl_parcel.dataPosition();
     int _aidl_parcelable_size = _aidl_parcel.readInt();
     try {
-      if (_aidl_parcelable_size < 0) return;
+      if (_aidl_parcelable_size < 0) throw new android.os.BadParcelableException("Parcelable too small");;
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
       shouldContainThreeFs = _aidl_parcel.createIntArray();
       if (_aidl_parcel.dataPosition() - _aidl_start_pos >= _aidl_parcelable_size) return;
