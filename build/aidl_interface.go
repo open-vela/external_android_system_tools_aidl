84a1252d61ba58eb1a0932f083fece878a8c0d16
Steven Moreland <smoreland@google.com>
2022-01-21 22:56:12 +0000

    srcs_available true by default
    The ODR checks take these into account now. These are true by
    default since downstream partners need to work past the same
    cyclical dependencies that we need to work around in our tree,
    and if this needs to be specified, it requires downstream to
    modify AOSP build files, which can introduce merge conflicts.
    
    Path:
    - true by default
    - cleanup users
    - remove srcs_available
    
    Bug: 215754978
    Test: aidl_test
    Change-Id: I80361aef2d68acc88d0dc720f907f441f31de930

--- build/aidl_interface.go
+++ build/aidl_interface.go
@@ -834,7 +834,7 @@ func srcsVisibility(mctx android.LoadHookContext, lang string) []string {
 	if a, ok := mctx.Module().(*aidlInterface); !ok {
 		panic(fmt.Errorf("%q is not aidl_interface", mctx.Module().String()))
 	} else {
-		if proptools.Bool(a.commonBackendProperties(lang).Srcs_available) {
+		if proptools.BoolDefault(a.commonBackendProperties(lang).Srcs_available, true) {
 			// Returning nil so that the visibility of the source module defaults to the
 			// the package-level default visibility. This way, the source module gets
 			// the same visibility as the library modules.
