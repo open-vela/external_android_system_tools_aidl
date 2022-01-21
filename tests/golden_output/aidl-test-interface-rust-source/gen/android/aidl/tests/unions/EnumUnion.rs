81dae4e437b75665c8f57f93f2d7b1ee22d60587
liujinye <liujinye@xiaomi.com>
2022-01-22 04:10:34 +0800

    Snap for 8105868 from 1ab2111f326f377dbfc0fba984d761b11beb9aba to tm-release
    Change-Id: Ib3bd445cc9067dbe9db5705bc4a67da174214191

--- tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/unions/EnumUnion.rs
+++ tests/golden_output/aidl-test-interface-rust-source/gen/android/aidl/tests/unions/EnumUnion.rs
@@ -10,8 +10,8 @@ impl Default for EnumUnion {
     Self::IntEnum(crate::mangled::_7_android_4_aidl_5_tests_7_IntEnum::FOO)
   }
 }
-impl binder::parcel::Parcelable for EnumUnion {
-  fn write_to_parcel(&self, parcel: &mut binder::parcel::BorrowedParcel) -> binder::Result<()> {
+impl binder::Parcelable for EnumUnion {
+  fn write_to_parcel(&self, parcel: &mut binder::binder_impl::BorrowedParcel) -> std::result::Result<(), binder::StatusCode> {
     match self {
       Self::IntEnum(v) => {
         parcel.write(&0i32)?;
@@ -23,7 +23,7 @@ impl binder::parcel::Parcelable for EnumUnion {
       }
     }
   }
-  fn read_from_parcel(&mut self, parcel: &binder::parcel::BorrowedParcel) -> binder::Result<()> {
+  fn read_from_parcel(&mut self, parcel: &binder::binder_impl::BorrowedParcel) -> std::result::Result<(), binder::StatusCode> {
     let tag: i32 = parcel.read()?;
     match tag {
       0 => {
@@ -44,7 +44,7 @@ impl binder::parcel::Parcelable for EnumUnion {
 }
 binder::impl_serialize_for_parcelable!(EnumUnion);
 binder::impl_deserialize_for_parcelable!(EnumUnion);
-impl binder::parcel::ParcelableMetadata for EnumUnion {
+impl binder::binder_impl::ParcelableMetadata for EnumUnion {
   fn get_descriptor() -> &'static str { "android.aidl.tests.unions.EnumUnion" }
 }
 pub(crate) mod mangled {
