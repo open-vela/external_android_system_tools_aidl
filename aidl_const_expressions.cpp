619fc4ec0d87a29a388d127e68c606cf1274fa48
openvela-robot <openvela-robot@xiaomi.com>
2022-02-03 02:12:38 +0800

    Snap for 8142553 from b8249f1d80e555070996ddd4af23ebf3844af19b to tm-release
    Change-Id: I5a8bb14eca55f6f39d5e5b5ac05f63bb2cc1ffd8

--- aidl_const_expressions.cpp
+++ aidl_const_expressions.cpp
@@ -666,14 +666,6 @@ bool AidlConstantValue::CheckValid() const {
   return true;
 }
 
-bool AidlConstantValue::Evaluate() const {
-  if (CheckValid()) {
-    return evaluate();
-  } else {
-    return false;
-  }
-}
-
 bool AidlConstantValue::evaluate() const {
   if (is_evaluated_) {
     return is_valid_;
