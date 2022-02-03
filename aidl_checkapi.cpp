a96e82e8689eb8abb24ab97b5172f9a67f0a9807
openvela-robot <openvela-robot@xiaomi.com>
2022-02-03 21:59:00 +0800

    Add Delegator support to the Java backend am: 14ca7d95a2 am: 0283c83865
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1968809
    
    Change-Id: I8d5fceb68c93135edf75c1aa0df5ea2cdff0368b

--- aidl_checkapi.cpp
+++ aidl_checkapi.cpp
@@ -79,6 +79,7 @@ static vector<string> get_strict_annotations(const AidlAnnotatable& node) {
       // @JavaDerive doesn't affect read/write
       AidlAnnotation::Type::JAVA_DERIVE,
       AidlAnnotation::Type::JAVA_DEFAULT,
+      AidlAnnotation::Type::JAVA_DELEGATOR,
       AidlAnnotation::Type::JAVA_ONLY_IMMUTABLE,
       AidlAnnotation::Type::JAVA_SUPPRESS_LINT,
       // @Backing for a enum type is checked by the enum checker
