9054cebd3cd7977cf8b05b56a851c683d72ea861
Devin Moore <devinmoore@google.com>
2022-02-04 18:08:22 +0000

    Revert "Revert "Add Delegator support to NDK backend""
    Try again without removing "final" from the Bn* meta methods.
    When creating a new *Delegator object, it will reach out to the
    implementation to make sure both versions of the interface are the same.
    This is currently a limit for the NDK backend.
    
    This reverts commit 70164fcf17dbc39d3257a480d6203548d399c0b3.
    
    Test: atest aidl_integration_test
    Bug: 198785815
    Bug: 222347502
    
    Change-Id: Ic47c7547e5cc1be64e5ba1586db9b2956256f88e

--- tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/BnNewName.h
+++ tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/BnNewName.h
@@ -3,6 +3,13 @@
 #include "aidl/android/aidl/tests/INewName.h"
 
 #include <android/binder_ibinder.h>
+#include <cassert>
+
+#ifndef __BIONIC__
+#ifndef __assert2
+#define __assert2(a,b,c,d) ((void)0)
+#endif
+#endif
 
 namespace aidl {
 namespace android {
@@ -16,6 +23,19 @@ protected:
   ::ndk::SpAIBinder createBinder() override;
 private:
 };
+class INewNameDelegator : public BnNewName {
+public:
+  explicit INewNameDelegator(const std::shared_ptr<INewName> &impl) : _impl(impl) {
+  }
+
+  ::ndk::ScopedAStatus RealName(std::string* _aidl_return) override {
+    return _impl->RealName(_aidl_return);
+  }
+protected:
+private:
+  std::shared_ptr<INewName> _impl;
+};
+
 }  // namespace tests
 }  // namespace aidl
 }  // namespace android
