5cda07ddeb5fe4fb2fba45bdb4a05398aa7bb584
liujinye <liujinye@xiaomi.com>
2022-02-15 01:24:58 +0800

    Fix gen_rpc for versioned interfaces. am: 781f4327f8 am: 57056c3e05 am: 9744f3b1da
    Original change: https://android-review.googlesource.com/c/platform/system/tools/aidl/+/1982896
    
    Change-Id: I058be96478ce4b99ea16160c96ea4d29dcf06262

--- generate_java_binder.cpp
+++ generate_java_binder.cpp
@@ -994,13 +994,14 @@ static void GenerateMethods(const AidlInterface& iface, const AidlMethod& method
            << "public int " << kGetInterfaceVersion << "()"
            << " throws "
            << "android.os.RemoteException {\n"
-           << "  if (mCachedVersion == -1) {\n"
-           << "    android.os.Parcel data = android.os.Parcel.obtain();\n"
-           << "    android.os.Parcel reply = android.os.Parcel.obtain();\n";
+           << "  if (mCachedVersion == -1) {\n";
       if (options.GenRpc()) {
-        code << "    data.markForBinder(asBinder());\n";
+        code << "    android.os.Parcel data = android.os.Parcel.obtain(asBinder());\n";
+      } else {
+        code << "    android.os.Parcel data = android.os.Parcel.obtain();\n";
       }
-      code << "    try {\n"
+      code << "    android.os.Parcel reply = android.os.Parcel.obtain();\n"
+           << "    try {\n"
            << "      data.writeInterfaceToken(DESCRIPTOR);\n"
            << "      boolean _status = mRemote.transact(Stub." << transactCodeName << ", "
            << "data, reply, 0);\n";
@@ -1028,13 +1029,14 @@ static void GenerateMethods(const AidlInterface& iface, const AidlMethod& method
            << "public synchronized String " << kGetInterfaceHash << "()"
            << " throws "
            << "android.os.RemoteException {\n"
-           << "  if (\"-1\".equals(mCachedHash)) {\n"
-           << "    android.os.Parcel data = android.os.Parcel.obtain();\n"
-           << "    android.os.Parcel reply = android.os.Parcel.obtain();\n";
+           << "  if (\"-1\".equals(mCachedHash)) {\n";
       if (options.GenRpc()) {
-        code << "    data.markForBinder(asBinder());\n";
+        code << "    android.os.Parcel data = android.os.Parcel.obtain(asBinder());\n";
+      } else {
+        code << "    android.os.Parcel data = android.os.Parcel.obtain();\n";
       }
-      code << "    try {\n"
+      code << "    android.os.Parcel reply = android.os.Parcel.obtain();\n"
+           << "    try {\n"
            << "      data.writeInterfaceToken(DESCRIPTOR);\n"
            << "      boolean _status = mRemote.transact(Stub." << transactCodeName << ", "
            << "data, reply, 0);\n";
