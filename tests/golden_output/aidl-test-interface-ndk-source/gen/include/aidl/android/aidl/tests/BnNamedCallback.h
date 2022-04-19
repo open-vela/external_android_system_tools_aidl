3a2955dab921ee21e17406b52babb0af89355a80
openvela-robot <openvela-robot@xiaomi.com>
2022-04-19 18:12:29 +0800

    [automerger skipped] Java: require interfaceVersion/hash for ToT vers. am: 5ac0050901 -s ours
    am skip reason: Merged-In I0b350d9cbc4aca62bff6dc268ee7419044fc6d01 with SHA-1 ed0490c75a is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/tools/aidl/+/17715748
    
    Change-Id: I8c3ac8db13eaab77dccd1d587a00ec83f971e752
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/BnNamedCallback.h
+++ tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/BnNamedCallback.h
@@ -3,13 +3,6 @@
 #include "aidl/android/aidl/tests/INamedCallback.h"
 
 #include <android/binder_ibinder.h>
-#include <cassert>
-
-#ifndef __BIONIC__
-#ifndef __assert2
-#define __assert2(a,b,c,d) ((void)0)
-#endif
-#endif
 
 namespace aidl {
 namespace android {
@@ -23,19 +16,6 @@ protected:
   ::ndk::SpAIBinder createBinder() override;
 private:
 };
-class INamedCallbackDelegator : public BnNamedCallback {
-public:
-  explicit INamedCallbackDelegator(const std::shared_ptr<INamedCallback> &impl) : _impl(impl) {
-  }
-
-  ::ndk::ScopedAStatus GetName(std::string* _aidl_return) override {
-    return _impl->GetName(_aidl_return);
-  }
-protected:
-private:
-  std::shared_ptr<INamedCallback> _impl;
-};
-
 }  // namespace tests
 }  // namespace aidl
 }  // namespace android
